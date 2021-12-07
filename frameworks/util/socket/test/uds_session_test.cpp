ee23ea1b7bf7cae88ef131a54a83b4eef9788dbc
h00514358 <huiyuehong@huawei.com>
2021-12-07 11:49:43 +0000

    Signed-off-by:hellohyh001<huiyuehong@huawei.com>
    Signed-off-by: h00514358 <huiyuehong@huawei.com>

--- frameworks/util/socket/test/uds_session_test.cpp
+++ frameworks/util/socket/test/uds_session_test.cpp
@@ -20,9 +20,6 @@
 namespace {
 using namespace testing::ext;
 using namespace OHOS::MMI;
-namespace {
-    constexpr int32_t UID_ROOT = 0;
-}
 
 class UDSSessionTest : public testing::Test {
 public:
@@ -31,14 +28,12 @@ public:
     int fd_ = -1;
     static constexpr char programName_[] = "uds_sesion_test";
     const int moduleType_ = 3; // 3 CONNECT_MODULE_TYPE_ST_TEST
-    static int32_t pid_;
     void SetUp() override;
     void TearDown()  override;
 };
-int32_t UDSSessionTest::pid_ = 0;
+
 void UDSSessionTest::SetUp()
 {
-    UDSSessionTest::pid_ = getpid();
     fd_ = STDOUT_FILENO;
 }
 
@@ -49,7 +44,7 @@ void UDSSessionTest::TearDown()
 
 HWTEST_F(UDSSessionTest, Construct, TestSize.Level1)
 {
-    UDSSession udsSession(programName_, moduleType_, fd_, UID_ROOT, pid_);
+    UDSSession udsSession(programName_, moduleType_, fd_);
     udsSession.Close();
 }
 
@@ -57,7 +52,7 @@ HWTEST_F(UDSSessionTest, SendMsg_type1_001, TestSize.Level1)
 {
     const char *buf = "1234";
     size_t size = 4;
-    UDSSession sesObj(programName_, moduleType_, fd_, UID_ROOT, pid_);
+    UDSSession sesObj(programName_, moduleType_, fd_);
     bool retResult = sesObj.SendMsg(buf, size);
     EXPECT_TRUE(retResult);
 }
@@ -67,7 +62,7 @@ HWTEST_F(UDSSessionTest, SendMsg_type1_002, TestSize.Level1)
     const char *buf = nullptr;
     size_t size = 4;
 
-    UDSSession sesObj(programName_, moduleType_, fd_, UID_ROOT, pid_);
+    UDSSession sesObj(programName_, moduleType_, fd_);
     bool retResult = sesObj.SendMsg(buf, size);
     EXPECT_TRUE(retResult);
 }
@@ -76,7 +71,7 @@ HWTEST_F(UDSSessionTest, SendMsg_type1_003, TestSize.Level1)
 {
     const char *buf = nullptr;
     size_t size = 0;
-    UDSSession sesObj(programName_, moduleType_, fd_, UID_ROOT, pid_);
+    UDSSession sesObj(programName_, moduleType_, fd_);
     bool retResult = sesObj.SendMsg(buf, size);
     EXPECT_FALSE(retResult);
 }
@@ -86,7 +81,7 @@ HWTEST_F(UDSSessionTest, SendMsg_type1_004, TestSize.Level1)
     const char *buf = "this unit data";
     size_t size = 14;
 
-    UDSSession sesObj(programName_, moduleType_, fd_, UID_ROOT, pid_);
+    UDSSession sesObj(programName_, moduleType_, fd_);
     bool retResult = sesObj.SendMsg(buf, size);
     EXPECT_TRUE(retResult);
 }
@@ -96,7 +91,7 @@ HWTEST_F(UDSSessionTest, SendMsg_type1_005, TestSize.Level1)
     const char *buf = "this unit data";
     size_t size = -1001;
 
-    UDSSession sesObj(programName_, moduleType_, fd_, UID_ROOT, pid_);
+    UDSSession sesObj(programName_, moduleType_, fd_);
     bool retResult = sesObj.SendMsg(buf, size);
     EXPECT_FALSE(retResult);
 }
@@ -106,7 +101,7 @@ HWTEST_F(UDSSessionTest, SendMsg_type2_001, TestSize.Level1)
     int32_t fd = -1;
     NetPacket newPacket(MmiMessageId::INVALID);
 
-    UDSSession sesObj(programName_, moduleType_, fd, UID_ROOT, pid_);
+    UDSSession sesObj(programName_, moduleType_, fd);
     bool retResult = sesObj.SendMsg(newPacket);
     EXPECT_FALSE(retResult);
 }
@@ -115,7 +110,7 @@ HWTEST_F(UDSSessionTest, SendMsg_type2_002, TestSize.Level1)
 {
     NetPacket newPacket(MmiMessageId::BEGIN);
 
-    UDSSession sesObj(programName_, moduleType_, fd_, UID_ROOT, pid_);
+    UDSSession sesObj(programName_, moduleType_, fd_);
     bool retResult = sesObj.SendMsg(newPacket);
     EXPECT_FALSE(retResult);
 }
@@ -125,7 +120,7 @@ HWTEST_F(UDSSessionTest, SendMsg_type2_003, TestSize.Level1)
     int32_t fd = -65535;
     NetPacket newPacket(MmiMessageId::BEGIN);
 
-    UDSSession sesObj(programName_, moduleType_, fd, UID_ROOT, pid_);
+    UDSSession sesObj(programName_, moduleType_, fd);
     bool retResult = sesObj.SendMsg(newPacket);
     EXPECT_FALSE(retResult);
 }
