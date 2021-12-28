5ee4d922ad7d9087f983d04ad916417fa168e4e2
mayunteng_1 <mayunteng@huawei.com>
2021-12-28 17:01:15 +0800

    Signed-off-by:mayunteng_1<mayunteng@huawei.com>
    Signed-off-by: mayunteng_1 <mayunteng@huawei.com>

--- service/event_package/src/event_package.cpp
+++ service/event_package/src/event_package.cpp
@@ -537,7 +537,7 @@ int32_t EventPackage::PackageTouchEvent(multimodal_libinput_event &ev,
             break;
         }
         case LIBINPUT_EVENT_TOUCH_UP: {
-            MMIRegEvent->GetTouchInfoByTouchId(touch, MAKEPAIR(touch, deviceId, touch.seat_slot));
+            MMIRegEvent->GetTouchInfoByTouchId(touch, MAKEPAIR(touch.deviceId, touch.seat_slot));
             touch.time = libinput_event_touch_get_time_usec(data);
             touch.eventType = LIBINPUT_EVENT_TOUCH_UP;
             break;
