d2aaf544c102d2fffca71f5ade1ba23fcb61f373
baiwei <baiwei28@huawei.com>
2022-01-15 08:07:33 +0800

    处理编译报错
    Signed-off-by: baiwei <baiwei28@huawei.com>

--- service/event_dispatch/src/event_dispatch.cpp
+++ service/event_dispatch/src/event_dispatch.cpp
@@ -136,7 +136,7 @@ int32_t OHOS::MMI::EventDispatch::DispatchRegEvent(const MmiMessageId& idMsg, OH
     std::vector<int32_t> fds;
     RegEventHM->FindSocketFdsByEventHandle(idMsg, fds);
     if (fds.empty()) {
-        MMI_LOGW("Yet none of socketFds is found!\n")
+        MMI_LOGW("Yet none of socketFds is found!\n");
         return RET_OK;
     }
 
