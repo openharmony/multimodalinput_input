e6e0472f24f2229afaaba25c728dd1a333c32815
zhaoqil <zhaoqi70@huawei.com>
2022-01-15 20:22:32 +0800

    修改检查指针宏定义 Signed-off-by: zhaoqil <zhaoqi70@huawei.com>

--- common/include/define_multimodal.h
+++ common/include/define_multimodal.h
@@ -74,64 +74,106 @@
 #endif
 
 #ifdef DEBUG_CODE_TEST
-#define CK(msg, ec) \
-    if (!(msg)) { \
-        MMI_LOGE("%{public}s(%{public}d): CK(%{public}s) errCode:%{public}d", __FILE__, __LINE__, #msg, ec); \
-    }
+#define CK(cond, ec) \
+    do { \
+        if (!(cond)) { \
+            MMI_LOGE("%{public}s, (%{public}d), CK(%{public}s), errCode:%{public}d",
+                __FILE__, __LINE__, #cond, ec); \
+        } \
+    } while (0)
 
-#define CHK(msg, ec) \
-    if (!(msg)) { \
-        MMI_LOGE("%{public}s(%{public}d): CHK(%{public}s) errCode:%{public}d", __FILE__, __LINE__, #msg, ec); \
-        return; \
-    }
+#define CHK(cond, ec) \
+    do { \
+        if (!(cond)) { \
+            MMI_LOGE("%{public}s, (%{public}d), CHK(%{public}s), errCode:%{public}d",
+                __FILE__, __LINE__, #cond, ec); \
+            return; \
+        } \
+    } while (0)
 
-#define CHKF(msg, ec) \
-    if (!(msg)) { \
-        MMI_LOGE("%{public}s(%{public}d): CHKF(%{public}s) errCode:%{public}d", __FILE__, __LINE__, #msg, ec); \
-        return 0; \
-    }
+#define CHKF(cond, ec) \
+    do { \
+        if (!(cond)) { \
+            MMI_LOGE("%{public}s, (%{public}d), CHKF(%{public}s), errCode:%{public}d",
+                __FILE__, __LINE__, #cond, ec); \
+            return 0; \
+        } \
+    } while (0)
 
-#define CHKC(msg, ec) \
-    if (!(msg)) { \
-        MMI_LOGE("%{public}s(%{public}d): CHKC(%{public}s) errCode:%{public}d", __FILE__, __LINE__, #msg, ec); \
-        continue; \
-    }
+#define CHKC(cond, ec) \
+    do { \
+        if (!(cond)) { \
+            MMI_LOGE("%{public}s, (%{public}d), CHKC(%{public}s), errCode:%{public}d",
+                __FILE__, __LINE__, #cond, ec); \
+            continue; \
+        } \
+    } while (0)
 
-#define CHKR(msg, ec, r) \
-    if (!(msg)) { \
-        MMI_LOGE("%{public}s(%{public}d): CHKR(%{public}s) errCode:%{public}d", __FILE__, __LINE__, #msg, ec); \
-        return r; \
-    }
+#define CHKR(cond, ec, r) \
+    do { \
+        if ((cond) == nullptr) { \
+            MMI_LOGE("%{public}s, (%{public}d), CHKR(%{public}s), errCode:%{public}d",
+                __FILE__, __LINE__, #cond, ec); \
+            return r; \
+        } \
+    } while (0)
+
+#define CHKB(cond, ec, r) \
+    do { \
+        if (!(cond)) { \
+            MMI_LOGE("%{public}s, (%{public}d), CHKB(%{public}s), errCode:%{public}d",
+                __FILE__, __LINE__, #cond, ec); \
+            return r; \
+        } \
+    } while (0)
+
 #else
-#define CK(msg, ec) \
-    if (!(msg)) { \
-        MMI_LOGE("CK(%{public}s) errCode:%{public}d", #msg, ec); \
-    }
+#define CK(cond, ec) \
+    do { \
+        if (!(cond)) { \
+            MMI_LOGE("CK(%{public}s), errCode:%{public}d", #cond, ec); \
+        } \
+    } while (0)
 
-#define CHK(msg, ec) \
-    if (!(msg)) { \
-        MMI_LOGE("CHK(%{public}s) errCode:%{public}d", #msg, ec); \
-        return; \
-    }
+#define CHK(cond, ec) \
+    do { \
+        if (!(cond)) { \
+            MMI_LOGE("CHK(%{public}s), errCode:%{public}d", #cond, ec); \
+            return; \
+        } \
+    } while (0)
 
-#define CHKF(msg, ec) \
-    if (!(msg)) { \
-        MMI_LOGE("CHKF(%{public}s) errCode:%{public}d", #msg, ec); \
-        return 0; \
-    }
+#define CHKF(cond, ec) \
+    do { \
+        if (!(cond)) { \
+            MMI_LOGE("CHKF(%{public}s), errCode:%{public}d", #cond, ec); \
+            return 0; \
+        } \
+    } while (0)
 
-#define CHKC(msg, ec) \
-    if (!(msg)) { \
-        MMI_LOGE("CHKC(%{public}s) errCode:%{public}d", #msg, ec); \
-        continue; \
-    }
+#define CHKC(cond, ec) \
+    do { \
+        if (!(cond)) { \
+            MMI_LOGE("CHKC(%{public}s), errCode:%{public}d", #cond, ec); \
+            continue; \
+        } \
+    } while (0)
 
-#define CHKR(msg, ec, r) \
-    if (!(msg)) { \
-        MMI_LOGE("CHKR(%{public}s) errCode:%{public}d", #msg, ec); \
-        return r; \
-    }
+#define CHKR(cond, ec, r) \
+    do { \
+        if ((cond) == nullptr) { \
+            MMI_LOGE("CHKR(%{public}s), errCode:%{public}d", #cond, ec); \
+            return r; \
+        } \
+    } while (0)
+
+#define CHKB(cond, ec, r) \
+    do { \
+        if (!(cond)) { \
+            MMI_LOGE("CHKB(%{public}s), errCode:%{public}d", #cond, ec); \
+            return r; \
+        } \
+    } while (0)
+
 #endif
-
-
 #endif
