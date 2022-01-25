3009d1e19c29e3f1634d6356b97010ac14e73633
baiwei <baiwei28@huawei.com>
2022-01-25 08:52:49 +0000

    update service/event_handle/src/input_event_handler.cpp. Signed-off-by: baiwei <baiwei28@huawei.com>

--- service/event_handle/src/input_event_handler.cpp
+++ service/event_handle/src/input_event_handler.cpp
@@ -733,7 +733,7 @@ int32_t InputEventHandler::OnGestureEvent(libinput_event *event)
     CHKR(udsServer_, ERROR_NULL_POINTER, RET_ERR);
 
     auto pointerEvent = EventPackage::LibinputEventToPointerEvent(event);
-    if (RET_OK == eventDispatch_.handlePointerEvent(pointerEvent)) {
+    if (RET_OK == eventDispatch_.HandlePointerEvent(pointerEvent)) {
         MMI_LOGD("interceptor of OnGestureEvent end .....");
         return RET_OK;
     }
