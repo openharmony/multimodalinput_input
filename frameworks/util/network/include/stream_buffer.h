7668442468828571c88e2fbc0b46f1d32c9c345d
bobz202 <zhangbo374@huawei.com>
2022-01-29 21:42:52 +0800

    流方式写入或读取数据增加错误状态 告警清理 Signed-off-by: bobz202 <zhangbo374@huawei.com>

--- frameworks/util/network/include/stream_buffer.h
+++ frameworks/util/network/include/stream_buffer.h
@@ -128,8 +128,8 @@ template<typename T>
 bool StreamBuffer::Read(T &data)
 {
     if (!Read(reinterpret_cast<char *>(&data), sizeof(data))) {
-        MMI_LOGE("[%{public}s] size:%{public}d count:%{public}d,errCode:%{public}d",
-            GetErrorStatusRemark().c_str(), static_cast<int32_t>(sizeof(data)), rCount_+1, STREAM_BUF_READ_FAIL);
+        MMI_LOGE("[%{public}s] size:%{public}zu count:%{public}d,errCode:%{public}d",
+            GetErrorStatusRemark().c_str(), sizeof(data), rCount_+1, STREAM_BUF_READ_FAIL);
         return false;
     }
     return true;
@@ -139,8 +139,8 @@ template<typename T>
 bool StreamBuffer::Write(const T &data)
 {
     if (!Write(reinterpret_cast<char *>(const_cast<T *>(&data)), sizeof(data))) {
-        MMI_LOGE("[%{public}s] size:%{public}d,count:%{public}d,errCode:%{public}d",
-            GetErrorStatusRemark().c_str(), static_cast<int32_t>(sizeof(data)), wCount_+1, STREAM_BUF_WRITE_FAIL);
+        MMI_LOGE("[%{public}s] size:%{public}zu,count:%{public}d,errCode:%{public}d",
+            GetErrorStatusRemark().c_str(), sizeof(data), wCount_+1, STREAM_BUF_WRITE_FAIL);
         return false;
     }
     return true;
