abb2652e6070a3327f67cdfa2a2bd28d71012acf
ywx1116641 <yangzhou42@huawei.com>
2022-02-14 10:00:21 +0800

    修改函数入参 Signed-off-by: ywx1116641 <yangzhou42@huawei.com>

--- frameworks/proxy/event_handle/src/input_manager_impl.cpp
+++ frameworks/proxy/event_handle/src/input_manager_impl.cpp
@@ -247,7 +247,7 @@ void InputManagerImpl::PrintDisplayDebugInfo()
 
 int32_t InputManagerImpl::AddMonitor(std::function<void(std::shared_ptr<KeyEvent>)> monitor)
 {
-    CHKPR(monitor, ERROR_NULL_POINTER, InputEventMonitorManager::INVALID_MONITOR_ID);
+    CHKPR(monitor, ERROR_NULL_POINTER);
     int32_t monitorId = InputMonitorMgr.AddInputEventMontior(monitor);
     monitorId = monitorId * ADD_MASK_BASE + MASK_KEY;
     return monitorId;
@@ -255,7 +255,7 @@ int32_t InputManagerImpl::AddMonitor(std::function<void(std::shared_ptr<KeyEvent
 
 int32_t InputManagerImpl::AddMontior(std::function<void(std::shared_ptr<PointerEvent>)> monitor)
 {
-    CHKPR(monitor, ERROR_NULL_POINTER, InputEventMonitorManager::INVALID_MONITOR_ID);
+    CHKPR(monitor, ERROR_NULL_POINTER);
     std::shared_ptr<IInputEventConsumer> consumer =
         std::make_shared<PublicIInputEventConsumer>(monitor);
     return InputManagerImpl::AddMonitor(consumer);
@@ -263,7 +263,7 @@ int32_t InputManagerImpl::AddMontior(std::function<void(std::shared_ptr<PointerE
 
 int32_t InputManagerImpl::AddMonitor(std::shared_ptr<IInputEventConsumer> consumer)
 {
-    CHKPR(consumer, ERROR_NULL_POINTER, InputEventMonitorManager::INVALID_MONITOR_ID);
+    CHKPR(consumer, ERROR_NULL_POINTER);
     int32_t monitorId = InputMonitorManager::GetInstance().AddMonitor(consumer);
     monitorId = monitorId * ADD_MASK_BASE + MASK_TOUCH;
     return monitorId;
