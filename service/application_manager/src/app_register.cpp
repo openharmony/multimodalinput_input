6a59f0e06cf6d0389d5b91e299056a2eb22a6f49
ywx1116641 <yangzhou42@huawei.com>
2022-02-18 15:49:14 +0800

    删除多余的打点日志
    Signed-off-by: ywx1116641 <yangzhou42@huawei.com>

--- service/application_manager/src/app_register.cpp
+++ service/application_manager/src/app_register.cpp
@@ -36,7 +36,6 @@ AppRegister::~AppRegister()
 
 bool AppRegister::Init(UDSServer& udsServer)
 {
-    MMI_LOGD("enter");
     surfaceInfo_.clear();
     waitQueue_.clear();
     connectState_.clear();
@@ -44,25 +43,21 @@ bool AppRegister::Init(UDSServer& udsServer)
         mu_.unlock();
     }
     udsServer_ = &udsServer;
-    MMI_LOGD("leave");
     return true;
 }
 
 const AppInfo& AppRegister::FindByWinId(int32_t windowId)
 {
-    MMI_LOGD("enter");
     std::lock_guard<std::mutex> lock(mu_);
     auto it = surfaceInfo_.find(windowId);
     if (it != surfaceInfo_.end()) {
         return it->second;
     }
-    MMI_LOGD("leave");
     return AppRegister::appInfoError_;
 }
 
 const AppInfo& AppRegister::FindBySocketFd(int32_t fd)
 {
-    MMI_LOGD("enter");
     std::lock_guard<std::mutex> lock(mu_);
     CHKR(fd >= 0, PARAM_INPUT_INVALID, appInfoError_);
     for (const auto &item : surfaceInfo_) {
@@ -70,31 +65,25 @@ const AppInfo& AppRegister::FindBySocketFd(int32_t fd)
             return item.second;
         }
     }
-    MMI_LOGD("leave");
     return AppRegister::appInfoError_;
 }
 
 void AppRegister::RegisterAppInfoforServer(const AppInfo& appInfo)
 {
-    MMI_LOGD("enter");
     std::lock_guard<std::mutex> lock(mu_);
     surfaceInfo_.insert(std::pair<int32_t, AppInfo>(appInfo.windowId, appInfo));
     AddId(fds_, appInfo.fd);
-    MMI_LOGD("leave");
 }
 
 void AppRegister::UnregisterAppInfoBySocketFd(int32_t fd)
 {
-    MMI_LOGD("enter");
     std::lock_guard<std::mutex> lock(mu_);
     CHK(fd >= 0, PARAM_INPUT_INVALID);
     UnregisterBySocketFd(fd);
-    MMI_LOGD("leave");
 }
 
 void AppRegister::UnregisterBySocketFd(int32_t fd)
 {
-    MMI_LOGD("enter");
     auto it = surfaceInfo_.begin();
     while (it != surfaceInfo_.end()) {
         if (it->second.fd == fd) {
@@ -103,18 +92,15 @@ void AppRegister::UnregisterBySocketFd(int32_t fd)
             it++;
         }
     }
-    MMI_LOGD("leave");
 }
 
 std::map<int32_t, AppInfo>::iterator AppRegister::EraseAppInfo(const std::map<int32_t, AppInfo>::iterator &it)
 {
-    MMI_LOGD("enter");
     return surfaceInfo_.erase(it);
 }
 
 std::map<int32_t, AppInfo>::iterator AppRegister::UnregisterAppInfo(int32_t winId)
 {
-    MMI_LOGD("enter");
     if (winId <= 0) {
         return surfaceInfo_.end();
     }
@@ -122,48 +108,39 @@ std::map<int32_t, AppInfo>::iterator AppRegister::UnregisterAppInfo(int32_t winI
     if (itr == surfaceInfo_.end()) {
         return surfaceInfo_.end();
     }
-    MMI_LOGD("leave");
     return EraseAppInfo(itr);
 }
 
 void AppRegister::PrintfMap()
 {
-    MMI_LOGD("enter");
     std::lock_guard<std::mutex> lock(mu_);
     for (const auto &item : surfaceInfo_) {
         std::cout << "mapSurface " << item.second.abilityId << ", " << item.second.windowId <<
             ", " << item.second.fd << std::endl;
     }
-    MMI_LOGD("leave");
 }
 
 void OHOS::MMI::AppRegister::Dump(int32_t fd)
 {
-    MMI_LOGD("enter");
     std::lock_guard<std::mutex> lock(mu_);
     mprintf(fd, "AppInfos: count=%d", surfaceInfo_.size());
     for (const auto &item : surfaceInfo_) {
         mprintf(fd, "\tabilityId=%d windowId=%d fd=%d bundlerName=%s appName=%s", item.second.abilityId,
                 item.second.windowId, item.second.fd, item.second.bundlerName.c_str(), item.second.appName.c_str());
     }
-    MMI_LOGD("leave");
 }
 
 void AppRegister::SurfacesDestroyed(const std::vector<int32_t> &desList)
 {
-    MMI_LOGD("enter");
     std::lock_guard<std::mutex> lock(mu_);
     for (const auto &item : desList) {
         UnregisterAppInfo(item);
     }
-    MMI_LOGD("leave");
 }
 
 int32_t AppRegister::QueryMapSurfaceNum()
 {
-    MMI_LOGD("enter");
     std::lock_guard<std::mutex> lock(mu_);
-    MMI_LOGD("leave");
     return static_cast<int32_t>(surfaceInfo_.size());
 }
 
@@ -200,7 +177,6 @@ bool AppRegister::IsMultimodeInputReady(MmiMessageId idMsg, const int32_t findFd
 
 WaitQueueEvent AppRegister::GetWaitQueueEvent(int32_t fd, int32_t idMsg)
 {
-    MMI_LOGD("enter");
     std::lock_guard<std::mutex> lock(mu_);
     auto find_fun = [fd, idMsg](const WaitQueueEvent &ev) -> bool {
         if (fd != ev.fd || idMsg != ev.event) {
@@ -213,36 +189,30 @@ WaitQueueEvent AppRegister::GetWaitQueueEvent(int32_t fd, int32_t idMsg)
     if (it == waitQueue_.end()) {
         return faultData;
     }
-    MMI_LOGD("leave");
     return *it;
 }
 
 bool AppRegister::CheckFindFdError(const int32_t findFd)
 {
-    MMI_LOGD("enter");
     if (findFd < 0) {
         MMI_LOGE(" IsMultimodeInputReady: Find fd error, errCode:%{public}d", FD_FIND_FAIL);
         return false;
     }
-    MMI_LOGD("leave");
     return true;
 }
 
 bool AppRegister::CheckConnectionIsDead(const int32_t findFd)
 {
-    MMI_LOGD("enter");
     if (connectState_.find(findFd) == connectState_.end()) {
         MMI_LOGE("IsMultimodeInputReady: The connection is dead! fd:%{public}d, errCode:%{public}d",
                  findFd, CONN_BREAK);
         return false;
     }
-    MMI_LOGD("leave");
     return true;
 }
 
 bool AppRegister::CheckWaitQueueBlock(ssize_t currentTime, ssize_t timeOut, const int32_t findFd)
 {
-    MMI_LOGD("enter");
     for (auto iter = waitQueue_.begin(); iter != waitQueue_.end(); iter++) {
         if (findFd == iter->fd) {
             if (currentTime >= (iter->serverTime + timeOut)) {
@@ -253,13 +223,11 @@ bool AppRegister::CheckWaitQueueBlock(ssize_t currentTime, ssize_t timeOut, cons
             }
         }
     }
-    MMI_LOGD("leave");
     return true;
 }
 
 void AppRegister::DeleteEventFromWaitQueue(int32_t fd, int32_t idMsg)
 {
-    MMI_LOGD("enter");
     std::lock_guard<std::mutex> lock(mu_);
     CHK(fd >= 0, PARAM_INPUT_INVALID);
     for (auto iter = waitQueue_.begin(); iter != waitQueue_.end(); iter++) {
@@ -268,30 +236,24 @@ void AppRegister::DeleteEventFromWaitQueue(int32_t fd, int32_t idMsg)
             break;
         }
     }
-    MMI_LOGD("leave");
 }
 
 bool AppRegister::OnAnrLocked(int32_t fd) const
 {
-    MMI_LOGD("enter");
     MMI_LOGE("Dispatch Timeout, Application Not Responding. fd:%{public}d,errCode:%{public}d",
              fd, APP_NOT_RESP);
-    MMI_LOGD("leave");
     return true;
 }
 
 void AppRegister::RegisterConnectState(int32_t fd)
 {
-    MMI_LOGD("enter");
     CHK(fd >= 0, PARAM_INPUT_INVALID);
     std::lock_guard<std::mutex> lock(mu_);
     connectState_.insert(std::pair<int32_t, int8_t>(fd, 0));
-    MMI_LOGD("leave");
 }
 
 void AppRegister::UnregisterConnectState(int32_t fd)
 {
-    MMI_LOGD("enter");
     CHK(fd >= 0, PARAM_INPUT_INVALID);
     std::lock_guard<std::mutex> lock(mu_);
 
@@ -299,7 +261,6 @@ void AppRegister::UnregisterConnectState(int32_t fd)
     if (iter != connectState_.end()) {
         connectState_.erase(iter);
     }
-    MMI_LOGD("leave");
 }
 } // namespace MMI
 } // namespace OHOS
