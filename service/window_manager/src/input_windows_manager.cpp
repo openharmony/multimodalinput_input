a0b3d92f4cd27142de720cfd1f5b15d30a19d98c
lifenghui <lifenghui3@huawei.com>
2022-02-18 09:53:32 +0800

    日志中displayid删除id
    Signed-off-by: lifenghui <lifenghui3@huawei.com>

--- service/window_manager/src/input_windows_manager.cpp
+++ service/window_manager/src/input_windows_manager.cpp
@@ -712,7 +712,7 @@ bool OHOS::MMI::InputWindowsManager::TouchMotionPointToDisplayPoint(libinput_eve
 
     for (const auto &display : logicalDisplays_) {
         if (targetDisplayId == display.id ) {
-            MMI_LOGD("targetDisplayId is %{public}d, displayX is %{public}d, displayY is %{public}d ",
+            MMI_LOGD("targetDisplay is %{public}d, displayX is %{public}d, displayY is %{public}d ",
                 targetDisplayId, displayX, displayY);
             displayX = globalLogicalX - display.topLeftX;
             displayY = globalLogicalY - display.topLeftY;
@@ -746,7 +746,7 @@ bool OHOS::MMI::InputWindowsManager::TouchDownPointToDisplayPoint(libinput_event
         logicalDisplayId = display.id;
         logicalX = globalLogicalX - display.topLeftX;
         logicalY = globalLogicalY - display.topLeftY;
-        MMI_LOGD("targetDisplayId is %{public}d, displayX is %{public}d, displayY is %{public}d ",
+        MMI_LOGD("targetDisplay is %{public}d, displayX is %{public}d, displayY is %{public}d ",
             logicalDisplayId, logicalX, logicalY);
         return true;
     }
