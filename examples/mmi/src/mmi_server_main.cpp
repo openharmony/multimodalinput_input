13a72fb8296cf7835271ff82ec3a2ec95dfc29b3
li-yaoyao777 <liyaoyao7@huawei.com>
2022-02-24 12:54:18 +0800

    ddd
    Signed-off-by: li-yaoyao777 <liyaoyao7@huawei.com>

--- examples/mmi/src/mmi_server_main.cpp
+++ examples/mmi/src/mmi_server_main.cpp
@@ -19,7 +19,7 @@
 namespace OHOS {
 namespace MMI {
     namespace {
-    constexpr OHOS::HiviewDFX::HiLogLabel LABEL = { LOG_CORE, MMI_LOG_DOMAIN, "MmiServerDemo" }; //namespace
+    constexpr OHOS::HiviewDFX::HiLogLabel LABEL = { LOG_CORE, MMI_LOG_DOMAIN, "MmiServerDemo" }; // namespace
     }
 }
 }
