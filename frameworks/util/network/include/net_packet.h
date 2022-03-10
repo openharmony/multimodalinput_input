0e3646f49a18130f312f5a30464c778e708a8d3f
mayunteng_1 <mayunteng@huawei.com>
2022-03-10 07:01:04 +0000

    拉起ability代码检视修改
    Signed-off-by: mayunteng_1 <mayunteng@huawei.com>
    Change-Id: I900b00026b5802499f0a859b91971dcccc14689c

--- frameworks/util/network/include/net_packet.h
+++ frameworks/util/network/include/net_packet.h
@@ -28,7 +28,7 @@ using PACKHEAD = struct PackHead {
 namespace OHOS {
 namespace MMI {
 class NetPacket : public StreamBuffer {
-     static constexpr OHOS::HiviewDFX::HiLogLabel LABEL = { LOG_CORE, MMI_LOG_DOMAIN, "NetPacket" };
+    static constexpr OHOS::HiviewDFX::HiLogLabel LABEL = { LOG_CORE, MMI_LOG_DOMAIN, "NetPacket" };
 public:
     explicit NetPacket(MmiMessageId msgId);
     NetPacket(const NetPacket& pkt);
