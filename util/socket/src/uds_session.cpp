61c63a463c589cd6965155c881364d0c9a73f47a
xueyuanzhao <zhaoxueyuan@huawei.com>
2022-03-28 01:52:26 +0000

    Signed-off-by:xueyuanzhao<zhaoxueyuan@huawei.com>
    Signed-off-by: xueyuanzhao <zhaoxueyuan@huawei.com>
    Change-Id: I7ffa577c4e5692164e116d559ac3452719561c5a

--- util/socket/src/uds_session.cpp
+++ util/socket/src/uds_session.cpp
@@ -54,14 +54,31 @@ bool UDSSession::SendMsg(const char *buf, size_t size) const
         MMI_HILOGE("fd_ is less than 0");
         return false;
     }
-    ssize_t ret = write(fd_, static_cast<void *>(const_cast<char *>(buf)), size);
-    if (ret < 0) {
-        MMI_HILOGE("write return %{public}zd,"
-                   "fd_:%{public}d,errno:%{public}d",
-                   ret, fd_, errno);
-        return false;
+
+    int32_t retryTimes = 32;
+    while (size > 0 && retryTimes > 0) {
+        retryTimes--;
+        auto count = send(fd_, static_cast<void *>(const_cast<char *>(buf)), size, MSG_DONTWAIT | MSG_NOSIGNAL);
+        if (count < 0) {
+            if (errno == EAGAIN || errno == EINTR || errno == EWOULDBLOCK) {
+                MMI_HILOGW("send msg failed, errno:%{public}d", errno);
+                continue;
+            }
+            MMI_HILOGE("Send return failed,error:%{public}d fd:%{public}d", errno, fd_);
+            return false;
+        }
+
+        size_t ucount = static_cast<size_t>(count);
+        if (ucount >= size) {
+            return true;
+        }
+        size -= ucount;
+        buf += ucount;
+        int32_t sleepTime = 10000;
+        usleep(sleepTime);
     }
-    return true;
+    MMI_HILOGE("send msg failed");
+    return false;
 }
 
 void UDSSession::Close()
