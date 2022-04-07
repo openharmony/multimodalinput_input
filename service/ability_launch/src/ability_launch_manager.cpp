4c4c4e350d784a785767628808f7cb096058ada6
mayunteng_1 <mayunteng@huawei.com>
2022-04-07 01:50:07 +0000

    codedex modify
    Signed-off-by: mayunteng_1 <mayunteng@huawei.com>
    Change-Id: I7b99ce2631370b4526aaaa54a3bc383e9f6fb09e

--- service/ability_launch/src/ability_launch_manager.cpp
+++ service/ability_launch/src/ability_launch_manager.cpp
@@ -276,7 +276,7 @@ bool AbilityLaunchManager::HandleKeyDown(ShortcutKey &shortcutKey)
         MMI_HILOGE("Timer add failed");
         return false;
     }
-    MMI_HILOGD("add timer success, timeid:%{public}d", shortcutKey.timerId);
+    MMI_HILOGD("add timer success");
     lastMatchedKey_ = shortcutKey;
     return true;
 }
