d7738b10182e85307cb3d5455749c96085028432
zhxiap <xiaozhuohan@huawei.com>
2022-04-14 10:30:35 +0800

    鼠标扩展按键翻译完成,申请合入
    Signed-off-by: zhxiap <xiaozhuohan@huawei.com>

--- interfaces/native/innerkits/event/include/pointer_event.h
+++ interfaces/native/innerkits/event/include/pointer_event.h
@@ -198,36 +198,36 @@ public:
      */
     static constexpr int32_t MOUSE_BUTTON_MIDDLE = 2;
 
-    /**
-     * @brief 表示鼠标SIDE按钮.
+     /**
+     * Indicates the side button on a mouse.
      *
      * @since 9
      */
     static constexpr int32_t MOUSE_BUTTON_SIDE = 3;
 
     /**
-     * @brief 表示鼠标EXTRA按钮.
+     * Indicates the extra button on a mouse.
      *
      * @since 9
      */
     static constexpr int32_t MOUSE_BUTTON_EXTRA = 4;
 
     /**
-     * @brief 表示鼠标FORWARD按钮.
+     * Indicates the forward button on a mouse.
      *
      * @since 9
      */
     static constexpr int32_t MOUSE_BUTTON_FORWARD = 5;
 
     /**
-     * @brief 表示鼠标BACK按钮.
+     * Indicates the back button on a mouse.
      *
      * @since 9
      */
     static constexpr int32_t MOUSE_BUTTON_BACK = 6;
 
     /**
-     * @brief 表示鼠标TASK按钮.
+     * Indicates the task button on a mouse.
      *
      * @since 9
      */
