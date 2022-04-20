16708ab2fa45a431a4631c44f8715e0353176e41
guwenjin <guwenjin2@huawei.com>
2022-04-20 18:22:55 +0800

    客户端发送消息不走monitorManager
    Signed-off-by: guwenjin <guwenjin2@huawei.com>

--- frameworks/proxy/event_handler/src/input_manager_impl.cpp
+++ frameworks/proxy/event_handler/src/input_manager_impl.cpp
@@ -460,10 +460,12 @@ void InputManagerImpl::MoveMouse(int32_t offsetX, int32_t offsetY)
 {
     std::lock_guard<std::mutex> guard(mtx_);
     if (!MMIEventHdl.StartClient()) {
-        MMI_HILOGE("get mmi client is nullptr");
+        MMI_HILOGE("client init failed");
         return;
     }
-    monitorManager_.MoveMouse(offsetX, offsetY);
+    if (MMIEventHdl.MoveMouseEvent(offsetX, offsetY) != RET_OK) {
+        MMI_HILOGE("Failed to inject movemove offset event");
+    }
 }
 
 int32_t InputManagerImpl::AddInterceptor(std::shared_ptr<IInputEventConsumer> interceptor)
