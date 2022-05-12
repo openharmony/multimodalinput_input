c286d44f7821829ddc0009752c8c95f079bd0868
guwenjin <guwenjin2@huawei.com>
2022-05-12 15:39:55 +0800

    接口文件还原
    Signed-off-by: guwenjin <guwenjin2@huawei.com>

--- frameworks/proxy/event_handler/include/input_manager_impl.h
+++ frameworks/proxy/event_handler/include/input_manager_impl.h
@@ -56,9 +56,9 @@ public:
     void OnPointerEvent(std::shared_ptr<PointerEvent> pointerEvent);
     int32_t PackDisplayData(NetPacket &pkt);
 
-    int32_t AddMonitor(std::function<void(std::shared_ptr<KeyEvent>)> interceptor);
-    int32_t AddMonitor(std::function<void(std::shared_ptr<PointerEvent>)> interceptor);
-    int32_t AddMonitor(std::shared_ptr<IInputEventConsumer> interceptor);
+    int32_t AddMonitor(std::function<void(std::shared_ptr<KeyEvent>)> monitor);
+    int32_t AddMonitor(std::function<void(std::shared_ptr<PointerEvent>)> monitor);
+    int32_t AddMonitor(std::shared_ptr<IInputEventConsumer> consumer);
 
     void RemoveMonitor(int32_t monitorId);
     void MarkConsumed(int32_t monitorId, int32_t eventId);
