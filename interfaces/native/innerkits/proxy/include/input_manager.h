ea05bec4a938e443c8160071c777d4041deb66a4
guwenjin <guwenjin2@huawei.com>
2022-05-13 13:20:55 +0800

    单例初始化方式还原
    Signed-off-by: guwenjin <guwenjin2@huawei.com>

--- interfaces/native/innerkits/proxy/include/input_manager.h
+++ interfaces/native/innerkits/proxy/include/input_manager.h
@@ -232,7 +232,7 @@ public:
 private:
     InputManager() = default;
     DISALLOW_COPY_AND_MOVE(InputManager);
-    inline static InputManager *instance_ = nullptr;
+    static InputManager *instance_;
 };
 } // namespace MMI
 } // namespace OHOS
