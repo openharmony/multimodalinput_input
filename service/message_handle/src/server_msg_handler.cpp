b9dd8c70d2435ce9fc7a0bec834be4d4a933724b
liuwei <liuwei595@huawei.com>
2022-05-13 09:17:09 +0000

    delete unused interface
    Signed-off-by: liuwei <liuwei595@huawei.com>
    Change-Id: I7df8963fe4e6c4a1727a7b7187f5e65d1166827a

--- service/message_handle/src/server_msg_handler.cpp
+++ service/message_handle/src/server_msg_handler.cpp
@@ -55,7 +55,6 @@ void ServerMsgHandler::Init(UDSServer& udsServer)
     }
 #endif
     MsgCallback funs[] = {
-        {MmiMessageId::ON_VIRTUAL_KEY, MsgCallbackBind2(&ServerMsgHandler::OnVirtualKeyEvent, this)},
         {MmiMessageId::MARK_PROCESS,
             MsgCallbackBind2(&ServerMsgHandler::MarkProcessed, this)},
         {MmiMessageId::ON_DUMP, MsgCallbackBind2(&ServerMsgHandler::OnDump, this)},
@@ -129,24 +128,6 @@ int32_t ServerMsgHandler::OnHdiInject(SessionPtr sess, NetPacket& pkt)
     return RET_OK;
 }
 #endif // OHOS_BUILD_HDF
-
-int32_t ServerMsgHandler::OnVirtualKeyEvent(SessionPtr sess, NetPacket& pkt)
-{
-    VirtualKey virtualKeyEvent;
-    pkt >> virtualKeyEvent;
-    if (pkt.ChkRWError()) {
-        MMI_HILOGE("Packet read virtualKeyEvent failed");
-        return PACKET_READ_FAIL;
-    }
-    if (virtualKeyEvent.keyCode == HOS_KEY_HOME) {
-        MMI_HILOGD(" home press");
-    } else if (virtualKeyEvent.keyCode == HOS_KEY_BACK) {
-        MMI_HILOGD(" back press");
-    } else if (virtualKeyEvent.keyCode == HOS_KEY_VIRTUAL_MULTITASK) {
-        MMI_HILOGD(" multitask press");
-    }
-    return RET_OK;
-}
 
 int32_t ServerMsgHandler::OnDump(SessionPtr sess, NetPacket& pkt)
 {
