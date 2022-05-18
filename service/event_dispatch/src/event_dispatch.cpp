e571eda5991ceca34ad70f12a635be00b0829119
mayunteng_1 <mayunteng@huawei.com>
2022-05-18 02:48:31 +0000

    delete anr
    Signed-off-by: mayunteng_1 <mayunteng@huawei.com>
    Change-Id: Id1baf447365f2ad194998f559af530d60aa98234

--- service/event_dispatch/src/event_dispatch.cpp
+++ service/event_dispatch/src/event_dispatch.cpp
@@ -129,25 +129,10 @@ int32_t EventDispatch::HandlePointerEvent(std::shared_ptr<PointerEvent> point)
         return RET_ERR;
     }
 
-    auto session = udsServer->GetSession(fd);
-    CHKPF(session);
-    if (session->isANRProcess_) {
-        MMI_HILOGD("application not responsing");
-        return RET_OK;
-    }
-
-    auto currentTime = GetSysClockTime();
-    if (TriggerANR(currentTime, session)) {
-        session->isANRProcess_ = true;
-        MMI_HILOGW("the pointer event does not report normally, application not response");
-        return RET_OK;
-    }
-
     if (!udsServer->SendMsg(fd, pkt)) {
         MMI_HILOGE("Sending structure of EventTouch failed! errCode:%{public}d", MSG_SEND_FAIL);
         return RET_ERR;
     }
-    session->AddEvent(point->GetId(), currentTime);
     return RET_OK;
 }
 
@@ -187,19 +172,6 @@ int32_t EventDispatch::DispatchKeyEventPid(UDSServer& udsServer, std::shared_ptr
                key->GetEventType(),
                key->GetFlag(), key->GetKeyAction(), fd);
     InputHandlerManagerGlobal::GetInstance().HandleEvent(key);
-    auto session = udsServer.GetSession(fd);
-    CHKPF(session);
-    if (session->isANRProcess_) {
-        MMI_HILOGD("is ANR process");
-        return RET_OK;
-    }
-
-    auto currentTime = GetSysClockTime();
-    if (TriggerANR(currentTime, session)) {
-        session->isANRProcess_ = true;
-        MMI_HILOGW("the key event does not report normally, triggering ANR");
-        return RET_OK;
-    }
 
     NetPacket pkt(MmiMessageId::ON_KEYEVENT);
     InputEventDataTransformation::KeyEventToNetPacket(key, pkt);
@@ -213,7 +185,6 @@ int32_t EventDispatch::DispatchKeyEventPid(UDSServer& udsServer, std::shared_ptr
         MMI_HILOGE("Sending structure of EventKeyboard failed! errCode:%{public}d", MSG_SEND_FAIL);
         return MSG_SEND_FAIL;
     }
-    session->AddEvent(key->GetId(), currentTime);
     return RET_OK;
 }
 
