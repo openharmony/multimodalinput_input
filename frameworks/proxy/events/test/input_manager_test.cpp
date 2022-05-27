baab718f19f347a15bdcdc82f81be91a10c3d5d0
caoxiaosong <caoxiaosong@huawei.com>
2022-05-27 07:50:29 +0000

    支持通过C++ API查询键盘类型
    Signed-off-by: caoxiaosong <caoxiaosong@huawei.com>
    Change-Id: I029059c8ee9527fa2482aea2b928806e8c27479d

--- frameworks/proxy/events/test/input_manager_test.cpp
+++ frameworks/proxy/events/test/input_manager_test.cpp
@@ -2760,38 +2760,33 @@ HWTEST_F(InputManagerTest, InputManagerTest_MoveMouse_02, TestSize.Level1)
     InputManager::GetInstance()->MoveMouse(-1000, 100);
     std::vector<std::string> tLogs { SearchLog(command, sLogs) };
 }
-int32_t deviceIDtest = 0;
+
+static int32_t deviceIDtest = 0;
 static void GetKeyboardTypeCallback(int32_t keyboardType)
 {
     switch (keyboardType) {
         case KEYBOARD_TYPE_NONE: {
-            MMI_HILOGD("IdAndType: deviceID:%{public}d-->KeyboardType: %{public}s, keyboardType:%{public}d",
-                       deviceIDtest, "None", keyboardType);
+            MMI_HILOGD("deviceIDtest:%{public}d-->KeyboardType: %{public}s", deviceIDtest, "None");
             break;
             }
         case KEYBOARD_TYPE_UNKNOWN: {
-            MMI_HILOGD("IdAndType: deviceID:%{public}d-->KeyboardType: %{public}s, keyboardType:%{public}d",
-                       deviceIDtest, "unknown", keyboardType);
+            MMI_HILOGD("deviceIDtest:%{public}d-->KeyboardType: %{public}s", deviceIDtest, "unknown");
             break;
         }
         case KEYBOARD_TYPE_ALPHABETICKEYBOARD: {
-            MMI_HILOGD("IdAndType: deviceID:%{public}d-->KeyboardType: %{public}s, keyboardType:%{public}d",
-                       deviceIDtest, "alphabetickeyboard", keyboardType);
+            MMI_HILOGD("deviceIDtest:%{public}d-->KeyboardType: %{public}s", deviceIDtest, "alphabetickeyboard");
             break;
         }
         case KEYBOARD_TYPE_DIGITALKEYBOARD: {
-            MMI_HILOGD("IdAndType: deviceID:%{public}d-->KeyboardType: %{public}s, keyboardType:%{public}d",
-                       deviceIDtest, "digitalkeyboard", keyboardType);
+            MMI_HILOGD("deviceIDtest:%{public}d-->KeyboardType: %{public}s", deviceIDtest, "digitalkeyboard");
             break;
         }
         case KEYBOARD_TYPE_HANDWRITINGPEN: {
-            MMI_HILOGD("IdAndType: deviceID:%{public}d-->KeyboardType: %{public}s, keyboardType:%{public}d",
-                       deviceIDtest, "handwritingpen", keyboardType);
+            MMI_HILOGD("deviceIDtest:%{public}d-->KeyboardType: %{public}s", deviceIDtest, "handwritingpen");
             break;
         }
         case KEYBOARD_TYPE_REMOTECONTROL: {
-            MMI_HILOGD("IdAndType: deviceID:%{public}d-->KeyboardType: %{public}s, keyboardType:%{public}d",
-                       deviceIDtest, "remotecontrol", keyboardType);
+            MMI_HILOGD("deviceIDtest:%{public}d-->KeyboardType: %{public}s", deviceIDtest, "remotecontrol");
             break;
         }
         default: {
@@ -2806,6 +2801,7 @@ static void GetKeyboardTypeCallback(int32_t keyboardType)
  * @tc.type: FUNC
  * @tc.require:
  */
+
 HWTEST_F(InputManagerTest, InputManagerTest_GetKeyboardType, TestSize.Level1)
 {
     MMI_HILOGD("Start InputManagerTest_GetKeyboardType");
@@ -2813,7 +2809,7 @@ HWTEST_F(InputManagerTest, InputManagerTest_GetKeyboardType, TestSize.Level1)
     {
         deviceIDtest = i;
         InputManager::GetInstance()->GetKeyboardType(deviceIDtest, GetKeyboardTypeCallback);
-        MMI_HILOGD("i:%{public}d, deviceIDtest:%{public}d", i, deviceIDtest);
+        MMI_HILOGD("i:%{public}d", i);
         std::this_thread::sleep_for(std::chrono::milliseconds(TIME_WAIT_FOR_LOG));
     }
     std::this_thread::sleep_for(std::chrono::milliseconds(TIME_WAIT_FOR_LOG));
