ee853aa7609c179c5bd18363c70d240537374330
张海峰 <zhanghaifeng45@huawei.com>
2022-06-01 14:16:36 +0000

    重复按键读取配置项修改
    Signed-off-by: 张海峰 <zhanghaifeng45@huawei.com>
    Change-Id: I68e9169d98d5f1004ce598a7d83cac512cf4d0c6

--- util/common/include/util.h
+++ util/common/include/util.h
@@ -67,6 +67,7 @@ std::string ReadJsonFile(const std::string &filePath);
 std::string ReadUinputToolFile(const std::string &filePath);
 int32_t ReadTomlFile(const std::string &filePath, DeviceConfig& devConf);
 int32_t ReadConfigFile(const std::string &realPath, DeviceConfig& devConf);
+int32_t ConfigItemSwitch(const std::string &configItem, const std::string &value, DeviceConfig& devConf);
 inline bool IsNum(const std::string &str)
 {
     std::istringstream sin(str);
