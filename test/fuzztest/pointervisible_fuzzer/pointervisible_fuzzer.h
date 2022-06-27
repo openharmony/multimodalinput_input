990e45e5e03c601740677071ca92d681ca9f5732
duanwenhao <duanwenhao2@huawei.com>
2022-06-27 09:29:39 +0000

    添加多模Fuzz用例
    Signed-off-by: duanwenhao <duanwenhao2@huawei.com>
    Change-Id: I1b68eb7f8ec19ae9fd777a5b62359f60c4f23287

--- test/fuzztest/pointervisible_fuzzer/pointervisible_fuzzer.h
+++ test/fuzztest/pointervisible_fuzzer/pointervisible_fuzzer.h
@@ -13,9 +13,9 @@
  * limitations under the License.
  */
 
-#ifndef ADDMONITOR_FUZZER_H
-#define ADDMONITOR_FUZZER_H
+#ifndef POINTERVISIBLE_FUZZER_H
+#define POINTERVISIBLE_FUZZER_H
 
-#define FUZZ_PROJECT_NAME "addmonitor_fuzzer"
+#define FUZZ_PROJECT_NAME "pointervisible_fuzzer"
 
-#endif // ADDMONITOR_FUZZER_H
+#endif // POINTERVISIBLE_FUZZER_H
