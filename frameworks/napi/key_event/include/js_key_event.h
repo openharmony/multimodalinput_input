338691415233d9f0a49a09d26ea22401b57edae1
hungry_feiwei <huxiao31@huawei.com>
2022-06-30 21:50:41 +0800

    add enum
    Signed-off-by: hungry_feiwei <huxiao31@huawei.com>
    Change-Id: Ic427e4f30716089710fe81e930a8e58d73c8de55

---
+++ frameworks/napi/key_event/include/js_key_event.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef JS_KEY_EVENT
+#define JS_KEY_EVENT
+
+#include "napi/native_api.h"
+#include "napi/native_node_api.h"
+#include "nocopyable.h"
+
+namespace OHOS {
+namespace MMI {
+class JsKeyEvent {
+public:
+    JsKeyEvent() = default;
+    ~JsKeyEvent() = default;
+    DISALLOW_COPY_AND_MOVE(JsKeyEvent);
+    static napi_value Export(napi_env env, napi_value exports);
+private:
+    static napi_value GetNapiInt32(napi_env env, int32_t code);
+    static napi_value EnumClassConstructor(napi_env env, napi_callback_info info);
+};
+} // namespace MMI
+} // namespace OHOS
+
+#endif // JS_KEY_EVENT
