61d8fbb803477ec770ab6ec0dfd17b4566ef02d7
lilong <lilong121@huawei.com>
2022-07-05 14:26:03 +0800

    支持逻辑层注入鼠标双击输入事件功能：删除打印信息中多余的空格
    Signed-off-by: lilong <lilong121@huawei.com>

--- tools/inject_event/src/input_manager_command.cpp
+++ tools/inject_event/src/input_manager_command.cpp
@@ -356,7 +356,7 @@ int32_t InputManagerCommand::ParseCommand(int32_t argc, char *argv[])
                             if ((clickIntervalTimeMs < minClickIntervalTimeMs) ||
                                 (clickIntervalTimeMs > maxClickIntervalTimeMs)) {
                                 std::cout << "click interval time is out of range:" << minClickIntervalTimeMs << " ms"
-                                    " < " << clickIntervalTimeMs << " < " << maxClickIntervalTimeMs << " ms" 
+                                    " < " << clickIntervalTimeMs << " < " << maxClickIntervalTimeMs << " ms"
                                     << std::endl;
                                 return RET_ERR;
                             }
