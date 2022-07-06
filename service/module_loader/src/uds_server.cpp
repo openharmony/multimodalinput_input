81d61e250a1c68cdee7537b75b9c6e3c06cec872
zhaoqil <zhaoqi70@huawei.com>
2022-07-06 10:54:00 +0800

    删除废旧事件监听权限 Signed-off-by: zhaoqil <zhaoqi70@huawei.com>

--- service/module_loader/src/uds_server.cpp
+++ service/module_loader/src/uds_server.cpp
@@ -20,8 +20,6 @@
 
 #include <sys/socket.h>
 
-#include "accesstoken_kit.h"
-#include "ipc_skeleton.h"
 #include "dfx_hisysevent.h"
 #include "i_multimodal_input_connect.h"
 #include "mmi_log.h"
@@ -151,7 +149,6 @@ int32_t UDSServer::AddSocketPairInfo(const std::string& programName,
             programName.c_str(), pid, MAKE_SHARED_FAIL);
         return RET_ERR;
     }
-    AddPermission(sess);
 #ifdef OHOS_BUILD_MMI_DEBUG
     sess->SetClientFd(toReturnClientFd);
 #endif // OHOS__BUILD_MMI_DEBUG
@@ -165,23 +162,6 @@ int32_t UDSServer::AddSocketPairInfo(const std::string& programName,
     return RET_OK;
 }
 
-void UDSServer::AddPermission(SessionPtr sess)
-{
-    uint32_t callerToken = IPCSkeleton::GetCallingTokenID();
-    std::string permissionMonitor = "ohos.permission.INPUT_MONITORING";
-    
-    if (Security::AccessToken::AccessTokenKit::GetTokenTypeFlag(callerToken) ==
-        Security::AccessToken::ATokenTypeEnum::TOKEN_HAP) {
-        MMI_HILOGD("type flag matched");
-        int32_t result = Security::AccessToken::AccessTokenKit::VerifyAccessToken(callerToken, permissionMonitor);
-        MMI_HILOGD("verify access result:%{public}d", result);
-        if (result != Security::AccessToken::PERMISSION_GRANTED) {
-            MMI_HILOGD("permission is not granted");
-            sess->AddPermission(false);
-        }
-    }
-}
-
 void UDSServer::Dump(int32_t fd, const std::vector<std::string> &args)
 {
     CALL_DEBUG_ENTER;
@@ -191,9 +171,8 @@ void UDSServer::Dump(int32_t fd, const std::vector<std::string> &args)
         std::shared_ptr<UDSSession> udsSession = item.second;
         CHKPV(udsSession);
         mprintf(fd,
-                "Uid:%d | Pid:%d | Fd:%d | HasPermission:%s | Descript:%s\t",
+                "Uid:%d | Pid:%d | Fd:%d | Descript:%s\t",
                 udsSession->GetUid(), udsSession->GetPid(), udsSession->GetFd(),
-                udsSession->HasPermission() ? "true" : "false",
                 udsSession->GetDescript().c_str());
     }
 }
