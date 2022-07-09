b6ddd332d7be87724fb3041ab952a2d5ad59e276
meng-xinhai <mengxinhai1@huawei.com>
2022-07-09 09:21:25 +0000

    æè解决双指事件第二个手指派发到第一ä¿个手指的窗口
    Signed-off-by: meng-xinhai <mengxinhai1@huawei.com>
    Change-Id: I35b2544a23b145fcc312f0157fd6d35e1094532e

--- service/event_dispatch/src/event_dispatch.cpp
+++ service/event_dispatch/src/event_dispatch.cpp
@@ -81,9 +81,25 @@ void EventDispatch::HandlePointerEvent(std::shared_ptr<PointerEvent> point)
         MMI_HILOGW("the pointer event does not report normally, application not response");
         return;
     }
-
+    auto pid = udsServer->GetClientPid(fd);
+    auto pointerEvent = std::make_shared<PointerEvent>(*point);
+    auto pointerIdList = pointerEvent->GetPointersIdList();
+    if (pointerIdList.size() > 1) {
+        for (const auto& id : pointerIdList) {
+            PointerEvent::PointerItem pointeritem;
+            if (!pointerEvent->GetPointerItem(id, pointeritem)) {
+                MMI_HILOGW("can't find this poinerItem");
+                continue;
+            }
+            auto pointerPid = WinMgr->GetWindowPid(pointeritem.GetTargetWindowId());
+            if (pointerPid >=0 && pointerPid != pid) {
+                pointerEvent->RemovePointerItem(id);
+                MMI_HILOGD("pointerIdList size: %{public}lu",pointerEvent->GetPointersIdList().size());
+            }
+        }
+    }
     NetPacket pkt(MmiMessageId::ON_POINTER_EVENT);
-    InputEventDataTransformation::Marshalling(point, pkt);
+    InputEventDataTransformation::Marshalling(pointerEvent, pkt);
     BytraceAdapter::StartBytrace(point, BytraceAdapter::TRACE_STOP);
     if (!udsServer->SendMsg(fd, pkt)) {
         MMI_HILOGE("Sending structure of EventTouch failed! errCode:%{public}d", MSG_SEND_FAIL);
