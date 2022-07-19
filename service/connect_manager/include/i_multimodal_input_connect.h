97e41bb0d8b3796edba2fd45f777526a261948ec
liuwei <liuwei595@huawei.com>
2022-07-19 08:34:21 +0800

    MarkProcess binder to socket
    Signed-off-by: liuwei <liuwei595@huawei.com>

--- service/connect_manager/include/i_multimodal_input_connect.h
+++ service/connect_manager/include/i_multimodal_input_connect.h
@@ -36,7 +36,6 @@ public:
     virtual int32_t AddInputEventFilter(sptr<IEventFilter> filter) = 0;
     virtual int32_t SetPointerVisible(bool visible) = 0;
     virtual int32_t IsPointerVisible(bool &visible) = 0;
-    virtual int32_t MarkEventProcessed(int32_t eventId) = 0;
     virtual int32_t AddInputHandler(int32_t handlerId, InputHandlerType handlerType,
         HandleEventType eventType) = 0;
     virtual int32_t RemoveInputHandler(int32_t handlerId, InputHandlerType handlerType) = 0;
@@ -52,7 +51,6 @@ public:
         ADD_INPUT_EVENT_FILTER = 1,
         SET_POINTER_VISIBLE = 2,
         IS_POINTER_VISIBLE = 3,
-        MARK_EVENT_PROCESSED = 4,
         SUBSCRIBE_KEY_EVENT = 6,
         UNSUBSCRIBE_KEY_EVENT = 7,
         ADD_INPUT_HANDLER = 8,
