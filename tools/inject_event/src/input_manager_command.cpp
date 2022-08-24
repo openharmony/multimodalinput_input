b749dd7257892bce96a21e3877d9da1c1ff5439a
xueyuanzhao <zhaoxueyuan@huawei.com>
2022-08-24 09:31:23 +0000

    Signed-off-by:xueyuanzhao<zhaoxueyuan@huawei.com>
    Signed-off-by: xueyuanzhao <zhaoxueyuan@huawei.com>
    Change-Id: I57f5bb3a7f70bee7f124784356e3d6d0cb23402f

--- tools/inject_event/src/input_manager_command.cpp
+++ tools/inject_event/src/input_manager_command.cpp
@@ -655,6 +655,14 @@ int32_t InputManagerCommand::ParseCommand(int32_t argc, char *argv[])
 
                             item.SetDisplayX(px2);
                             item.SetDisplayY(py2);
+                            pointerEvent->SetActionTime(endTimeMs - 1);
+                            pointerEvent->UpdatePointerItem(0, item);
+                            pointerEvent->SetPointerAction(PointerEvent::POINTER_ACTION_MOVE);
+                            InputManager::GetInstance()->SimulateInputEvent(pointerEvent);
+                            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+                            item.SetDisplayX(px2);
+                            item.SetDisplayY(py2);
                             pointerEvent->SetActionTime(endTimeMs);
                             pointerEvent->UpdatePointerItem(0, item);
                             pointerEvent->SetPointerAction(PointerEvent::POINTER_ACTION_UP);
