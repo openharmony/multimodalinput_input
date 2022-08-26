113270bc25a4a7c80c19175bedef88f2427046ec
mayunteng_1 <mayunteng@huawei.com>
2022-08-26 14:11:55 +0800

    解决跨屏拖拽坐标不准确问题
    Signed-off-by: mayunteng_1 <mayunteng@huawei.com>
    Change-Id: Ie767f2c571171e284c9438b9e0e433c2f6d49e30

--- service/window_manager/include/i_pointer_drawing_manager.h
+++ service/window_manager/include/i_pointer_drawing_manager.h
@@ -31,8 +31,8 @@ public:
     static std::shared_ptr<IPointerDrawingManager> GetInstance();
     virtual void DrawPointer(int32_t displayId, int32_t physicalX, int32_t physicalY,
         const MOUSE_ICON mouseStyle = MOUSE_ICON::DEFAULT) {}
-    virtual void OnDisplayInfo(const DisplayInfo& displayInfo,const WinInfo &info) {}
-    virtual void OnDisplayInfo(const DisplayGroupInfo& displayGroupInfo,const WinInfo &info) {}
+    virtual void OnDisplayInfo(const DisplayInfo& displayInfo, const WinInfo &info) {}
+    virtual void OnDisplayInfo(const DisplayGroupInfo& displayGroupInfo, const WinInfo &info) {}
     virtual bool Init()
     {
         return true;
