cac36914571aa3aa9d203b18fadc85f0625a6925
xueyuanzhao <zhaoxueyuan@huawei.com>
2022-09-01 01:46:04 +0000

    Signed-off-by:xueyuanzhao<zhaoxueyuan@huawei.com>
    Signed-off-by: xueyuanzhao <zhaoxueyuan@huawei.com>
    Change-Id: I3b829009d8f60a4ada432d0f9d47a01067a23c42

--- frameworks/proxy/event_handler/src/client_msg_handler.cpp
+++ frameworks/proxy/event_handler/src/client_msg_handler.cpp
@@ -20,6 +20,7 @@
 #include <sstream>
 
 #include "bytrace_adapter.h"
+#include "event_log_helper.h"
 #include "input_device.h"
 #ifdef OHOS_BUILD_ENABLE_COOPERATE
 #include "input_device_cooperate_impl.h"
@@ -137,7 +138,7 @@ int32_t ClientMsgHandler::OnKeyEvent(const UDSClient& client, NetPacket& pkt)
         return PACKET_READ_FAIL;
     }
     MMI_HILOGI("Key event dispatcher of client, Fd:%{public}d", fd);
-    PrintEventData(key);
+    EventLogHelper::PrintEventData(key);
     BytraceAdapter::StartBytrace(key, BytraceAdapter::TRACE_START, BytraceAdapter::KEY_DISPATCH_EVENT);
     key->SetProcessedCallback(dispatchCallback_);
     InputMgrImpl.OnKeyEvent(key);
@@ -157,7 +158,7 @@ int32_t ClientMsgHandler::OnPointerEvent(const UDSClient& client, NetPacket& pkt
         return RET_ERR;
     }
     MMI_HILOGD("Pointer event dispatcher of client:");
-    PrintEventData(pointerEvent);
+    EventLogHelper::PrintEventData(pointerEvent);
     if (PointerEvent::POINTER_ACTION_CANCEL == pointerEvent->GetPointerAction()) {
         MMI_HILOGI("Operation canceled.");
     }
