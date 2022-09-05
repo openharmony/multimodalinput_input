9f77bebdaba55bc5d9b0bda523fb1e90f86083c1
mayunteng_1 <mayunteng@huawei.com>
2022-09-05 14:57:38 +0800

    HDF Joint debugging across process services
    Signed-off-by: mayunteng_1 <mayunteng@huawei.com>
    Change-Id: Id6ef1b3fb426adc9173611a4f44b3b22b41c3b8f

--- uinput/hdf_device_event_dispatch.h
+++ uinput/hdf_device_event_dispatch.h
@@ -18,18 +18,21 @@
 
 #include <cstdint>
 
-#include "input_type.h"
 #include "inject_thread.h"
 #include "nocopyable.h"
+#include "v1_0/iinput_interfaces.h"
 
 namespace OHOS {
 namespace MMI {
-class HdfDeviceEventDispatch {
+using namespace OHOS::HDI::Input::V1_0;
+class HdfDeviceEventDispatch : public IInputCallback {
 public:
     HdfDeviceEventDispatch(const uint32_t maxX, const uint32_t maxY);
     DISALLOW_COPY_AND_MOVE(HdfDeviceEventDispatch);
     virtual ~HdfDeviceEventDispatch();
-    static void GetEventCallbackDispatch(const InputEventPackage **pkgs, uint32_t count, uint32_t devIndex);
+
+    int32_t EventPkgCallback(const std::vector<EventPackage> &pkgs, uint32_t devIndex) override;
+    int32_t HotPlugCallback(const HotPlugEvent &event) override;
 
 private:
     static InjectThread injectThread_;
