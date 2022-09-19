4c88ca1d638ec95117b0e824bae2d569db73d537
mayunteng_1 <mayunteng@huawei.com>
2022-09-19 14:24:07 +0800

    删除按键和指针事件分发
    Signed-off-by: mayunteng_1 <mayunteng@huawei.com>
    Change-Id: I224d91f377f83ed6a85d4ec303bc45a1fcf5a870

--- service/event_dispatch/src/event_dispatch_handler.cpp
+++ service/event_dispatch/src/event_dispatch_handler.cpp
@@ -89,7 +89,6 @@ void EventDispatchHandler::HandlePointerEventInner(const std::shared_ptr<Pointer
         DfxHisysevent::OnUpdateTargetPointer(point, fd, OHOS::HiviewDFX::HiSysEvent::EventType::FAULT);
         return;
     }
-    DfxHisysevent::OnUpdateTargetPointer(point, fd, OHOS::HiviewDFX::HiSysEvent::EventType::BEHAVIOR);
     if (CheckPointerEvent(point)) {
         MMI_HILOGE("Check pointer event return true,filter out this pointer event");
         return;
@@ -142,7 +141,6 @@ int32_t EventDispatchHandler::DispatchKeyEventPid(UDSServer& udsServer, std::sha
         DfxHisysevent::OnUpdateTargetKey(key, fd, OHOS::HiviewDFX::HiSysEvent::EventType::FAULT);
         return RET_ERR;
     }
-    DfxHisysevent::OnUpdateTargetKey(key, fd, OHOS::HiviewDFX::HiSysEvent::EventType::BEHAVIOR);
     MMI_HILOGD("Event dispatcher of server:KeyEvent:KeyCode:%{public}d,Action:%{public}d,EventType:%{public}d,"
         "Fd:%{public}d", key->GetKeyCode(), key->GetAction(), key->GetEventType(), fd);
     auto session = udsServer.GetSession(fd);
