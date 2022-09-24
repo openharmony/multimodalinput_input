e4d697f80dcbde6cf680bf35416e5a298ad6238b
hhh2 <pengfei.hou@huawei.com>
2022-09-24 09:58:03 +0800

    solve build error
    Signed-off-by: hhh2 <pengfei.hou@huawei.com>
    Change-Id: I451a6d4de7550fb18ff2b3e3a7534f4a70ad9e77

--- service/event_handler/src/event_normalize_handler.cpp
+++ service/event_handler/src/event_normalize_handler.cpp
@@ -119,7 +119,9 @@ int32_t EventNormalizeHandler::OnEventDeviceAdded(libinput_event *event)
     InputDevMgr->OnInputDeviceAdded(device);
     KeyMapMgr->ParseDeviceConfigFile(device);
     KeyRepeat->AddDeviceConfig(device);
+#ifdef OHOS_BUILD_ENABLE_KEYBOARD
     KeyEventHdr->ResetKeyEvent(device);
+#endif // OHOS_BUILD_ENABLE_KEYBOARD
     return RET_OK;
 }
 
@@ -134,6 +136,7 @@ int32_t EventNormalizeHandler::OnEventDeviceRemoved(libinput_event *event)
     return RET_OK;
 }
 
+#ifdef OHOS_BUILD_ENABLE_KEYBOARD
 void EventNormalizeHandler::HandleKeyEvent(const std::shared_ptr<KeyEvent> keyEvent)
 {
     if (nextHandler_ == nullptr) {
@@ -141,7 +144,6 @@ void EventNormalizeHandler::HandleKeyEvent(const std::shared_ptr<KeyEvent> keyEv
         return;
     }
     DfxHisysevent::GetDispStartTime();
-#ifdef OHOS_BUILD_ENABLE_KEYBOARD
     CHKPV(keyEvent);
     EventLogHelper::PrintEventData(keyEvent);
 #ifdef OHOS_BUILD_ENABLE_COOPERATE
@@ -153,9 +155,10 @@ void EventNormalizeHandler::HandleKeyEvent(const std::shared_ptr<KeyEvent> keyEv
     nextHandler_->HandleKeyEvent(keyEvent);
     DfxHisysevent::CalcKeyDispTimes();
     DfxHisysevent::ReportDispTimes();
-#endif // OHOS_BUILD_ENABLE_KEYBOARD
 }
+#endif // OHOS_BUILD_ENABLE_KEYBOARD
 
+#ifdef OHOS_BUILD_ENABLE_POINTER
 void EventNormalizeHandler::HandlePointerEvent(const std::shared_ptr<PointerEvent> pointerEvent)
 {
     if (nextHandler_ == nullptr) {
@@ -163,7 +166,6 @@ void EventNormalizeHandler::HandlePointerEvent(const std::shared_ptr<PointerEven
         return;
     }
     DfxHisysevent::GetDispStartTime();
-#ifdef OHOS_BUILD_ENABLE_POINTER
     CHKPV(pointerEvent);
     if (pointerEvent->GetPointerAction() == PointerEvent::POINTER_ACTION_AXIS_END) {
         MMI_HILOGI("MouseEvent Normalization Results, PointerAction:%{public}d,PointerId:%{public}d,"
@@ -188,9 +190,10 @@ void EventNormalizeHandler::HandlePointerEvent(const std::shared_ptr<PointerEven
     nextHandler_->HandlePointerEvent(pointerEvent);
     DfxHisysevent::CalcPointerDispTimes();
     DfxHisysevent::ReportDispTimes();
-#endif // OHOS_BUILD_ENABLE_POINTER
 }
+#endif // OHOS_BUILD_ENABLE_POINTER
 
+#ifdef OHOS_BUILD_ENABLE_TOUCH
 void EventNormalizeHandler::HandleTouchEvent(const std::shared_ptr<PointerEvent> pointerEvent)
 {
     if (nextHandler_ == nullptr) {
@@ -198,14 +201,13 @@ void EventNormalizeHandler::HandleTouchEvent(const std::shared_ptr<PointerEvent>
         return;
     }
     DfxHisysevent::GetDispStartTime();
-#ifdef OHOS_BUILD_ENABLE_TOUCH
     CHKPV(pointerEvent);
     WinMgr->UpdateTargetPointer(pointerEvent);
     nextHandler_->HandleTouchEvent(pointerEvent);
     DfxHisysevent::CalcPointerDispTimes();
     DfxHisysevent::ReportDispTimes();
-#endif // OHOS_BUILD_ENABLE_TOUCH
 }
+#endif // OHOS_BUILD_ENABLE_TOUCH
 
 int32_t EventNormalizeHandler::HandleKeyboardEvent(libinput_event* event)
 {
@@ -316,16 +318,20 @@ int32_t EventNormalizeHandler::HandleMouseEvent(libinput_event* event)
         return ERROR_UNSUPPORT;
     }
 #ifdef OHOS_BUILD_ENABLE_POINTER
+#ifdef OHOS_BUILD_ENABLE_KEYBOARD
     const auto &keyEvent = KeyEventHdr->GetKeyEvent();
     CHKPR(keyEvent, ERROR_NULL_POINTER);
+#endif // OHOS_BUILD_ENABLE_KEYBOARD
     MouseEventHdr->Normalize(event);
     auto pointerEvent = MouseEventHdr->GetPointerEvent();
     CHKPR(pointerEvent, ERROR_NULL_POINTER);
+#ifdef OHOS_BUILD_ENABLE_KEYBOARD
     std::vector<int32_t> pressedKeys = keyEvent->GetPressedKeys();
     for (const int32_t& keyCode : pressedKeys) {
         MMI_HILOGI("Pressed keyCode:%{public}d", keyCode);
     }
     pointerEvent->SetPressedKeys(pressedKeys);
+#endif // OHOS_BUILD_ENABLE_KEYBOARD
     BytraceAdapter::StartBytrace(pointerEvent, BytraceAdapter::TRACE_START);
     nextHandler_->HandlePointerEvent(pointerEvent);
 #else
@@ -462,12 +468,14 @@ int32_t EventNormalizeHandler::AddHandleTimer(int32_t timeout)
 {
     CALL_DEBUG_ENTER;
     timerId_ = TimerMgr->AddTimer(timeout, 1, [this]() {
+#ifdef OHOS_BUILD_ENABLE_KEYBOARD
         auto keyEvent = KeyEventHdr->GetKeyEvent();
         CHKPV(keyEvent);
         CHKPV(nextHandler_);
         nextHandler_->HandleKeyEvent(keyEvent);
         int32_t triggerTime = KeyRepeat->GetIntervalTime(keyEvent->GetDeviceId());
         this->AddHandleTimer(triggerTime);
+#endif // OHOS_BUILD_ENABLE_KEYBOARD
     });
     return timerId_;
 }
