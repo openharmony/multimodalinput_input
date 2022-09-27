bb6193793076d92f104b27652598ab25ce0c38cf
mayunteng_1 <mayunteng@huawei.com>
2022-09-27 14:41:42 +0800

    API error code rectification
    Signed-off-by: mayunteng_1 <mayunteng@huawei.com>
    Change-Id: I2ea25cd934c18d3299983af15d5bab34e4fa40b1

--- util/common/include/util_napi_error.h
+++ util/common/include/util_napi_error.h
@@ -32,22 +32,20 @@ struct NapiError {
 };
 
 enum NapiErrorCode : int32_t {
+    OTHER_ERROR = -1,
     COMMON_PERMISSION_CHECK_ERROR = 201,
     COMMON_PARAMETER_ERROR = 401,
-    MONITOR_REGISTER_EXCEED_MAX = 4100001,
     COOPERATOR_TARGET_DEV_DESCRIPTOR_ERROR = 4400001,
-    COOPERATOR_DEVICE_ID_ERROE = 4400002,
-    COOPERATOR_FAIL = 4400003,
-    OTHER_ERROR = -1,
+    COOPERATOR_FAIL = 4400002,
+    COOPERATOR_DEVICE_ID_ERROE = 4400003,
 };
 
 const std::map<int32_t, NapiError> NAPI_ERRORS = {
     {COMMON_PERMISSION_CHECK_ERROR,  {"201", "Permission denied. An attempt was made to %s forbidden by permission:%s."}},
     {COMMON_PARAMETER_ERROR,  {"401", "Parameter error. The type of %s must be %s."}},
-    {MONITOR_REGISTER_EXCEED_MAX, {"401", "Maximum number of listeners exceeded for a single process"}},
     {COOPERATOR_TARGET_DEV_DESCRIPTOR_ERROR, {"4400001", "Incorrect descriptor for the target device"}},
-    {COOPERATOR_DEVICE_ID_ERROE, {"4400002", " Incorrect ID of the input device for screen hop"}},
-    {COOPERATOR_FAIL, {"4400003", "Screen hop failed"}},
+    {COOPERATOR_DEVICE_ID_ERROE, {"401", " Incorrect ID of the input device for screen hop"}},
+    {COOPERATOR_FAIL, {"4400002", "Screen hop failed"}},
 };
 
 #define THROWERR_API9(env, code, ...) \
