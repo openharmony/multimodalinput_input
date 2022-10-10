3565c626acd3d96b99dd051f67d8d8b84005a65c
mayunteng_1 <mayunteng@huawei.com>
2022-10-10 21:03:32 +0800

    back键
    Signed-off-by: mayunteng_1 <mayunteng@huawei.com>
    Change-Id: Ic42ce895d35cb939f614975e2bfe83e74cbce3b4

--- frameworks/napi/input_event_client/src/js_register_module.cpp
+++ frameworks/napi/input_event_client/src/js_register_module.cpp
@@ -35,62 +35,6 @@ static napi_value InjectEvent(napi_env env, napi_callback_info info)
     napi_value result = nullptr;
     size_t argc = 1;
     napi_value argv[1] = { 0 };
-    if (napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr) != napi_ok) {
-        MMI_HILOGE("Call napi_get_cb_info failed");
-        napi_create_int32(env, MMI_STANDARD_EVENT_INVALID_PARAM, &result);
-        return result;
-    }
-    NAPI_ASSERT(env, argc == 1, "paramater num error");
-
-    napi_value keyHandle = nullptr;
-    napi_get_named_property(env, argv[0], "KeyEvent", &keyHandle);
-    napi_valuetype tmpType = napi_undefined;
-    napi_typeof(env, keyHandle, &tmpType);
-    NAPI_ASSERT(env, tmpType == napi_object, "parameter1 is not napi_object");
-
-    auto keyEvent = KeyEvent::Create();
-    CHKPP(keyEvent);
-    bool isPressed = false;
-    int32_t ret = GetNamedPropertyBool(env, keyHandle, "isPressed", isPressed);
-    if (ret != RET_OK) {
-        MMI_HILOGE("Get isPressed failed");
-        return result;
-    }
-    if (isPressed) {
-        keyEvent->SetKeyAction(KeyEvent::KEY_ACTION_DOWN);
-    } else {
-        keyEvent->SetKeyAction(KeyEvent::KEY_ACTION_UP);
-    }
-    int32_t keyCode;
-    ret = GetNamedPropertyInt32(env, keyHandle, "keyCode", keyCode);
-    if (ret != RET_OK) {
-        MMI_HILOGE("Get keyCode failed");
-        return result;
-    }
-    keyEvent->SetKeyCode(keyCode);
-    keyEvent->AddFlag(InputEvent::EVENT_FLAG_NO_INTERCEPT);
-    int32_t keyDownDuration;
-    ret = GetNamedPropertyInt32(env, keyHandle, "keyDownDuration", keyDownDuration);
-    if (ret != RET_OK) {
-        MMI_HILOGE("Get keyDownDuration failed");
-        return result;
-    }
-    KeyEvent::KeyItem item;
-    item.SetKeyCode(keyCode);
-    item.SetPressed(isPressed);
-    item.SetDownTime(static_cast<int64_t>(keyDownDuration));
-    keyEvent->AddKeyItem(item);
-    InputManager::GetInstance()->SimulateInputEvent(keyEvent);
-    napi_create_int32(env, 0, &result);
-    return result;
-}
-
-static napi_value InjectKeyEvent(napi_env env, napi_callback_info info)
-{
-    CALL_DEBUG_ENTER;
-    napi_value result = nullptr;
-    size_t argc = 1;
-    napi_value argv[1] = { 0 };
     CHKRP(env, napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr), GET_CB_INFO);
     if (argc == 0) {
         MMI_HILOGE("Paramater number error");
@@ -153,7 +97,6 @@ static napi_value MmiInit(napi_env env, napi_value exports)
 {
     napi_property_descriptor desc[] = {
         DECLARE_NAPI_FUNCTION("injectEvent", InjectEvent),
-        DECLARE_NAPI_FUNCTION("injectKeyEvent", InjectKeyEvent),
     };
     NAPI_CALL(env, napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc));
     return exports;
