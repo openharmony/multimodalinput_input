bc6f27358c9de323045af2449bae2b96352269eb
mayunteng_1 <mayunteng@huawei.com>
2022-10-11 14:18:52 +0800

    Signed-off-by: mayunteng_1 <mayunteng@huawei.com> Change-Id: I257b8ef03fa58e735ca9e5f32aa8af06a53a1a6e

--- frameworks/napi/input_consumer/src/js_register_module.cpp
+++ frameworks/napi/input_consumer/src/js_register_module.cpp
@@ -87,12 +87,12 @@ napi_value GetEventInfoAPI9(napi_env env, napi_callback_info info, KeyEventMonit
         subKeyNames += ",";
         MMI_HILOGD("preKeys:%{public}d", item);
     }
-    auto optionalFinalKey = GetNamedPropertyInt32(env, argv[1], "finalKey");
-    if (!optionalFinalKey) {
+    std::optional<int32_t> tempFinalKey = GetNamedPropertyInt32(env, argv[1], "finalKey");
+    if (!tempFinalKey) {
         MMI_HILOGE("GetNamedPropertyInt32 failed");
         return nullptr;
     }
-    int32_t finalKey = optionalFinalKey.value();
+    int32_t finalKey = tempFinalKey.value();
     if (finalKey < 0) {
         MMI_HILOGE("finalKey:%{public}d is less 0, can not process", finalKey);
         THROWERR_CUSTOM(env, COMMON_PARAMETER_ERROR, "finalKey must be greater than or equal to 0");
@@ -112,12 +112,12 @@ napi_value GetEventInfoAPI9(napi_env env, napi_callback_info info, KeyEventMonit
     keyOption->SetFinalKeyDown(isFinalKeyDown);
     MMI_HILOGD("IsFinalKeyDown:%{public}d,map_key:%{public}s",
         (isFinalKeyDown == true?1:0), subKeyNames.c_str());
-    auto optionalkeyDownDuration = GetNamedPropertyInt32(env, argv[1], "finalKeyDownDuration");
-    if (!optionalkeyDownDuration) {
+    std::optional<int32_t> tempKeyDownDuration = GetNamedPropertyInt32(env, argv[1], "finalKeyDownDuration");
+    if (!tempKeyDownDuration) {
         MMI_HILOGE("GetNamedPropertyInt32 failed");
         return nullptr;
     }
-    int32_t finalKeyDownDuration = optionalkeyDownDuration.value();
+    int32_t finalKeyDownDuration = tempKeyDownDuration.value();
     if (finalKeyDownDuration < 0) {
         MMI_HILOGE("finalKeyDownDuration:%{public}d is less 0, can not process", finalKeyDownDuration);
         THROWERR_CUSTOM(env, COMMON_PARAMETER_ERROR, "finalKeyDownDuration must be greater than or equal to 0");
