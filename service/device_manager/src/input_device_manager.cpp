4aefb9818f2390f56e0b4599ecb239f1ef82e345
mayunteng_1 <mayunteng@huawei.com>
2022-10-12 09:19:01 +0800

    蓝区同步黄区失败修改区
    Signed-off-by: mayunteng_1 <mayunteng@huawei.com>
    Change-Id: Ic154eac4896741ec970c85b3d5ea0a2df7a97f97

--- service/device_manager/src/input_device_manager.cpp
+++ service/device_manager/src/input_device_manager.cpp
@@ -525,7 +525,12 @@ std::vector<std::string> InputDeviceManager::GetCooperateDhids(int32_t deviceId)
         if (networkId != pointerNetworkId) {
             continue;
         }
-        if (GetDeviceSupportKey(item.first) == KEYBOARD_TYPE_ALPHABETICKEYBOARD) {
+        int32_t keyboardType = KEYBOARD_TYPE_NONE;
+        if (GetDeviceSupportKey(item.first, keyboardType) != RET_OK) {
+            MMI_HILOGI("Get device support key failed");
+            return dhids;
+        }
+        if (keyboardType == KEYBOARD_TYPE_ALPHABETICKEYBOARD) {
             dhids.push_back(item.second.dhid_);
             MMI_HILOGI("unq: %{public}s, type:%{public}s", dhids.back().c_str(), "supportkey");
         }
