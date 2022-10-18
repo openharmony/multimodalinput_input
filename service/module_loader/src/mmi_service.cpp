49c9a5986637da54f732fa3863bf64a653480077
mayunteng_1 <mayunteng@huawei.com>
2022-10-18 14:40:16 +0800

    所有初始化后，发布sa服务
    Signed-off-by: mayunteng_1 <mayunteng@huawei.com>
    Change-Id: Ia2c6ee82c89cc31062024c9eba549bce6732b6ef

--- service/module_loader/src/mmi_service.cpp
+++ service/module_loader/src/mmi_service.cpp
@@ -214,10 +214,6 @@ bool MMIService::InitService()
         MMI_HILOGE("Service running status is not enabled");
         return false;
     }
-    if (!(Publish(this))) {
-        MMI_HILOGE("Service initialization failed");
-        return false;
-    }
     if (EpollCreat(MAX_EVENT_SIZE) < 0) {
         MMI_HILOGE("Create epoll failed");
         return false;
@@ -226,6 +222,10 @@ bool MMIService::InitService()
     if (ret <  0) {
         MMI_HILOGE("AddEpoll error ret:%{public}d", ret);
         EpollClose();
+        return false;
+    }
+    if (!(Publish(this))) {
+        MMI_HILOGE("Service initialization failed");
         return false;
     }
     MMI_HILOGI("AddEpoll, epollfd:%{public}d,fd:%{public}d", mmiFd_, epollFd_);
@@ -268,10 +268,6 @@ int32_t MMIService::Init()
         MMI_HILOGE("Create epoll failed");
         return EPOLL_CREATE_FAIL;
     }
-    if (!InitService()) {
-        MMI_HILOGE("Saservice init failed");
-        return SASERVICE_INIT_FAIL;
-    }
 #ifdef OHOS_BUILD_ENABLE_COOPERATE
     InputDevCooSM->Init();
 #endif // OHOS_BUILD_ENABLE_COOPERATE
@@ -289,6 +285,10 @@ int32_t MMIService::Init()
         std::placeholders::_2));
     KeyMapMgr->GetConfigKeyValue("default_keymap", KeyMapMgr->GetDefaultKeyId());
     OHOS::system::SetParameter(INPUT_POINTER_DEVICE, "false");
+    if (!InitService()) {
+        MMI_HILOGE("Saservice init failed");
+        return SASERVICE_INIT_FAIL;
+    }
     MMI_HILOGI("Set para input.pointer.device false");
     return RET_OK;
 }
