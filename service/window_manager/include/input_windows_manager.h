5ba8961ed48619c29ce9847ae1005886b8c4e526
mayunteng_1 <mayunteng@huawei.com>
2022-10-18 10:07:54 +0800

    编码规范-注释整改
    Signed-off-by: mayunteng_1 <mayunteng@huawei.com>
    Change-Id: I37eb4740ea9975cb6553bdb4095647485fe8b9f0

--- service/window_manager/include/input_windows_manager.h
+++ service/window_manager/include/input_windows_manager.h
@@ -140,7 +140,8 @@ private:
     std::map<int32_t, std::map<int32_t, int32_t>> pointerStyle_;
 #endif // OHOS_BUILD_ENABLE_POINTER
     DisplayGroupInfo displayGroupInfo_;
-    MouseLocation mouseLocation_ = { -1, -1 }; // physical coord
+    // physical coord
+    MouseLocation mouseLocation_ = { -1, -1 };
 };
 
 #define WinMgr ::OHOS::DelayedSingleton<InputWindowsManager>::GetInstance()
