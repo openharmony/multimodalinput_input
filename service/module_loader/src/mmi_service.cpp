74c796b0d6c9593d97fec7a32005118be903c30e
Justin_Hu <hugongcheng@huawei.com>
2022-11-07 10:17:17 +0000

    add comment
    Signed-off-by: Justin_Hu <hugongcheng@huawei.com>

--- service/module_loader/src/mmi_service.cpp
+++ service/module_loader/src/mmi_service.cpp
@@ -259,6 +259,10 @@ int32_t MMIService::Init()
         return EPOLL_CREATE_FAIL;
     }
 #ifdef OHOS_BUILD_ENABLE_COOPERATE
+    /* 
+     * PostAsyncTask is used here to prevent InputDeviceCooperateSM:: StartRemoteCooperate() and
+     * CheckPointerEvent() from holding locks, resulting in deadlocks
+     */
     InputDevCooSM->Init(std::bind(&DelegateTasks::PostAsyncTask, &delegateTasks_, std::placeholders::_1));
 #endif // OHOS_BUILD_ENABLE_COOPERATE
     MMI_HILOGD("Input msg handler init");
