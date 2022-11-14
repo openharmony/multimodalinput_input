62732d49282ef122a3a94fb60e9a5228e155856f
wuliangdong <wuliangdong1@huawei.com>
2022-11-14 18:57:28 +0800

    Signed-off-by:wuliangdong<wuliangdong1@huawei.com>
    Signed-off-by: wuliangdong <wuliangdong1@huawei.com>
    Change-Id: I15c485101a98e420379388635890d3e16570ffbc

--- tools/vuinput/src/virtual_device.cpp
+++ tools/vuinput/src/virtual_device.cpp
@@ -572,6 +572,7 @@ bool VirtualDevice::AddDevice(const std::string& startDeviceName)
         std::cout << "Failed to create file" <<std::endl;
         return false;
     }
+    flagFile.close();
     return true;
 }
 
