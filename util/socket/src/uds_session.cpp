14bf6d408d4a86f58b2ba7b3d642853bca4eed68
xueyuanzhao <zhaoxueyuan@huawei.com>
2022-11-21 12:43:54 +0000

    fixed 454e7c9 from https://gitee.com/xueyuanzhao/multimodalinput_input/pulls/2129 Signed-off-by:xueyuanzhao<zhaoxueyuan@huawei.com>
    Signed-off-by: xueyuanzhao <zhaoxueyuan@huawei.com>
    Change-Id: Ide76be4e47a4e120a5adcf229a48031f3b051867

--- util/socket/src/uds_session.cpp
+++ util/socket/src/uds_session.cpp
@@ -71,7 +71,6 @@ bool UDSSession::SendMsg(const char *buf, size_t size) const
         if (count < 0) {
             if (errno == EAGAIN || errno == EINTR || errno == EWOULDBLOCK) {
                 MMI_HILOGW("Continue for errno EAGAIN|EINTR|EWOULDBLOCK, errno:%{public}d", errno);
-                usleep(SEND_RETRY_SLEEP_TIME);
                 continue;
             }
             MMI_HILOGE("Send return failed,error:%{public}d fd:%{public}d", errno, fd_);
