387517b4b07ab7de7c94a4e025cba98fd37b5ef1
yuanxinying <yuanxinying@huawei.com>
2023-10-21 18:48:47 +0800

    add resample algorithm to mmi Signed-off-by: yuanxinying <yuanxinying@huawei.com>

--- service/event_handler/include/input_event_handler.h
+++ service/event_handler/include/input_event_handler.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -41,7 +41,7 @@ class InputEventHandler final {
 public:
     DISALLOW_COPY_AND_MOVE(InputEventHandler);
     void Init(UDSServer& udsServer);
-    void OnEvent(void *event);
+    void OnEvent(void *event, int64_t frameTime);
     UDSServer *GetUDSServer() const;
 
     std::shared_ptr<EventNormalizeHandler> GetEventNormalizeHandler() const;
