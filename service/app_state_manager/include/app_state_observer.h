6610b8164be6eac804b0fdc33fd04b1d75fbf76d
pengyuan2023 <pingping@huawei.com>
2023-12-02 14:51:07 +0800

    增加组合按键监听优先级     Signed-off-by: pengyuan <lipengyuan4@huawei.com>

---
+++ service/app_state_manager/include/app_state_observer.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_APP_STATE_OBSERVER_H
+#define OHOS_APP_STATE_OBSERVER_H
+
+#include "nocopyable.h"
+#include "singleton.h"
+
+#include "app_mgr_interface.h"
+#include "application_state_observer_stub.h"
+#include "if_system_ability_manager.h"
+#include "iservice_registry.h"
+#include "mmi_log.h"
+#include "running_process_info.h"
+#include "system_ability.h"
+#include "system_ability_definition.h"
+
+namespace OHOS {
+namespace MMI {
+class ApplicationStateObserver : public AppExecFwk::ApplicationStateObserverStub {
+public:
+    ApplicationStateObserver() {};
+    ~ApplicationStateObserver() = default;
+    void OnForegroundApplicationChanged(const AppExecFwk::AppStateData &appStateData) override;
+    std::vector<AppExecFwk::AppStateData> GetForegroundAppData();
+private:
+    sptr<AppExecFwk::IAppMgr> appManager_ = nullptr;
+    OHOS::sptr<OHOS::AppExecFwk::IAppMgr> GetAppMgr();
+    int32_t GetForegroundApplicationInfo(std::vector<AppExecFwk::AppStateData> &list);
+};
+
+class AppObserverManager final {
+    DECLARE_DELAYED_SINGLETON(AppObserverManager);
+public:
+    DISALLOW_COPY_AND_MOVE(AppObserverManager);
+    void InitAppStateObserver();
+    void SetForegroundAppData(std::vector<AppExecFwk::AppStateData> list);
+    std::vector<AppExecFwk::AppStateData> GetForegroundAppData();
+private:
+    bool hasInit_ { false };
+    std::vector<AppExecFwk::AppStateData> foregroundAppData_ {};
+};
+
+#define APP_OBSERVER_MGR ::OHOS::DelayedSingleton<AppObserverManager>::GetInstance()
+}  // namespace MMI
+}  // namespace OHOS
+#endif  // OHOS_APP_STATE_OBSERVER_H
