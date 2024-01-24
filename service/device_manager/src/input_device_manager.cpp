b99b31d0cf1d1f7067c8e487c0979d045b44fefd
yangjintaoYYDS <yangjintao16@huawei.com>
2024-01-24 06:30:02 +0000

    LogModification0123
    Signed-off-by: yangjintaoYYDS <yangjintao16@huawei.com>
    Change-Id: Ib592b62524ffb02de42239c376bd0697d68890ff

--- service/device_manager/src/input_device_manager.cpp
+++ service/device_manager/src/input_device_manager.cpp
@@ -473,7 +473,7 @@ void InputDeviceManager::ScanPointerDevice()
 {
     bool hasPointerDevice = false;
     for (auto it = inputDevice_.begin(); it != inputDevice_.end(); ++it) {
-        if (it->second.isPointerDevice && it->second.enable) {
+        if (it->second.isPointerDevice) {
             hasPointerDevice = true;
             break;
         }
