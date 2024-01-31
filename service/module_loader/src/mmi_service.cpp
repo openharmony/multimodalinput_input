e8b16a30bbe4019444ea01604df771a41956e4f1
pengyanggit <yangpeng177@huawei.com>
2024-01-31 19:20:58 +0800

    Signed-off-by: pengyanggit <yangpeng177@huawei.com>
    Change-Id: I6e12ab634e6240c1d1bed60b4ca23b6cb1a5250f

--- service/module_loader/src/mmi_service.cpp
+++ service/module_loader/src/mmi_service.cpp
@@ -330,10 +330,10 @@ void MMIService::OnStart()
             threadStatusFlag_ = false;
         } else {
             MMI_HILOGE("Watchdog happened");
-            std::string warningDescMsg = WatchDog->GetBlockDescription(WATCHDOG_INTERVAL_TIME / 2000);
-            WatchDog->SendEvent(warningDescMsg, "SERVICE_WARNING");
-            std::string blockDescMsg = WatchDog->GetBlockDescription(WATCHDOG_INTERVAL_TIME / 1000);
-            WatchDog->SendEvent(blockDescMsg, "SERVICE_BLOCK");
+            std::string warningDescMsg = WATCHDOG_TASK->GetBlockDescription(WATCHDOG_INTERVAL_TIME / 2000);
+            WATCHDOG_TASK->SendEvent(warningDescMsg, "SERVICE_WARNING");
+            std::string blockDescMsg = WATCHDOG_TASK->GetBlockDescription(WATCHDOG_INTERVAL_TIME / 1000);
+            WATCHDOG_TASK->SendEvent(blockDescMsg, "SERVICE_BLOCK");
         }
     };
     HiviewDFX::Watchdog::GetInstance().RunPeriodicalTask("MMIService", taskFunc, WATCHDOG_INTERVAL_TIME,
