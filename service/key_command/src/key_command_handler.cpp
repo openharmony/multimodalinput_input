c931e9f6e173b2cbebb3e4413b9c1a60b642e7c3
hewanglon <hewanglong@huawei.com>
2024-02-28 10:30:11 +0000

    PZAdaptiveComponent
    Signed-off-by: hewanglon <hewanglong@huawei.com>
    Change-Id: I9508bae06925df66c91a22c2dd20e6dfa0c7389c

--- service/key_command/src/key_command_handler.cpp
+++ service/key_command/src/key_command_handler.cpp
@@ -1297,19 +1297,13 @@ bool KeyCommandHandler::IsEnableCombineKey(const std::shared_ptr<KeyEvent> key)
     if (key->GetKeyCode() == KeyEvent::KEYCODE_BRIGHTNESS_DOWN
         || key->GetKeyCode() == KeyEvent::KEYCODE_BRIGHTNESS_UP) {
         auto items = key->GetKeyItems();
-        if (items.size() != 1) {
-            return enableCombineKey_;
-        }
-        return true;
+        return items.size() != 1 ? enableCombineKey_ : true;
     }
     if (key->GetKeyCode() == KeyEvent::KEYCODE_VOLUME_UP
         || key->GetKeyCode() == KeyEvent::KEYCODE_VOLUME_DOWN
         || key->GetKeyCode() == KeyEvent::KEYCODE_VOLUME_MUTE) {
         auto items = key->GetKeyItems();
-        if (items.size() != 1) {
-            return enableCombineKey_;
-        }
-        return true;
+        return items.size() != 1 ? enableCombineKey_ : true;
     }
     return enableCombineKey_;
 }
