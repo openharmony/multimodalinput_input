49fd13c54f9081f8a9d102db3b5b122ef2ec98ee
lwx1285820 <lishaoxiong10@huawei.com>
2024-03-01 07:43:42 +0000

    OpenHarmony-4.0-Release Reduce alerts Copyright header
    Signed-off-by: lwx1285820 <lishaoxiong10@huawei.com>
    Change-Id: Idb855b0561d348f95dcb39a522154bee64ad54c5

--- frameworks/proxy/events/test/input_manager_filter_manual_test.cpp
+++ frameworks/proxy/events/test/input_manager_filter_manual_test.cpp
@@ -1,7 +1,4 @@
 /*
- * Copyright (c) Huawei Technologies Co., Ltd. 2020-2020. All rights reserved.
- */
-/*
  * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
