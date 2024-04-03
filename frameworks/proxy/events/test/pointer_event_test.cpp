c778322f3918dd05970ce47a90846409e5659d8e
郭旭 <guoxu34@huawei.com>
2024-04-03 03:30:27 +0000

    update frameworks/proxy/events/test/pointer_event_test.cpp.
    Signed-off-by: 郭旭 <guoxu34@huawei.com>

--- frameworks/proxy/events/test/pointer_event_test.cpp
+++ frameworks/proxy/events/test/pointer_event_test.cpp
@@ -738,7 +738,7 @@ HWTEST_F(PointerEventTest, PointerEventTest_SetExtraData_001, TestSize.Level1)
 {
     CALL_TEST_DEBUG;
     const uint32_t length = 5;
-    std::shared_ptr<const uint8_t[]> data(std::make_shared uint8_t[length]{1, 2, 3, 4, 5});
+    auto data = std::shared_ptr<const uint8_t[]>(new uint8_t[length]{1, 2, 3, 4, 5});
     auto inputEvent = InputEvent::Create();
     ASSERT_NE(inputEvent, nullptr);
     inputEvent->SetExtraData(data, length);
@@ -753,8 +753,6 @@ HWTEST_F(PointerEventTest, PointerEventTest_SetExtraData_001, TestSize.Level1)
 HWTEST_F(PointerEventTest, PointerEventTest_GetExtraData_001, TestSize.Level1)
 {
     CALL_TEST_DEBUG;
-    const uint32_t testDataLength = 4;
-    std::shared_ptr<const uint8_t[]> testData(std::make_shared uint8_t[testDataLength]{1, 2, 3, 4});
     auto inputEvent = InputEvent::Create();
     std::shared_ptr<const uint8_t[]> retrievedData;
     uint32_t retrievedLength;
