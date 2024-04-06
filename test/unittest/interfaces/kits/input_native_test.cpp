a03349330da275a34d35902dd236567a79170195
wanghao505 <wanghao505@huawei.com>
2024-04-06 06:52:34 +0000

    interface coverage
    Signed-off-by: wanghao505 <wanghao505@huawei.com>
    Change-Id: Ibb184c634a20f92fd361a4c15b24487a2effedf0

--- test/unittest/interfaces/kits/input_native_test.cpp
+++ test/unittest/interfaces/kits/input_native_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,6 +15,8 @@
 
 #include <gtest/gtest.h>
 
+#include "input_manager.h"
+#include "key_event.h"
 #include "oh_input_manager.h"
 #include "oh_key_code.h"
 
@@ -114,6 +116,26 @@ HWTEST_F(InputNativeTest, InputNativeTest_GetKeyState_001, TestSize.Level1)
     ASSERT_EQ(OH_Input_GetKeySwitch(keyState), KEY_DEFAULT);
     ASSERT_EQ(OH_Input_GetKeyState(keyState), INPUT_SUCCESS);
     OH_Input_DestroyKeyState(&keyState);
+}
+
+/**
+ * @tc.name: InputNativeTest_Authorize_001
+ * @tc.desc: Verify the Authorize
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(InputNativeTest, InputNativeTest_Authorize_001, TestSize.Level1)
+{
+    Input_KeyEvent* keyEvent = OH_Input_CreateKeyEvent();
+    OH_Input_SetKeyEventAction(keyEvent, KEY_ACTION_DOWN);
+    OH_Input_SetKeyEventKeyCode(keyEvent, KEYCODE_A);
+    OH_Input_InjectKeyEvent(keyEvent);
+    OH_Input_SetKeyEventAction(keyEvent, KEY_ACTION_UP);
+    OH_Input_SetKeyEventKeyCode(keyEvent, KEYCODE_A);
+    OH_Input_InjectKeyEvent(keyEvent);
+    InputManager::GetInstance()->Authorize(true);
+    OH_Input_DestroyKeyEvent(&keyEvent);
+    ASSERT_EQ(keyEvent, nullptr);
 }
 } // namespace MMI
 } // namespace OHOS
