bf3a9c4def54f81849ebad3be43adc2c1ba04590
MarshallDTeach <songxuwei@huawei.com>
2024-04-11 15:36:30 +0800

    fix error
    Signed-off-by: MarshallDTeach <songxuwei@huawei.com>

--- tools/vuinput/src/virtual_fingerprint_key.cpp
+++ tools/vuinput/src/virtual_fingerprint_key.cpp
@@ -21,10 +21,9 @@ namespace MMI {
 #define EVENT_FINGERPRINT_DOWN 121
 #define EVENT_FINGERPRINT_UP 122
 #define EVENT_FINGERPRINT_CLICK 123
-
-VirtualFingerprintKey::VirtualFingerprintKey() : VirtualDevice("FingerprintKey", BUS_SUB, 0x12d1, 0x10a5)
+VirtualFingerprintKey::VirtualFingerprintKey() : VirtualDevice("FingerprintKey", BUS_USB, 0x12d1, 0x10a5)
 {
-    eventTypes_ = {EV_KEY, EV_REL, EV_MSC};
+    eventTypes_ = {EV_KEY, EV_MSC};
     miscellaneous_ = { MSC_SCAN };
     keys_ = {
         EVENT_FINGERPRINT_DOWN,
