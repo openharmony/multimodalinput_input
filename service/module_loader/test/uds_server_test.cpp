0c695ea91a4429dcb5a379d1dccdafb7d01c49e7
double_tree_1 <zhoujiajun15@h-partners.com>
2024-04-11 18:56:18 +0800

    ut_up_two
    Signed-off-by: double_tree_1 <zhoujiajun15@h-partners.com>

--- service/module_loader/test/uds_server_test.cpp
+++ service/module_loader/test/uds_server_test.cpp
@@ -111,5 +111,261 @@ HWTEST_F(UDSServerTest, GetSession_003, TestSize.Level1)
     auto retResult = UDS_server.GetSession(fd);
     EXPECT_TRUE(retResult == nullptr);
 }
+
+/**
+ * @tc.name: UdsStop_001
+ * @tc.desc: Test the function UdsStop
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(UDSServerTest, UdsStop_001, TestSize.Level1)
+{
+    UDSServer UDS_server;
+    UDS_server.UdsStop();
+}
+
+/**
+ * @tc.name: GetClientPid_001
+ * @tc.desc: Test the function GetClientPid
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(UDSServerTest, GetClientPid_001, TestSize.Level1)
+{
+    UDSServer UDS_server;
+    int32_t fd = 1;
+    int32_t pid = 0;
+    pid = UDS_server.GetClientPid(fd);
+    EXPECT_EQ(pid, INVALID_PID);
+}
+
+/**
+ * @tc.name: AddSocketPairInfo_001
+ * @tc.desc: Test the function AddSocketPairInfo
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(UDSServerTest, AddSocketPairInfo_001, TestSize.Level1)
+{
+    UDSServer UDS_server;
+    const std::string programName = "program";
+    const int32_t moduleType = 1;
+    const int32_t uid = 2;
+    const int32_t pid = 10;
+    int32_t serverFd = 1;
+    int32_t toReturnClientFd = 1;
+    int32_t tokenType = 1;
+    int32_t ret = 0;
+    ret = UDS_server.AddSocketPairInfo(programName, moduleType, uid, pid, serverFd, toReturnClientFd, tokenType);
+    EXPECT_EQ(ret, RET_ERR);
+}
+
+/**
+ * @tc.name: SetFdProperty_001
+ * @tc.desc: Test the function SetFdProperty
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(UDSServerTest, SetFdProperty_001, TestSize.Level1)
+{
+    UDSServer UDS_server;
+    int32_t ret = RET_ERR;
+    int32_t tokenType = TokenType::TOKEN_NATIVE;
+    int32_t serverFd = 1;
+    int32_t toReturnClientFd = 2;
+    ret = UDS_server.SetFdProperty(tokenType, serverFd, toReturnClientFd);
+    EXPECT_EQ(ret, RET_ERR);
+}
+
+/**
+ * @tc.name: SetFdProperty_002
+ * @tc.desc: Test the function SetFdProperty
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(UDSServerTest, SetFdProperty_002, TestSize.Level1)
+{
+    UDSServer UDS_server;
+    int32_t ret = RET_ERR;
+    int32_t tokenType = TokenType::TOKEN_SHELL;
+    int32_t serverFd = 1;
+    int32_t toReturnClientFd = 2;
+    ret = UDS_server.SetFdProperty(tokenType, serverFd, toReturnClientFd);
+    EXPECT_EQ(ret, RET_ERR);
+}
+
+/**
+ * @tc.name: Dump_001
+ * @tc.desc: Test the function Dump
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(UDSServerTest, Dump_001, TestSize.Level1)
+{
+    UDSServer UDS_server;
+    int32_t fd = 1;
+    const std::vector<std::string> args = {"help"};
+    UDS_server.Dump(fd, args);
+}
+
+/**
+ * @tc.name: OnConnected_001
+ * @tc.desc: Test the function OnConnected
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(UDSServerTest, OnConnected_001, TestSize.Level1)
+{
+    UDSServer UDS_server;
+    SessionPtr sess;
+    UDS_server.OnConnected(sess);
+}
+
+/**
+ * @tc.name: SetRecvFun_001
+ * @tc.desc: Test the function SetRecvFun
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(UDSServerTest, SetRecvFun_001, TestSize.Level1)
+{
+    UDSServer UDS_server;
+    MsgServerFunCallback fun{ nullptr };
+    UDS_server.SetRecvFun(fun);
+}
+
+/**
+ * @tc.name: ReleaseSession_001
+ * @tc.desc: Test the function ReleaseSession
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(UDSServerTest, ReleaseSession_001, TestSize.Level1)
+{
+    UDSServer UDS_server;
+    int32_t fd = 1;
+    epoll_event ev;
+    UDS_server.ReleaseSession(fd,ev);
+}
+
+/**
+ * @tc.name: OnEpollRecv_001
+ * @tc.desc: Test the function OnEpollRecv
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(UDSServerTest, OnEpollRecv_001, TestSize.Level1)
+{
+    UDSServer UDS_server;
+    int32_t size = 100;
+    epoll_event ev;
+    int32_t fd = epoll_create(size);
+    UDS_server.OnEpollRecv(fd,ev);
+}
+
+/**
+ * @tc.name: OnEpollRecv_002
+ * @tc.desc: Test the function OnEpollRecv
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(UDSServerTest, OnEpollRecv_002, TestSize.Level1)
+{
+    UDSServer UDS_server;
+    epoll_event ev;
+    int32_t fd = -1;
+    UDS_server.OnEpollRecv(fd,ev);
+}
+
+/**
+ * @tc.name: AddEpollEvent_001
+ * @tc.desc: Test the function AddEpollEvent
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(UDSServerTest, AddEpollEvent_001, TestSize.Level1)
+{
+    UDSServer UDS_server;
+    std::shared_ptr<mmi_epoll_event> epollEvent=std::make_shared<mmi_epoll_event>();
+    int32_t fd = 1;
+    UDS_server.AddEpollEvent(fd,epollEvent);
+}
+
+/**
+ * @tc.name: DumpSession_001
+ * @tc.desc: Test the function DumpSession
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(UDSServerTest, DumpSession_001, TestSize.Level1)
+{
+    UDSServer UDS_server;
+    const std::string title = "test_title";
+    UDS_server.DumpSession(title);
+}
+
+/**
+ * @tc.name: AddSession_001
+ * @tc.desc: Test the function AddSession
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(UDSServerTest, AddSession_001, TestSize.Level1)
+{
+    UDSServer UDS_server;
+    SessionPtr sess = nullptr;
+    const std::string programName = "test";
+    const int32_t moduleType = 1;
+    int32_t serverFd = 1;
+    const int32_t uid = 3;
+    const int32_t pid = 5;
+    sess = std::make_shared<UDSSession>(programName, moduleType, serverFd, uid, pid);
+    UDS_server.AddSession(sess);
+}
+
+/**
+ * @tc.name: DelSession_001
+ * @tc.desc: Test the function DelSession
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(UDSServerTest, DelSession_001, TestSize.Level1)
+{
+    UDSServer UDS_server;
+    int32_t fd = -1;
+    UDS_server.DelSession(fd);
+}
+
+/**
+ * @tc.name: DelSession_002
+ * @tc.desc: Test the function DelSession
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(UDSServerTest, DelSession_002, TestSize.Level1)
+{
+    UDSServer UDS_server;
+    int32_t fd = 1;
+    UDS_server.DelSession(fd);
+}
+
+/**
+ * @tc.name: NotifySessionDeleted_001
+ * @tc.desc: Test the function NotifySessionDeleted
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(UDSServerTest, NotifySessionDeleted_001, TestSize.Level1)
+{
+    UDSServer UDS_server;
+    SessionPtr sess = nullptr;
+    const std::string programName = "test";
+    const int32_t moduleType = 1;
+    int32_t serverFd = 1;
+    const int32_t uid = 3;
+    const int32_t pid = 5;
+    sess = std::make_shared<UDSSession>(programName, moduleType, serverFd, uid, pid);
+    UDS_server.NotifySessionDeleted(sess);
+}
 } // namespace MMI
 } // namespace OHOS
