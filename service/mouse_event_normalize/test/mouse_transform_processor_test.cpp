49008d982164f89f2bc1229c190cba715f503fca
郭旭 <guoxu34@huawei.com>
2024-04-13 03:27:34 +0000

    update service/mouse_event_normalize/test/mouse_transform_processor_test.cpp.
    Signed-off-by: 郭旭 <guoxu34@huawei.com>

--- service/mouse_event_normalize/test/mouse_transform_processor_test.cpp
+++ service/mouse_event_normalize/test/mouse_transform_processor_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -18,6 +18,8 @@
 
 #include "libinput.h"
 #include "mouse_transform_processor.h"
+#include "window_info.h"
+#include "mouse_device_state.h"
 
 namespace OHOS {
 namespace MMI {
