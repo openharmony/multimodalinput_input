cac8074006af0a3144de9a0745cce1aae6d2de43
blueyouh <bailiang18@huawei.com>
2024-05-05 02:25:22 +0000

    accelerate cursor movement in remote side
    Signed-off-by: blueyouh <bailiang18@huawei.com>
    Change-Id: Ieab2dacb4d8323a46412b1f9db445546a2b1f927

--- interfaces/native/innerkits/event/include/input_event.h
+++ interfaces/native/innerkits/event/include/input_event.h
@@ -111,6 +111,9 @@ public:
      */
     static constexpr uint32_t EVENT_FLAG_HIDE_POINTER = 0x00000008;
 
+    static constexpr uint32_t EVENT_FLAG_RAW_POINTER_MOVEMENT = 0x00000010;
+    static constexpr uint32_t EVENT_FLAG_TOUCHPAD_POINTER = 0x00000020;
+
 public:
     /**
      * Copy constructor function for InputEvent
