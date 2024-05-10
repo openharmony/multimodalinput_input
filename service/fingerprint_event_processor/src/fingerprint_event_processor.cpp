2cf1e56d0ca95190f4755158c33635a7864eac75
Rosita <wangjing596@huawei.com>
2024-05-10 11:25:08 +0000

    service/fingerprint_event_processor~service/key_event_normalizer日志整改
    Signed-off-by: Rosita <wangjing596@huawei.com>
    Change-Id: I508bc46bcc3bbad835a80c21d2f5a630a336506c

--- service/fingerprint_event_processor/src/fingerprint_event_processor.cpp
+++ service/fingerprint_event_processor/src/fingerprint_event_processor.cpp
@@ -19,6 +19,8 @@
 #include "input_event_handler.h"
 #include "pointer_event.h"
 
+#undef MMI_LOG_DOMAIN
+#define MMI_LOG_DOMAIN MMI_LOG_DISPATCH
 #undef MMI_LOG_TAG
 #define MMI_LOG_TAG "FingerprintEventProcessor"
 
