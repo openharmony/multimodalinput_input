58120728dbdf83ccda1d61b793519dc126a5eaaf
15549494042 <dongzhengkuan@huawei.com>
2024-05-11 19:38:40 +0800

    Isolation cursor settings Singed-off-by:dongzhengkuan@huawei.com
    Signed-off-by: 15549494042 <dongzhengkuan@huawei.com>

--- service/module_loader/src/multimodal_input_preferences_manager.cpp
+++ service/module_loader/src/multimodal_input_preferences_manager.cpp
@@ -92,6 +92,10 @@ int32_t MultiModalInputPreferencesManager::GetPreferencesSettings()
     g_touchpadScrollSwitch = touchpadPref->GetBool(touchpadScrollSwitch, BOOL_DEFAULT);
     g_touchpadRightClickType = touchpadPref->GetInt(touchpadRightClickType, RIGHT_CLICK_TYPE);
     g_touchpadScrollDirection = touchpadPref->GetBool(touchpadScrollDirection, BOOL_DEFAULT);
+#ifdef OHOS_BUILD_ENABLE_MAGICCURSOR
+    g_magicPointerSize = mousePref->GetInt(magicPointerSize, POINTER_SIZE);
+    g_magicPointerColor = mousePref->GetInt(magicPointerColor, POINTER_COLOR);
+#endif // OHOS_BUILD_ENABLE_MAGICCURSOR
     NativePreferences::PreferencesHelper::RemovePreferencesFromCache(path + mouseFileName);
     NativePreferences::PreferencesHelper::RemovePreferencesFromCache(path + keyboarFileName);
     NativePreferences::PreferencesHelper::RemovePreferencesFromCache(path + touchpadFileName);
@@ -116,6 +120,10 @@ int32_t MultiModalInputPreferencesManager::InitPreferencesMap()
     preferencesMap[touchpadScrollSwitch] = {touchpadFileName, static_cast<int32_t>(g_touchpadScrollSwitch)};
     preferencesMap[touchpadRightClickType] = {touchpadFileName, g_touchpadRightClickType};
     preferencesMap[touchpadScrollDirection] = {touchpadFileName, static_cast<int32_t>(g_touchpadScrollDirection)};
+#ifdef OHOS_BUILD_ENABLE_MAGICCURSOR
+    preferencesMap[magicPointerSize] = {mouseFileName, g_magicPointerSize};
+    preferencesMap[magicPointerColor] = {mouseFileName, g_magicPointerColor};
+#endif // OHOS_BUILD_ENABLE_MAGICCURSOR
     return RET_OK;
 }
 
