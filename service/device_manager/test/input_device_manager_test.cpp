eef03ee44b116b7660a494bffef3eb319ed823b5
郭旭 <guoxu34@huawei.com>
2024-05-13 14:20:03 +0000

    update service/device_manager/test/input_device_manager_test.cpp.
    Signed-off-by: 郭旭 <guoxu34@huawei.com>

--- service/device_manager/test/input_device_manager_test.cpp
+++ service/device_manager/test/input_device_manager_test.cpp
@@ -38,10 +38,10 @@ public:
     static void TearDownTestCase(void) {}
 };
 
-class MockUDSSession: public UDSSession {
+class MockUDSSession : public UDSSession {
 public:
     MOCK_METHOD1(SendMsg, int32_t(NetPacket &));
-    MockUDSSession() : UDSSession("mockProgram", 1, 2, 3, 4) {}
+    MockUDSSession() : UDSSession("str", 1, 2, 3, 4) {}
 };
 
 
