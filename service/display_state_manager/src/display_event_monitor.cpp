b802adb8a94a9ddcb56c7af32b218d9504ccb4a8
yajunstrong <wangyajun29@huawei.com>
2024-05-14 11:49:26 +0000

    Delete redundant code
    Signed-off-by: yajunstrong <wangyajun29@huawei.com>
    Change-Id: I1abfc8e6cde84b07d3ef5f0416d1817d94186d24

--- service/display_state_manager/src/display_event_monitor.cpp
+++ service/display_state_manager/src/display_event_monitor.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -62,7 +62,6 @@ public:
             DISPLAY_MONITOR->UpdateShieldStatusOnScreenOff();
         } else {
             MMI_HILOGW("Screen changed receiver event: unknown");
-            return;
         }
     }
 };
