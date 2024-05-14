1072215da58ccf668f1577d9bd269f6646d77c19
郭旭 <guoxu34@huawei.com>
2024-05-14 02:06:34 +0000

    update service/device_manager/test/input_device_manager_test.cpp.
    Signed-off-by: 郭旭 <guoxu34@huawei.com>

--- service/device_manager/test/input_device_manager_test.cpp
+++ service/device_manager/test/input_device_manager_test.cpp
@@ -40,13 +40,9 @@ public:
 
 class MockUDSSession : public UDSSession {
 public:
-    std::string programName = "MyProgram";
-    int32_t moduleType = 1;
-    int32_t fd = 1;
-    int32_t uid = 1;
-    int32_t pid = 1;
-    MOCK_METHOD1(SendMsg, int32_t(NetPacket &));
-    MockUDSSession() : UDSSession(programName, moduleType, fd, uid, pid) {}
+    MockUDSSession(const std::string &programName, const int32_t moduleType,
+        const int32_t fd, const int32_t uid, const int32_t pid)
+            : UDSSession(programName, moduleType, fd, uid, pid) {}
 };
 
 
