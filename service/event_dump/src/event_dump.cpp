6962cc984783291fa1561382087604e0c906fb96
wanghao505 <wanghao505@huawei.com>
2024-05-26 05:43:02 +0000

    fix
    Signed-off-by: wanghao505 <wanghao505@huawei.com>
    Change-Id: Iad6ad4b10971abf9d7aafc728615d1881bc57287

--- service/event_dump/src/event_dump.cpp
+++ service/event_dump/src/event_dump.cpp
@@ -126,7 +126,7 @@ void EventDump::ParseCommand(int32_t fd, const std::vector<std::string> &args)
         }
     }
     optind = 1;
-    int32_t c;
+    int32_t c = 0;
     while ((c = getopt_long (args.size(), argv, "hdlwusoifmck", dumpOptions, &optionIndex)) != -1) {
         switch (c) {
             case 'h': {
