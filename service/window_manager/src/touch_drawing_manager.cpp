4a0bdaf962f54d6b17916451e54c667ba4a39111
Justin_Hu <hugongcheng@huawei.com>
2024-05-28 06:01:00 +0000

    update service/window_manager/src/touch_drawing_manager.cpp.
    Signed-off-by: Justin_Hu <hugongcheng@huawei.com>

--- service/window_manager/src/touch_drawing_manager.cpp
+++ service/window_manager/src/touch_drawing_manager.cpp
@@ -44,7 +44,7 @@ constexpr int32_t CALCULATE_MIDDLE = 2;
 constexpr int32_t DEFAULT_VALUE = -1;
 constexpr int32_t RECT_COUNT = 6;
 constexpr int32_t PHONE_RECT_TOP = 118;
-constexpr int32_t PAD_RECT_TOP = 118;
+constexpr int32_t PAD_RECT_TOP = 58;
 constexpr int32_t RECT_HEIGHT = 50;
 constexpr int32_t TEXT_TOP = 40;
 constexpr int32_t PEN_WIDTH = 1;
@@ -192,7 +192,7 @@ void TouchDrawingManager::UpdateDisplayInfo(const DisplayInfo& displayInfo)
     bubble_.outerCircleWidth = static_cast<float>(displayInfo.dpi * INDEPENDENT_WIDTH_PIXELS) / DENSITY_BASELINE;
     itemRectW_ = static_cast<double>(displayInfo_.width) / RECT_COUNT;
     if (displayInfo_.direction == DIRECTION0 || displayInfo_.direction == DIRECTION180) {
-        rectTopPosition_ = PRODUCT_TYPE == PRODUCT_PHONE ? PHONE_RECT_TOP : RECT_TOP
+        rectTopPosition_ = PRODUCT_TYPE == PRODUCT_PHONE ? PHONE_RECT_TOP : PAD_RECT_TOP
     } else {
         rectTopPosition_ = 0;
     }
