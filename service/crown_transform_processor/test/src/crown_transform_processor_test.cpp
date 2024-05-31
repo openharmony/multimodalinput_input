09d67716346aec2016688a5ae207602a3f298e6b
Dragon51 <longchao14@h-partners.com>
2024-05-31 09:02:48 +0800

    fix codecheck
    Signed-off-by: Dragon51 <longchao14@h-partners.com>

--- service/crown_transform_processor/test/src/crown_transform_processor_test.cpp
+++ service/crown_transform_processor/test/src/crown_transform_processor_test.cpp
@@ -16,6 +16,7 @@
 #include <cstdio>
 #include <gtest/gtest.h>
 
+#include "mmi_log.h"
 #include "libinput.h"
 #include "libinput_wrapper.h"
 #include "general_crown.h"
@@ -23,6 +24,9 @@
 #include "input_device_manager.h"
 #include "input_windows_manager.h"
 #include "crown_transform_processor.h"
+
+#undef MMI_LOG_TAG
+#define MMI_LOG_TAG "CrownTransformProcessorTest"
 
 namespace OHOS {
 namespace MMI {
@@ -42,8 +46,6 @@ public:
 private:
     static GeneralCrown vCrown_;
     static LibinputWrapper libinput_;
-
-    CrownTransformProcessor g_processor_ { 0 };
 };
 
 GeneralCrown CrownTransformProcessorTest::vCrown_;
@@ -64,7 +66,7 @@ void CrownTransformProcessorTest::TearDownTestCase(void)
 void CrownTransformProcessorTest::SetupCrown()
 {
     ASSERT_TRUE(vCrown_.SetUp());
-    std::cout << "device node name: " << vCrown_.GetDevPath() << std::endl;
+    MMI_HILOGD("device node name: %{public}s", vCrown_.GetDevPath().c_str());
     ASSERT_TRUE(libinput_.AddPath(vCrown_.GetDevPath()));
 
     libinput_event *event = libinput_.Dispatch();
@@ -115,8 +117,8 @@ void CrownTransformProcessorTest::TearDown()
  */
 HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_GetPointerEvent_001, TestSize.Level1)
 {
-    CrownTransformProcessor processor;
-    auto ret = processor.GetPointerEvent();
+    CALL_TEST_DEBUG;
+    auto ret = CROWN_EVENT_HDR->GetPointerEvent();
     ASSERT_NE(ret, nullptr);
 }
 
@@ -135,9 +137,9 @@ HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_IsCrownEvent_0
     ASSERT_TRUE(event != nullptr);
     struct libinput_device *dev = libinput_event_get_device(event);
     ASSERT_TRUE(dev != nullptr);
-    std::cout << "pointer device: " << libinput_device_get_name(dev) << std::endl;
-    CrownTransformProcessor processor;
-    ASSERT_TRUE(processor.IsCrownEvent(event));
+    std::string name = libinput_device_get_name(dev);
+    MMI_HILOGD("pointer device: %{public}s", name.c_str());
+    ASSERT_TRUE(CROWN_EVENT_HDR->IsCrownEvent(event));
 }
 
 
@@ -156,9 +158,9 @@ HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_NormalizeRotat
     ASSERT_TRUE(event != nullptr);
     struct libinput_device *dev = libinput_event_get_device(event);
     ASSERT_TRUE(dev != nullptr);
-    std::cout << "pointer device: " << libinput_device_get_name(dev) << std::endl;
-    CrownTransformProcessor processor;
-    int32_t result = processor.NormalizeRotateEvent(event);
+    std::string name = libinput_device_get_name(dev);
+    MMI_HILOGD("pointer device: %{public}s", name.c_str());
+    int32_t result = CROWN_EVENT_HDR->NormalizeRotateEvent(event);
     EXPECT_EQ(result, RET_OK);
 }
 
@@ -177,11 +179,11 @@ HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_HandleCrownRot
     ASSERT_TRUE(event != nullptr);
     struct libinput_device *dev = libinput_event_get_device(event);
     ASSERT_TRUE(dev != nullptr);
-    std::cout << "pointer device: " << libinput_device_get_name(dev) << std::endl;
+    std::string name = libinput_device_get_name(dev);
+    MMI_HILOGD("pointer device: %{public}s", name.c_str());
     struct libinput_event_pointer *rawPointerEvent = libinput_event_get_pointer_event(event);
     ASSERT_TRUE(rawPointerEvent != nullptr);
-    CrownTransformProcessor processor;
-    int32_t result = processor.HandleCrownRotateBegin(rawPointerEvent);
+    int32_t result = CROWN_EVENT_HDR->HandleCrownRotateBegin(rawPointerEvent);
     EXPECT_EQ(result, RET_OK);
 }
 
@@ -200,11 +202,11 @@ HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_HandleCrownRot
     ASSERT_TRUE(event != nullptr);
     struct libinput_device *dev = libinput_event_get_device(event);
     ASSERT_TRUE(dev != nullptr);
-    std::cout << "pointer device: " << libinput_device_get_name(dev) << std::endl;
+    std::string name = libinput_device_get_name(dev);
+    MMI_HILOGD("pointer device: %{public}s", name.c_str());
     struct libinput_event_pointer *rawPointerEvent = libinput_event_get_pointer_event(event);
     ASSERT_TRUE(rawPointerEvent != nullptr);
-    CrownTransformProcessor processor;
-    int32_t result = processor.HandleCrownRotateUpdate(rawPointerEvent);
+    int32_t result = CROWN_EVENT_HDR->HandleCrownRotateUpdate(rawPointerEvent);
     EXPECT_EQ(result, RET_OK);
 }
 
@@ -216,8 +218,8 @@ HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_HandleCrownRot
  */
 HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_HandleCrownRotateEnd_006, TestSize.Level1)
 {
-    CrownTransformProcessor processor;
-    ASSERT_NO_FATAL_FAILURE(processor.HandleCrownRotateEnd());
+    CALL_TEST_DEBUG;
+    ASSERT_NO_FATAL_FAILURE(CROWN_EVENT_HDR->HandleCrownRotateEnd());
 }
 
 /**
@@ -235,11 +237,11 @@ HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_HandleCrownRot
     ASSERT_TRUE(event != nullptr);
     struct libinput_device *dev = libinput_event_get_device(event);
     ASSERT_TRUE(dev != nullptr);
-    std::cout << "pointer device: " << libinput_device_get_name(dev) << std::endl;
+    std::string name = libinput_device_get_name(dev);
+    MMI_HILOGD("pointer device: %{public}s", name.c_str());
     struct libinput_event_pointer *rawPointerEvent = libinput_event_get_pointer_event(event);
     ASSERT_TRUE(rawPointerEvent != nullptr);
-    CrownTransformProcessor processor;
-    int32_t result = processor.HandleCrownRotateBeginAndUpdate(rawPointerEvent);
+    int32_t result = CROWN_EVENT_HDR->HandleCrownRotateBeginAndUpdate(rawPointerEvent);
     EXPECT_EQ(result, RET_OK);
 }
 
@@ -251,11 +253,11 @@ HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_HandleCrownRot
  */
 HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_HandleCrownRotatePostInner_008, TestSize.Level1)
 {
-    CrownTransformProcessor processor;
+    CALL_TEST_DEBUG;
     double angularVelocity = 67.0;
     double degree = 11.0;
     int32_t action = PointerEvent::POINTER_ACTION_AXIS_UPDATE;
-    ASSERT_NO_FATAL_FAILURE(processor.HandleCrownRotatePostInner(angularVelocity, degree, action));
+    ASSERT_NO_FATAL_FAILURE(CROWN_EVENT_HDR->HandleCrownRotatePostInner(angularVelocity, degree, action));
 }
 
 /**
@@ -266,8 +268,8 @@ HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_HandleCrownRot
  */
 HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_DumpInner_009, TestSize.Level1)
 {
-    CrownTransformProcessor processor;
-    ASSERT_NO_FATAL_FAILURE(processor.DumpInner());
+    CALL_TEST_DEBUG;
+    ASSERT_NO_FATAL_FAILURE(CROWN_EVENT_HDR->DumpInner());
 }
 
 /**
@@ -278,11 +280,11 @@ HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_DumpInner_009,
  */
 HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_Dump_010, TestSize.Level1)
 {
+    CALL_TEST_DEBUG;
     std::vector<std::string> args;
     std::vector<std::string> idNames;
-    CrownTransformProcessor processor;
     int32_t fd = 0;
-    processor.Dump(fd, args);
+    CROWN_EVENT_HDR->Dump(fd, args);
     ASSERT_EQ(args, idNames);
 }
 }
