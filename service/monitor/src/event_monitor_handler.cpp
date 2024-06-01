fefec87eeaa303f1ab6bc5a69e3acd6013acccb7
wanghao505 <wanghao505@huawei.com>
2024-06-01 09:01:03 +0000

    fix
    Signed-off-by: wanghao505 <wanghao505@huawei.com>
    Change-Id: Ifef9ac2a337d1750a6d20f609b3516deb6a3f1be

--- service/monitor/src/event_monitor_handler.cpp
+++ service/monitor/src/event_monitor_handler.cpp
@@ -36,11 +36,11 @@ namespace OHOS {
 namespace MMI {
 namespace {
 #ifdef OHOS_BUILD_ENABLE_TOUCH
-constexpr size_t MAX_EVENTIDS_SIZE = 1000;
+constexpr size_t MAX_EVENTIDS_SIZE { 1000 };
 #endif // OHOS_BUILD_ENABLE_TOUCH
-constexpr int32_t ACTIVE_EVENT = 2;
-constexpr int32_t REMOVE_OBSERVER = -2;
-constexpr int32_t UNOBSERVED = -1;
+constexpr int32_t ACTIVE_EVENT { 2 };
+constexpr int32_t REMOVE_OBSERVER { -2 };
+constexpr int32_t UNOBSERVED { -1 };
 } // namespace
 
 #ifdef OHOS_BUILD_ENABLE_KEYBOARD
