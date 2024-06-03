0d85bf8e53690f5e1d3bbe0581dcf5b328546754
wanghao505 <wanghao505@huawei.com>
2024-06-03 13:06:13 +0000

    fix
    Signed-off-by: wanghao505 <wanghao505@huawei.com>
    Change-Id: I9bcae719661c4474a67ef9b6458bee0023ae56a7

--- service/window_manager/src/knuckle_glow_point.cpp
+++ service/window_manager/src/knuckle_glow_point.cpp
@@ -25,16 +25,16 @@
 namespace OHOS {
 namespace MMI {
 namespace {
-constexpr int32_t SEC_TO_NANOSEC = 1000000000;
-constexpr int32_t NANOSECOND_TO_MILLISECOND = 1000000;
-constexpr int32_t PAINT_WIDTH = 20;
-constexpr int32_t ARGB_A = 0;
-constexpr int32_t ARGB_RGB = 255;
-constexpr double BASIC_LIFESPAN = 200.0f;
-constexpr int32_t TRACE_COLOR = 255;
-constexpr float BASIC_SIZE = 100.0f;
-constexpr int32_t ARGB_COLOR_ARRAY = 0x11c8ffff;
-constexpr double HALF = 2.0;
+constexpr int32_t SEC_TO_NANOSEC { 1000000000 };
+constexpr int32_t NANOSECOND_TO_MILLISECOND { 1000000 };
+constexpr int32_t PAINT_WIDTH { 20 };
+constexpr int32_t ARGB_A { 0 };
+constexpr int32_t ARGB_RGB { 255 };
+constexpr double BASIC_LIFESPAN { 200.0f };
+constexpr int32_t TRACE_COLOR { 255 };
+constexpr float BASIC_SIZE { 100.0f };
+constexpr int32_t ARGB_COLOR_ARRAY { 0x11c8ffff };
+constexpr double HALF { 2.0 };
 } // namespace
 
 KnuckleGlowPoint::KnuckleGlowPoint(std::shared_ptr<Rosen::Drawing::Bitmap> bitmap) : traceShadow_(bitmap) {}
