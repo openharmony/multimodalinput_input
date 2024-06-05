c885ef80dbf8c9985eff01b453060652b2294f4e
wanghao505 <wanghao505@huawei.com>
2024-06-05 07:42:36 +0000

    fix
    Signed-off-by: wanghao505 <wanghao505@huawei.com>
    Change-Id: Ied31ade37b059dc83028f0606873d532b25b9b0f

--- service/event_handler/test/input_event_handler_test.cpp
+++ service/event_handler/test/input_event_handler_test.cpp
@@ -15,13 +15,13 @@
 
 #include <gtest/gtest.h>
 #include <libinput.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #include <cinttypes>
 #include <cstdio>
 #include <cstring>
 #include <functional>
-#include <sys/stat.h>
-#include <unistd.h>
 #include <vector>
 
 #include "display_manager.h"
@@ -30,9 +30,9 @@
 #include "input_device_manager.h"
 #include "input_event_handler.h"
 #include "i_input_windows_manager.h"
+#include "key_command_handler.h"
 #include "libinput_wrapper.h"
 #include "mmi_log.h"
-#include "key_command_handler.h"
 #include "timer_manager.h"
 #include "util.h"
 
