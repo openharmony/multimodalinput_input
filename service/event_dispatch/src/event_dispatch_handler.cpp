af2567a23e51c9bf51559f25efdfe12af9e1a007
Rosita <wangjing596@huawei.com>
2024-06-05 07:45:51 +0000

    调整是事件上报间隔
    Signed-off-by: Rosita <wangjing596@huawei.com>
    Change-Id: I48340bb509ac450eb28a611562e773f13b65ffe2

--- service/event_dispatch/src/event_dispatch_handler.cpp
+++ service/event_dispatch/src/event_dispatch_handler.cpp
@@ -141,7 +141,8 @@ void EventDispatchHandler::NotifyPointerEventToRS(int32_t pointAction, const std
 bool EventDispatchHandler::AcquireEnableMark(std::shared_ptr<PointerEvent> event)
 {
     auto currentEventTime = std::chrono::high_resolution_clock::now();
-    int64_t tm64Cost = currentEventTime - LasteventBeginTime_;
+    int64_t tm64Cost = std::chrono::duration_cast<std::chrono::milliseconds>(
+        std::chrono::high_resolution_clock::now() - LasteventBeginTime).count();
 
     if (event->GetPointerAction() == PointerEvent::POINTER_ACTION_PULL_MOVE
         || event->GetPointerAction() == PointerEvent::POINTER_ACTION_MOVE) {
