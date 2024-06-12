3eeec247a059d03f289fdc9140dd0ab6dc603bec
郭旭 <guoxu34@huawei.com>
2024-06-12 12:52:14 +0000

    update test/fuzztest/stubgetmouseprimarybutton_fuzzer/stubgetmouseprimarybutton_fuzzer.cpp.
    Signed-off-by: 郭旭 <guoxu34@huawei.com>

--- test/fuzztest/stubgetmouseprimarybutton_fuzzer/stubgetmouseprimarybutton_fuzzer.cpp
+++ test/fuzztest/stubgetmouseprimarybutton_fuzzer/stubgetmouseprimarybutton_fuzzer.cpp
@@ -36,7 +36,8 @@ bool StubGetMousePrimaryButtonFuzzTest(const uint8_t* data, size_t size)
     }
     MessageParcel reply;
     MessageOption option;
-    DelayedSingleton<MMIService>::GetInstance()->OnRemoteRequest(static_cast<uint32_t>           (MMI::MultimodalinputConnectInterfaceCode::GET_MOUSE_PRIMARY_BUTTON), datas, reply, option);
+    DelayedSingleton<MMIService>::GetInstance()->OnRemoteRequest(
+        static_cast<uint32_t>(MultimodalinputConnectInterfaceCode::GET_MOUSE_PRIMARY_BUTTON), datas, reply, option);
     return true;
 }
 } // namespace OHOS
