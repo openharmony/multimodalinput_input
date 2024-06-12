59658a9bf13171fff5373e40723e6d837651c9f2
王瑞涛 <wangruitao10@huawei.com>
2024-06-12 13:35:17 +0000

    update test/fuzztest/stubsetkeyboardrepeatrate_fuzzer/stubsetkeyboardrepeatrate_fuzzer.cpp.
    Signed-off-by: 王瑞涛 <wangruitao10@huawei.com>

--- test/fuzztest/stubsetkeyboardrepeatrate_fuzzer/stubsetkeyboardrepeatrate_fuzzer.cpp
+++ test/fuzztest/stubsetkeyboardrepeatrate_fuzzer/stubsetkeyboardrepeatrate_fuzzer.cpp
@@ -37,7 +37,7 @@ bool StubSetKeyboardRepeatRateFuzzTest(const uint8_t* data, size_t size)
     MessageParcel reply;
     MessageOption option;
     DelayedSingleton<MMIService>::GetInstance()->OnRemoteRequest(
-        static_cast<uint32_t>(MMI::MultimodalinputConnectInterfaceCode::SET_KEYBOARD_REPEAT_RATE), datas, reply, option);
+        static_cast<uint32_t>(MultimodalinputConnectInterfaceCode::SET_KEYBOARD_REPEAT_RATE), datas, reply, option);
     return true;
 }
 } // namespace OHOS
