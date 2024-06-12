5e978d0f9ae02e30b2dc92de3f8162bd98b82769
duyufan <duyufan1@huawei.com>
2024-06-12 12:50:29 +0000

    update test/fuzztest/gettprightclicktype_fuzzer/gettprightclicktype_fuzzer.cpp.
    Signed-off-by: duyufan <duyufan1@huawei.com>

--- test/fuzztest/gettprightclicktype_fuzzer/gettprightclicktype_fuzzer.cpp
+++ test/fuzztest/gettprightclicktype_fuzzer/gettprightclicktype_fuzzer.cpp
@@ -27,7 +27,7 @@ namespace MMI {
 namespace OHOS {
 const std::u16string FORMMGR_INTERFACE_TOKEN { u"ohos.multimodalinput.IConnectManager" };
 
-bool GetTpRightClickTypeFuzzTest(const uint8_t* data, size_t size)aaa
+bool GetTpRightClickTypeFuzzTest(const uint8_t* data, size_t size)
 {
     MessageParcel datas;
     if (!datas.WriteInterfaceToken(FORMMGR_INTERFACE_TOKEN) || !datas.WriteBuffer(data, size) || !datas.RewindRead(0)) {
