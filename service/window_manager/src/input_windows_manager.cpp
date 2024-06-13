c8793b889b43b58658beec48977de963514e05b4
mayunteng_1 <mayunteng@huawei.com>
2024-06-13 08:19:00 +0000

    Signed-off-by: mayunteng_1 <mayunteng@huawei.com> Change-Id: I9ea5d0543107b6eee3974942b3f21b29b2ff671a

--- service/window_manager/src/input_windows_manager.cpp
+++ service/window_manager/src/input_windows_manager.cpp
@@ -3300,9 +3300,8 @@ void InputWindowsManager::PrintChangedWindowByEvent(int32_t eventType, const Win
     auto iter = lastMatchedWindow_.find(eventType);
     if (iter != lastMatchedWindow_.end() && iter->second.id != newWindowInfo.id) {
         MMI_HILOGI("Target window changed %{public}d %{public}d %{public}d %{public}f"
-            "%{public}d %{public}d %{public}f",
-            eventType, iter->second.id, iter->second.pid, iter->second.zOrder,
-            newWindowInfo.id, newWindowInfo.pid, newWindowInfo.zOrder);
+            "%{public}d %{public}d %{public}f", eventType, iter->second.id, iter->second.pid,
+            iter->second.zOrder, newWindowInfo.id, newWindowInfo.pid, newWindowInfo.zOrder);
     }
     lastMatchedWindow_[eventType] = newWindowInfo;
 }
