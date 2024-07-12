1e67f7cd3d20239d029613cced367a0c490e49ff
wanghao505 <wanghao505@huawei.com>
2024-07-12 13:08:46 +0000

    common
    Signed-off-by: wanghao505 <wanghao505@huawei.com>
    Change-Id: Iff19704740f12c4471cbf3d400d5e99523eabdd8

---
+++ intention/data/include/default_params.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef INTENTION_DEFAULT_PARAMS_H
+#define INTENTION_DEFAULT_PARAMS_H
+
+#include "intention_identity.h"
+
+namespace OHOS {
+namespace Msdp {
+namespace DeviceStatus {
+struct DefaultParam final : public ParamBase {
+    DefaultParam() = default;
+    explicit DefaultParam(int32_t userData);
+    bool Marshalling(MessageParcel &parcel) const override;
+    bool Unmarshalling(MessageParcel &parcel) override;
+
+    int32_t userData { -1 };
+};
+
+struct DefaultReply final : public ParamBase {
+    bool Marshalling(MessageParcel &parcel) const override;
+    bool Unmarshalling(MessageParcel &parcel) override;
+};
+
+struct BooleanReply final : public ParamBase {
+    BooleanReply() = default;
+    explicit BooleanReply(bool state);
+    bool Marshalling(MessageParcel &parcel) const override;
+    bool Unmarshalling(MessageParcel &parcel) override;
+    bool state { false };
+};
+} // namespace DeviceStatus
+} // namespace Msdp
+} // namespace OHOS
+#endif // INTENTION_DEFAULT_PARAMS_H
