61e82d7d4397881956a98bd244af6f8b38328cea
hewanglon <hewanglong@huawei.com>
2024-07-16 02:48:30 +0000

    repair the problems
    Signed-off-by: hewanglon <hewanglong@huawei.com>
    Change-Id: I99989567b7a8949bddab5b9d070c74ba1741a77f

--- service/subscriber/src/key_subscriber_handler.cpp
+++ service/subscriber/src/key_subscriber_handler.cpp
@@ -378,11 +378,11 @@ bool KeySubscriberHandler::OnSubscribeKeyEvent(std::shared_ptr<KeyEvent> keyEven
         return false;
     }
     if (IsRepeatedKeyEvent(keyEvent)) {
-        MMI_HILOGI("Repeat KeyEvent, skip");
+        MMI_HILOGD("Repeat KeyEvent, skip");
         return true;
     }
     if (keyGestureMgr_.Intercept(keyEvent)) {
-        MMI_HILOGI("Key gesture recognized");
+        MMI_HILOGD("Key gesture recognized");
         return true;
     }
     keyEvent_ = KeyEvent::Clone(keyEvent);
@@ -506,10 +506,9 @@ void KeySubscriberHandler::NotifyKeyDownRightNow(const std::shared_ptr<KeyEvent>
     std::list<std::shared_ptr<Subscriber>> &subscribers, bool &handled)
 {
     CALL_DEBUG_ENTER;
-    MMI_HILOGD("The subscribe list size is %{public}d", subscribers.size());
+    MMI_HILOGD("The subscribe list size is %{public}zu", subscribers.size());
     for (auto &subscriber : subscribers) {
         CHKPC(subscriber);
-        MMI_HILOGI("Subscribe id : %{public}d", subscriber->id_);
         auto sess = subscriber->sess_;
         CHKPC(sess);
         if (!isForegroundExits_ || keyEvent->GetKeyCode() == KeyEvent::KEYCODE_POWER ||
@@ -525,10 +524,9 @@ void KeySubscriberHandler::NotifyKeyDownDelay(const std::shared_ptr<KeyEvent> &k
     std::list<std::shared_ptr<Subscriber>> &subscribers, bool &handled)
 {
     CALL_DEBUG_ENTER;
-    MMI_HILOGD("The subscribe list size is %{public}d", subscribers.size());
+    MMI_HILOGD("The subscribe list size is %{public}zu", subscribers.size());
     for (auto &subscriber : subscribers) {
         CHKPC(subscriber);
-        MMI_HILOGI("Subscribe id : %{public}d", subscriber->id_);
         auto sess = subscriber->sess_;
         CHKPC(sess);
         if (!isForegroundExits_ || keyEvent->GetKeyCode() == KeyEvent::KEYCODE_POWER ||
