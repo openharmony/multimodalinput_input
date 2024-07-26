d834d0fee96e3dfe3033823f32c48ccef8a0ebc9
hewanglon <hewanglong@huawei.com>
2024-07-26 07:57:59 +0000

    logFix
    Signed-off-by: hewanglon <hewanglong@huawei.com>
    Change-Id: Iafe1653d4a5076dbc4ef8f68995bac8019aa5fd5

--- service/mouse_event_normalize/src/mouse_transform_processor.cpp
+++ service/mouse_event_normalize/src/mouse_transform_processor.cpp
@@ -663,9 +663,8 @@ void MouseTransformProcessor::DumpInner()
     EventLogHelper::PrintEventData(pointerEvent_, MMI_LOG_FREEZE);
     auto device = INPUT_DEV_MGR->GetInputDevice(pointerEvent_->GetDeviceId());
     CHKPV(device);
-    aggregator_.Record(MMI_LOG_FREEZE, "Pointer event created by: " + device->GetName() + ", target window: " +
-        std::to_string(pointerEvent_->GetTargetWindowId()) + ", action: " + pointerEvent_->DumpPointerAction(),
-        std::to_string(pointerEvent_->GetId()));
+    aggregator_.Record(MMI_LOG_FREEZE, device->GetName() + ", TW: " +
+        std::to_string(pointerEvent_->GetTargetWindowId()), std::to_string(pointerEvent_->GetId()));
 }
 
 DeviceType MouseTransformProcessor::CheckDeviceType(int32_t width, int32_t height)
