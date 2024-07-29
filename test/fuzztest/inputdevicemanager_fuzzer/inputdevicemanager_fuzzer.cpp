1c5252dda2af8bccfeee4fa04abe4b60eacf01f7
Rosita <wangjing596@huawei.com>
2024-07-29 14:26:51 +0000

    fix fuzz
    Signed-off-by: Rosita <wangjing596@huawei.com>
    Change-Id: If4d55cd23271fc16b14454442d2107002af745aa

--- test/fuzztest/inputdevicemanager_fuzzer/inputdevicemanager_fuzzer.cpp
+++ test/fuzztest/inputdevicemanager_fuzzer/inputdevicemanager_fuzzer.cpp
@@ -13,10 +13,9 @@
  * limitations under the License.
  */
 
-#include "inputdevicemanager_fuzzer.h"
-
 #include "mmi_log.h"
 #include "input_device_manager.h"
+#include "inputdevicemanager_fuzzer.h"
 
 #undef MMI_LOG_TAG
 #define MMI_LOG_TAG "InputDeviceManagerFuzzTest"
@@ -26,14 +25,7 @@ namespace MMI {
 namespace OHOS {
 bool InputDeviceManagerFuzzTest(const uint8_t *data, size_t size)
 {
-    const std::u16string FORMMGR_INTERFACE_TOKEN{ u"ohos.multimodalinput.IConnectManager" };
-    MessageParcel datas;
-    if (!datas.WriteInterfaceToken(FORMMGR_INTERFACE_TOKEN) || !datas.WriteBuffer(data, size) || !datas.RewindRead(0)) {
-        return false;
-    }
-
     int32_t id = 1;
-    int32_t fd = 1;
     int32_t deviceId = 1;
     int32_t keyboardType = 1;
     std::string type = "hello";
@@ -42,65 +34,52 @@ bool InputDeviceManagerFuzzTest(const uint8_t *data, size_t size)
     bool hasPointerDevice = true;
     bool isVisible = true;
     bool isHotPlug = true;
-    struct libinput_device *device = nullptr;
     libinput_device * deviceOrigin = nullptr;
-    std::vector<int32_t> keyCodes;
-    keyCodes.push_back(KeyEvent::KEYCODE_T);
-    keyCodes.push_back(KeyEvent::KEYCODE_NUMPAD_1);
+    std::shared_ptr<InputDevice> inputDevice;
+    std::shared_ptr<IDeviceObserver> observer;
+    std::shared_ptr<InputDevice> devicePtr(new InputDevice());
+    struct libinput_device *struct_device = nullptr;
     std::vector<int32_t> keyCodes = {1};
     std::vector<bool> keystroke = {true};
     std::vector<std::string> args = {"hello"};
     SessionPtr session;
-    SessionPtr sess;
-    struct InputDeviceInfo inDevice;
-    shared_ptr<IDeviceObserver> observer = nullptr;
+    InputDeviceManager::InputDeviceInfo inDevice;
+
     INPUT_DEV_MGR->GetInputDevice(deviceId, checked);
-    INPUT_DEV_MGR->FillInputDevice(std::shared_ptr<InputDevice> inputDevice, deviceOrigin);
+    INPUT_DEV_MGR->FillInputDevice(inputDevice, deviceOrigin);
     INPUT_DEV_MGR->GetInputDeviceIds();
     INPUT_DEV_MGR->SupportKeys(deviceId, keyCodes, keystroke);
-    INPUT_DEV_MGR->IsMatchKeys(device, keyCodes);
+    INPUT_DEV_MGR->IsMatchKeys(struct_device, keyCodes);
     INPUT_DEV_MGR->GetDeviceConfig(deviceId, keyboardType);
     INPUT_DEV_MGR->GetKeyboardBusMode(deviceId);
     INPUT_DEV_MGR->GetDeviceSupportKey(deviceId, keyboardType);
     INPUT_DEV_MGR->GetKeyboardType(deviceId, keyboardType);
-    INPUT_DEV_MGR->AddDevListener(sess);
-    INPUT_DEV_MGR->RemoveDevListener(sess);
-    ifdef OHOS_BUILD_ENABLE_POINTER_DRAWING
+    INPUT_DEV_MGR->AddDevListener(session);
+    INPUT_DEV_MGR->RemoveDevListener(session);
+    #ifdef OHOS_BUILD_ENABLE_POINTER_DRAWING
     INPUT_DEV_MGR->HasPointerDevice();
-    endif // OHOS_BUILD_ENABLE_POINTER_DRAWING
+    #endif // OHOS_BUILD_ENABLE_POINTER_DRAWING
     INPUT_DEV_MGR->HasTouchDevice();
-    INPUT_DEV_MGR->GetInputIdentification(device);
     INPUT_DEV_MGR->NotifyDevCallback(deviceId, inDevice);
-    INPUT_DEV_MGR->ParseDeviceId(device);
-    INPUT_DEV_MGR->OnInputDeviceAdded(device);
-    INPUT_DEV_MGR->MakeDeviceInfo(device, struct InputDeviceInfo & info);
-    INPUT_DEV_MGR->OnInputDeviceRemoved(device);
     INPUT_DEV_MGR->ScanPointerDevice();
-    INPUT_DEV_MGR->IsPointerDevice(device);
-    INPUT_DEV_MGR->IsKeyboardDevice(device);
-    INPUT_DEV_MGR->IsTouchDevice(device);
-    INPUT_DEV_MGR->Attach(std::shared_ptr<IDeviceObserver> observer);
-    INPUT_DEV_MGR->Detach(std::shared_ptr<IDeviceObserver> observer);
+    INPUT_DEV_MGR->IsKeyboardDevice(devicePtr);
+    INPUT_DEV_MGR->Attach(observer);
+    INPUT_DEV_MGR->Detach(observer);
     INPUT_DEV_MGR->NotifyPointerDevice(hasPointerDevice, isVisible, isHotPlug);
-    INPUT_DEV_MGR->FindInputDeviceId(device);
-    INPUT_DEV_MGR->Dump(fd, args);
-    INPUT_DEV_MGR->DumpDeviceList(fd, args);
-    INPUT_DEV_MGR->IsRemote(device);
     INPUT_DEV_MGR->IsRemote(id);
     INPUT_DEV_MGR->GetVendorConfig(deviceId);
     INPUT_DEV_MGR->OnEnableInputDevice(enable);
-    INPUT_DEV_MGR->AddVirtualInputDevice(std::shared_ptr<InputDevice> device, deviceId);
+    INPUT_DEV_MGR->AddVirtualInputDevice(devicePtr, deviceId);
     INPUT_DEV_MGR->RemoveVirtualInputDevice(deviceId);
-    INPUT_DEV_MGR->MakeVirtualDeviceInfo(std::shared_ptr<InputDevice> device, InputDeviceInfo & deviceInfo);
+    INPUT_DEV_MGR->MakeVirtualDeviceInfo(devicePtr, inDevice);
     INPUT_DEV_MGR->GenerateVirtualDeviceId(deviceId);
-    INPUT_DEV_MGR->NotifyDevRemoveCallback(deviceId, const InputDeviceInfo &deviceInfo);
+    INPUT_DEV_MGR->NotifyDevRemoveCallback(deviceId, inDevice);
     INPUT_DEV_MGR->NotifyMessage(session, id, type);
     INPUT_DEV_MGR->InitSessionLostCallback();
     INPUT_DEV_MGR->OnSessionLost(session);
     INPUT_DEV_MGR->GetTouchPadIds();
-    INPUT_DEV_MGR->IsPointerDevice(std::shared_ptr<InputDevice> inputDevice);
-    INPUT_DEV_MGR->IsTouchableDevice(std::shared_ptr<InputDevice> inputDevice);
-    INPUT_DEV_MGR->IsKeyboardDevice(std::shared_ptr<InputDevice> inputDevice);
+    INPUT_DEV_MGR->IsTouchableDevice(devicePtr);
+    return true;
 }
 } // namespace OHOS
 
--- test/fuzztest/stubhandleallocsocketfd_fuzzer/stubhandleallocsocketfd_fuzzer.cpp
+++ test/fuzztest/stubhandleallocsocketfd_fuzzer/stubhandleallocsocketfd_fuzzer.cpp
@@ -15,17 +15,55 @@
 
 #include "stubhandleallocsocketfd_fuzzer.h"
 
-#include "singleton.h"
-
 #include "mmi_service.h"
 #include "multimodal_input_connect_stub.h"
 
 #undef MMI_LOG_TAG
 #define MMI_LOG_TAG "StubHandleAllocSocketFdFuzzTest"
 
+class UDSSession;
+using SessionPtr = std::shared_ptr<UDSSession>;
+
 namespace OHOS {
 namespace MMI {
 const std::u16string FORMMGR_INTERFACE_TOKEN { u"ohos.multimodalinput.IConnectManager" };
+EpollEventType event_type = EPOLL_EVENT_SIGNAL;
+InputHandlerType handlerType = NONE;
+HandleEventType eventType = HANDLE_EVENT_TYPE_NONE;
+int32_t tmpfd = 1;
+uint32_t tmp32 = 1;
+int32_t type = 1;
+int64_t number = 1;
+int32_t infoId = 1;
+int32_t userId = 1;
+int32_t deviceId = 1;
+uint32_t frameCount = 1;
+uint32_t vsyncCount = 1;
+int64_t infraredFrequency = 1;
+std::vector<int32_t> vec = {1,2,3};
+std::vector<bool> vec_bool = {1};
+std::vector<std::u16string> args = {u"hello", u"worid"};
+std::vector<int64_t> pattern = {1,2,3};
+std::map<int32_t, int32_t> mp = {
+    {1,2},{2,2},{3,2}
+};
+std::vector<InfraredFrequency> requencys = {{1,2},{2,2}};
+int tmpdate = 1;
+void* pixelMap = &tmpdate;
+bool isAuthorize = true;
+bool isNativeInject = true;
+bool switchFlag = true;
+bool enable = true;
+bool rotateSwitch = true;
+bool state = true;
+std::string msg = "hello";
+std::string businessId = "hello";
+std::string flag = "hello";
+std::shared_ptr<InputDevice> inputDevice;
+std::shared_ptr<InputDevice> device;
+std::shared_ptr<PointerEvent> pointerEvent;
+std::shared_ptr<KeyOption> p_option;
+std::shared_ptr<KeyEvent> keyEvent;
 
 bool StubHandleAllocSocketFdFuzzTest(const uint8_t *data, size_t size)
 {
@@ -36,6 +74,81 @@ bool StubHandleAllocSocketFdFuzzTest(const uint8_t *data, size_t size)
     }
     MessageParcel reply;
     MessageOption option;
+    MMIService::GetInstance()->AddEpoll(event_type, tmpfd);
+    MMIService::GetInstance()->DelEpoll(event_type, tmpfd);
+    MMIService::GetInstance()->InitLibinputService();
+    MMIService::GetInstance()->InitDelegateTasks();
+    MMIService::GetInstance()->AddAppDebugListener();
+    MMIService::GetInstance()->SetMouseScrollRows(tmpfd);
+    MMIService::GetInstance()->SetMouseIcon(tmpfd, pixelMap);
+    MMIService::GetInstance()->ReadMouseScrollRows(tmpfd);
+    MMIService::GetInstance()->MarkProcessed(tmpfd, tmpfd);
+    MMIService::GetInstance()->ReadPointerColor(tmpfd);
+    MMIService::GetInstance()->OnSupportKeys(tmpfd, vec, vec_bool);
+    MMIService::GetInstance()->OnGetDeviceIds(vec);
+    MMIService::GetInstance()->OnGetDevice(tmpfd, inputDevice);
+    MMIService::GetInstance()->OnGetKeyboardType(tmpfd, tmpfd);
+    MMIService::GetInstance()->SetKeyboardRepeatDelay(tmpfd);
+    MMIService::GetInstance()->CheckRemoveInput(tmpfd, handlerType, eventType, tmpfd, tmp32);
+    MMIService::GetInstance()->RemoveInputHandler(handlerType, eventType, tmpfd, tmp32);
+    MMIService::GetInstance()->MarkEventConsumed(tmpfd);
+    MMIService::GetInstance()->MoveMouseEvent(tmpfd, tmpfd);
+    MMIService::GetInstance()->InjectKeyEvent(keyEvent, isNativeInject);
+    MMIService::GetInstance()->CheckInjectKeyEvent(keyEvent, tmpfd, isNativeInject);
+    MMIService::GetInstance()->OnGetKeyState(vec, mp);
+    MMIService::GetInstance()->AdaptScreenResolution(pointerEvent);
+    MMIService::GetInstance()->InjectPointerEvent(pointerEvent, isNativeInject);
+    MMIService::GetInstance()->OnAddSystemAbility(tmpfd, "deviceId");
+    MMIService::GetInstance()->SubscribeKeyEvent(tmpfd, p_option);
+    MMIService::GetInstance()->UnsubscribeKeyEvent(tmpfd);
+    MMIService::GetInstance()->SubscribeSwitchEvent(tmpfd, tmpfd);
+    MMIService::GetInstance()->SetDisplayBind(tmpfd, tmpfd, msg);
+    MMIService::GetInstance()->SetFunctionKeyState(tmpfd, enable);
+    MMIService::GetInstance()->SetPointerLocation(tmpfd, tmpfd);
+    MMIService::GetInstance()->AddReloadDeviceTimer();
+    MMIService::GetInstance()->Dump(tmpfd, args);
+    MMIService::GetInstance()->OnGetWindowPid(tmpfd, tmpfd);
+    MMIService::GetInstance()->GetWindowPid(tmpfd);
+    MMIService::GetInstance()->SetKeyDownDuration(businessId, tmpfd);
+    MMIService::GetInstance()->ReadTouchpadScrollSwich(switchFlag);
+    MMIService::GetInstance()->ReadTouchpadScrollDirection(switchFlag);
+    MMIService::GetInstance()->ReadTouchpadtapSwitch(switchFlag);
+    MMIService::GetInstance()->ReadTouchpadPointerSpeed(tmpfd);
+    MMIService::GetInstance()->ReadTouchpadPinchSwitch(switchFlag);
+    MMIService::GetInstance()->ReadTouchpadSwipeSwitch(switchFlag);
+    MMIService::GetInstance()->ReadTouchpadRightMenuType(tmpfd);
+    MMIService::GetInstance()->ReadTouchpadRotateSwitch(rotateSwitch);
+    MMIService::GetInstance()->SetTouchpadScrollSwitch(switchFlag);
+    MMIService::GetInstance()->GetTouchpadScrollSwitch(switchFlag);
+    MMIService::GetInstance()->SetTouchpadScrollDirection(state);
+    MMIService::GetInstance()->GetTouchpadScrollDirection(state);
+    MMIService::GetInstance()->SetTouchpadTapSwitch(switchFlag);
+    MMIService::GetInstance()->GetTouchpadTapSwitch(switchFlag);
+    MMIService::GetInstance()->SetTouchpadPointerSpeed(tmpfd);
+    MMIService::GetInstance()->GetTouchpadPointerSpeed(tmpfd);
+    MMIService::GetInstance()->SetTouchpadPinchSwitch(switchFlag);
+    MMIService::GetInstance()->GetTouchpadPinchSwitch(switchFlag);
+    MMIService::GetInstance()->SetTouchpadSwipeSwitch(switchFlag);
+    MMIService::GetInstance()->GetTouchpadSwipeSwitch(switchFlag);
+    MMIService::GetInstance()->SetTouchpadRightClickType(type);
+    MMIService::GetInstance()->SetTouchpadRotateSwitch(rotateSwitch);
+    MMIService::GetInstance()->GetTouchpadRotateSwitch(rotateSwitch);
+    MMIService::GetInstance()->GetKeyState(vec, mp);
+    MMIService::GetInstance()->Authorize(isAuthorize);
+    MMIService::GetInstance()->OnAuthorize(isAuthorize);
+    MMIService::GetInstance()->CancelInjection();
+    MMIService::GetInstance()->OnCancelInjection();
+    MMIService::GetInstance()->GetInfraredFrequencies(requencys);
+    MMIService::GetInstance()->TransmitInfrared(number, pattern);
+    MMIService::GetInstance()->OnGetInfraredFrequencies(requencys);
+    MMIService::GetInstance()->OnTransmitInfrared(infraredFrequency, pattern);
+    MMIService::GetInstance()->SetPixelMapData(infoId, pixelMap);
+    MMIService::GetInstance()->SetCurrentUser(userId);
+    MMIService::GetInstance()->AddVirtualInputDevice(device, deviceId);
+    MMIService::GetInstance()->RemoveVirtualInputDevice(tmpfd);
+    MMIService::GetInstance()->EnableHardwareCursorStats(enable);
+    MMIService::GetInstance()->GetHardwareCursorStats(frameCount, vsyncCount);
+    MMIService::GetInstance()->GetPointerSnapshot(pixelMap);
     MMIService::GetInstance()->state_ = ServiceRunningState::STATE_RUNNING;
     MMIService::GetInstance()->OnRemoteRequest(
         static_cast<uint32_t>(MultimodalinputConnectInterfaceCode::ALLOC_SOCKET_FD), datas, reply, option);
