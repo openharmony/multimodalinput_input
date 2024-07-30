6d597e9e372dadbb9652a1b44856e9384de788d6
Dragon51 <longchao14@h-partners.com>
2024-07-30 11:35:22 +0000

    多模输入支持触控板右键区域范围产品自定义CCM配置项
    Signed-off-by: Dragon51 <longchao14@h-partners.com>

--- service/libinput_adapter/include/libinput_adapter.h
+++ service/libinput_adapter/include/libinput_adapter.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -49,6 +49,7 @@ private:
     void OnEventHandler();
     void OnDeviceAdded(std::string path);
     void OnDeviceRemoved(std::string path);
+    void InitRightButtonAreaConfig();
 
     int32_t fd_ { -1 };
     libinput *input_ { nullptr };
