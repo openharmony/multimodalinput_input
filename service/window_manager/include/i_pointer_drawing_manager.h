c617381619147eb6ee77c38653c86d812eb13b83
Dragon51 <longchao14@h-partners.com>
2024-08-01 07:59:32 +0000

    智能表小型化裁剪适配
    Signed-off-by: Dragon51 <longchao14@h-partners.com>
    Change-Id: Ic72840b7fd50c7ac89dac30dbbe9669dfc8e3cab

--- service/window_manager/include/i_pointer_drawing_manager.h
+++ service/window_manager/include/i_pointer_drawing_manager.h
@@ -135,7 +135,7 @@ public:
     {
         return 0;
     }
-    virtual void ForceClearPointerVisiableStatus();
+    virtual void ForceClearPointerVisiableStatus() {}
     virtual void InitPointerObserver() {}
     virtual void OnSessionLost(int32_t pid) {}
     virtual int32_t SkipPointerLayer(bool isSkip)
