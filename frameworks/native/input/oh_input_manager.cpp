08bdf3647a0741a9b5a786aafcb46322c84c7984
mr-liang2504 <liangzhicheng10@huawei.com>
2024-08-07 07:45:38 +0000

    Signed-off-by: mr-liang2504 <liangzhicheng10@huawei.com>
    Change-Id: Ic1676a9e3ee5f6e28aa4e0014b608735183c22c3

--- frameworks/native/input/oh_input_manager.cpp
+++ frameworks/native/input/oh_input_manager.cpp
@@ -88,6 +88,12 @@ static int32_t g_keyMonitorId = INVALID_MONITOR_ID;
 static int32_t g_pointerMonitorId = INVALID_MONITOR_ID;
 static int32_t g_keyInterceptorId = INVALID_INTERCEPTOR_ID;
 static int32_t g_pointerInterceptorId = INVALID_INTERCEPTOR_ID;
+static const std::set<int32_t> g_keyCodeValueSet = {
+    KEYCODE_DPAD_UP, KEYCODE_DPAD_DOWN, KEYCODE_DPAD_LEFT, KEYCODE_DPAD_RIGHT, KEYCODE_ALT_LEFT, KEYCODE_ALT_RIGHT,
+    KEYCODE_SHIFT_LEFT, KEYCODE_SHIFT_RIGHT, KEYCODE_TAB, KEYCODE_CTRL_LEFT, KEYCODE_CTRL_RIGHT, KEYCODE_CAPS_LOCK,
+    KEYCODE_SCROLL_LOCK, KEYCODE_F1, KEYCODE_F2, KEYCODE_F3, KEYCODE_F4, KEYCODE_F5, KEYCODE_F6, KEYCODE_F7,
+    KEYCODE_F8, KEYCODE_F9, KEYCODE_F10, KEYCODE_F11, KEYCODE_F12, KEYCODE_NUM_LOCK
+};
 
 Input_Result OH_Input_GetKeyState(struct Input_KeyState* keyState)
 {
@@ -95,6 +101,10 @@ Input_Result OH_Input_GetKeyState(struct Input_KeyState* keyState)
     CHKPR(keyState, INPUT_PARAMETER_ERROR);
     if (keyState->keyCode < 0 || keyState->keyCode > KEYCODE_NUMPAD_RIGHT_PAREN) {
         MMI_HILOGE("keyCode is invalid, keyCode:%{public}d", keyState->keyCode);
+        return INPUT_PARAMETER_ERROR;
+    }
+    if (g_keyCodeValueSet.find(keyState->keyCode) == g_keyCodeValueSet.end()) {
+        MMI_HILOGE("keyCode is not within the query range, keyCode:%{public}d", keyState->keyCode);
         return INPUT_PARAMETER_ERROR;
     }
     std::vector<int32_t> pressedKeys;
