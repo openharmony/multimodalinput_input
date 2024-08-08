7e10e9bc869a152c89ff68a67b8bf78d25cdb8ed
wuliangdong <wuliangdong1@huawei.com>
2024-08-08 12:41:09 +0000

    Remove Expire Interface:RnableInputDEvice
    Signed-off-by: wuliangdong <wuliangdong1@huawei.com>
    Change-Id: I6a395f2a7cc5d387df55b528fa7da4a5dc576e9e

--- service/connect_manager/src/multimodal_input_connect_stub.cpp
+++ service/connect_manager/src/multimodal_input_connect_stub.cpp
@@ -1697,17 +1697,8 @@ int32_t MultimodalInputConnectStub::StubEnableCombineKey(MessageParcel& data, Me
 int32_t MultimodalInputConnectStub::StubEnableInputDevice(MessageParcel& data, MessageParcel& reply)
 {
     CALL_DEBUG_ENTER;
-    if (!IsRunning()) {
-        MMI_HILOGE("Service is not running");
-        return MMISERVICE_NOT_RUNNING;
-    }
-    bool enable;
-    READBOOL(data, enable, IPC_PROXY_DEAD_OBJECT_ERR);
-    int32_t ret = EnableInputDevice(enable);
-    if (ret != RET_OK) {
-        MMI_HILOGE("Call EnableInputDevice failed, ret:%{public}d", ret);
-    }
-    return ret;
+    MMI_HILOGW("EnableInputDevice is not supported yet");
+    return RET_OK;
 }
 
 int32_t MultimodalInputConnectStub::StubSetKeyDownDuration(MessageParcel& data, MessageParcel& reply)
