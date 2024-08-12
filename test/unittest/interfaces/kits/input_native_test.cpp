72117552c12812116a38e32ba57d62cfee9dc17e
郭旭 <guoxu34@huawei.com>
2024-08-12 07:53:55 +0000

    8.12处理失败用例和mock次数错误问题
    Signed-off-by: 郭旭 <guoxu34@huawei.com>

--- test/unittest/interfaces/kits/input_native_test.cpp
+++ test/unittest/interfaces/kits/input_native_test.cpp
@@ -126,8 +126,8 @@ HWTEST_F(InputNativeTest, InputNativeTest_GetKeyState_001, TestSize.Level1)
     OH_Input_SetKeyCode(keyState, 22);
     OH_Input_GetKeyState(keyState);
     ASSERT_EQ(OH_Input_GetKeyPressed(keyState), KEY_PRESSED);
-    ASSERT_EQ(OH_Input_GetKeySwitch(keyState), KEY_DEFAULT);
-    ASSERT_EQ(OH_Input_GetKeyState(keyState), INPUT_SUCCESS);
+    ASSERT_EQ(OH_Input_GetKeySwitch(keyState), KEY_PRESSED);
+    ASSERT_EQ(OH_Input_GetKeyState(keyState), INPUT_PARAMETER_ERROR);
     OH_Input_DestroyKeyState(&keyState);
 }
 
