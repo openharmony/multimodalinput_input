2ee416f6cc4af3f27c8d2f9d5d8d443b7d80fd90
hellohyh001 <huiyuehong@huawei.com>
2024-08-16 10:17:04 +0000

    Get the time interval interface of the system since the last input
    Signed-off-by: hellohyh001 <huiyuehong@huawei.com>
    Change-Id: I9165855b6c758864ff59e2914aa60d580e7138a7

--- frameworks/napi/input_device/src/js_input_device_context.cpp
+++ frameworks/napi/input_device/src/js_input_device_context.cpp
@@ -713,6 +713,7 @@ napi_value JsInputDeviceContext::Export(napi_env env, napi_value exports)
         DECLARE_NAPI_STATIC_FUNCTION("setKeyboardRepeatRate", SetKeyboardRepeatRate),
         DECLARE_NAPI_STATIC_FUNCTION("getKeyboardRepeatDelay", GetKeyboardRepeatDelay),
         DECLARE_NAPI_STATIC_FUNCTION("getKeyboardRepeatRate", GetKeyboardRepeatRate),
+        DECLARE_NAPI_STATIC_FUNCTION("getIntervalSinceLastInput", GetIntervalSinceLastInput),
     };
     CHKRP(napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc), DEFINE_PROPERTIES);
     CHKPP(CreateEnumKeyboardType(env, exports));
