f1ffde02f8e632e0eb89e9de7e67756d7dd97978
hellohyh001 <huiyuehong@huawei.com>
2024-08-16 10:22:43 +0000

    Get the time interval interface of the system since the last input
    Signed-off-by: hellohyh001 <huiyuehong@huawei.com>
    Change-Id: I175081394f863706488fe28411f29adb5de1626b

--- test/fuzztest/getintervalsincelastinput_fuzzer/getintervalsincelastinput_fuzzer.cpp
+++ test/fuzztest/getintervalsincelastinput_fuzzer/getintervalsincelastinput_fuzzer.cpp
@@ -41,7 +41,7 @@ size_t GetObject(const uint8_t *data, size_t size, T &object)
 
 void GetIntervalSinceLastInputFuzzTest(const uint8_t* data, size_t size)
 {
-    int64_t timeInterval = 0;
+    int64_t timeInterval = -1;
     InputManager::GetInstance()->GetIntervalSinceLastInput(timeInterval);
 }
 } // namespace MMI
