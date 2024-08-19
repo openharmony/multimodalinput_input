63da786462d0bdbc728f15e32baf8be9365b8a29
hellohyh001 <huiyuehong@huawei.com>
2024-08-19 03:09:54 +0000

    notify state error
    Signed-off-by: hellohyh001 <huiyuehong@huawei.com>
    Change-Id: I008b58feba8ee24e6f5497cf103fe51f36924f9f

--- service/window_manager/src/input_windows_manager.cpp
+++ service/window_manager/src/input_windows_manager.cpp
@@ -256,6 +256,14 @@ const std::vector<WindowInfo> &InputWindowsManager::GetWindowGroupInfoByDisplayI
     return iter->second.windowsInfo;
 }
 
+bool InputWindowsManager::GetCancelEventFlag(std::shared_ptr<PointerEvent> pointerEvent)
+{
+    auto iter = touchItemDownInfos_.find(pointerEvent->GetPointerId());
+    if (iter != touchItemDownInfos_.end()) {
+        return iter->second.flag;
+    }
+}
+
 #if defined(OHOS_BUILD_ENABLE_POINTER) || defined(OHOS_BUILD_ENABLE_TOUCH)
 int32_t InputWindowsManager::GetClientFd(std::shared_ptr<PointerEvent> pointerEvent)
 {
