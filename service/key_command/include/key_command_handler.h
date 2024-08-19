7b484ffe3452499ee574b49723752639981eec08
mayunteng_1 <mayunteng@huawei.com>
2024-08-19 03:37:40 +0000

    修复输入法窗口内禁止指关节功能
    Signed-off-by: mayunteng_1 <mayunteng@huawei.com>
    Change-Id: I7b216e645ee04f268080129cff7764fb672e564d

--- service/key_command/include/key_command_handler.h
+++ service/key_command/include/key_command_handler.h
@@ -189,6 +189,7 @@ public:
     void SetKnuckleDoubleTapIntervalTime(int64_t interval);
     void SetKnuckleDoubleTapDistance(float distance);
     bool GetKnuckleSwitchValue();
+    bool CheckInputMethodArea(const std::shared_ptr<PointerEvent> touchEvent);
 #ifdef OHOS_BUILD_ENABLE_KEYBOARD
     bool OnHandleEvent(const std::shared_ptr<KeyEvent> keyEvent);
     int32_t SetIsFreezePowerKey(const std::string pageName);
@@ -285,7 +286,6 @@ private:
     void StopTwoFingerGesture();
     bool CheckTwoFingerGestureAction() const;
 #endif // OHOS_BUILD_ENABLE_GESTURESENSE_WRAPPER
-    bool CheckInputMethodArea(const std::shared_ptr<PointerEvent> touchEvent);
 #ifdef OHOS_BUILD_ENABLE_GESTURESENSE_WRAPPER
     void HandleFingerGestureDownEvent(const std::shared_ptr<PointerEvent> touchEvent);
     void HandleFingerGestureUpEvent(const std::shared_ptr<PointerEvent> touchEvent);
