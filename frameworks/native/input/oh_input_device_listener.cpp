0fccd08971282b229e468a1aa92f41dd2f4727ed
hellohyh001 <huiyuehong@huawei.com>
2024-08-28 11:10:45 +0000

    111
    Signed-off-by: hellohyh001 <huiyuehong@huawei.com>
    Change-Id: I4598d9eb151e163ee26c73981657fdcc079149c1

---
+++ frameworks/native/input/oh_input_device_listener.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "oh_input_device_listener.h"
+
+#include "define_multimodal.h"
+#include "error_multimodal.h"
+#include "input_manager.h"
+#include "input_manager_impl.h"
+#include "mmi_log.h"
+
+#undef MMI_LOG_TAG
+#define MMI_LOG_TAG "OHInputDeviceListener"
+
+namespace OHOS {
+namespace MMI {
+OHInputDeviceListener::OHInputDeviceListener() {}
+
+OHInputDeviceListener::~OHInputDeviceListener() {}
+
+void OHInputDeviceListener::OnDeviceAdded(int32_t deviceId, const std::string &type)
+{
+    if (addCallback_ != nullptr) {
+        addCallback_(deviceId, type);
+    }
+}
+
+void OHInputDeviceListener::OnDeviceRemoved(int32_t deviceId, const std::string &type)
+{
+    if (removeCallback_!= nullptr) {
+        removeCallback_(deviceId, type);
+    }
+}
+
+void OHInputDeviceListener::SetDeviceAddedCallback(const std::function<void(int32_t deviceId, const std::string &type)> &callback)
+{
+    addCallback_ = callback;
+}
+
+void OHInputDeviceListener::SetDeviceRemovedCallback(const std::function<void(int32_t deviceId, const std::string &type)> &callback)
+{
+    removeCallback_ = callback;
+}
+}
+}
