68e1a8261761aed5852f600d6957bd0ee9756d98
duyufan <duyufan1@huawei.com>
2024-09-18 08:49:43 +0000

    update service/window_manager/src/knuckle_dynamic_drawing_manager.cpp.
    Signed-off-by: duyufan <duyufan1@huawei.com>

--- service/window_manager/src/knuckle_dynamic_drawing_manager.cpp
+++ service/window_manager/src/knuckle_dynamic_drawing_manager.cpp
@@ -381,7 +381,7 @@ void KnuckleDynamicDrawingManager::CreateTouchWindow(const int32_t displayId)
     if (displayInfo_.displayMode == DisplayMode::MAIN) {
         screenId_ = FOLD_SCREEN_MAIN_ID;
     }
-    MMI_HILOGI("screenId_: %{public}"zu, screenId_);
+    MMI_HILOGI("screenId_: %{public}zu", screenId_);
     surfaceNode_->AttachToDisplay(screenId_);
     CHKPV(knuckleDrawMgr_);
     if (knuckleDrawMgr_->CheckRotatePolicy(displayInfo_)) {
