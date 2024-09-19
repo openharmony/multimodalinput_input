bb5ded213020f31a40f1c6e5496958530e3b8372
郭旭 <guoxu34@huawei.com>
2024-09-19 01:08:00 +0000

    update service/connect_manager/include/multimodal_input_connect_manager.h.
    Signed-off-by: 郭旭 <guoxu34@huawei.com>

--- service/connect_manager/include/multimodal_input_connect_manager.h
+++ service/connect_manager/include/multimodal_input_connect_manager.h
@@ -130,7 +130,7 @@ public:
     int32_t SetPixelMapData(int32_t infoId, void* pixelMap);
     int32_t SetCurrentUser(int32_t userId);
     int32_t AddVirtualInputDevice(std::shared_ptr<InputDevice> device, int32_t &deviceId);
-    int32_t RemoveVirtualInputDevice(int32_t deviceId);   
+    int32_t RemoveVirtualInputDevice(int32_t deviceId);
     int32_t EnableHardwareCursorStats(bool enable);
     int32_t GetHardwareCursorStats(uint32_t &frameCount, uint32_t &vsyncCount);
     int32_t GetPointerSnapshot(void *pixelMapPtr);
