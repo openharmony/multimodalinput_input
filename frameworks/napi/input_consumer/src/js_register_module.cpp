a60618359671212679791cf9c7cafac71150eb95
王豪 <wanghao505@huawei.com>
2024-09-23 13:54:29 +0800

    fix
    Signed-off-by: 王豪 <wanghao505@huawei.com>
    Change-Id: If7ad7f3eae8fe19052a7134ef4f10c7f4d6a125a

--- frameworks/napi/input_consumer/src/js_register_module.cpp
+++ frameworks/napi/input_consumer/src/js_register_module.cpp
@@ -497,7 +497,13 @@ static napi_value JsOn(napi_env env, napi_callback_info info)
     std::string keyType;
     size_t argc = 3;
     napi_value argv[3] = { 0 };
-    CHKRP(napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr), GET_CB_INFO);
+    if (napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr) != napi_ok) {
+        MMI_HILOGE("GET_CB_INFO failed");
+        auto infoTemp = std::string("JsOn GET_CB_INFO failed");
+        napi_throw_error(env, nullptr, infoTemp.c_str());
+        delete event;
+        return nullptr;
+    }
     if (argc < INPUT_PARAMETER_MAX) {
         MMI_HILOGE("Parameter number error argc:%{public}zu", argc);
         THROWERR_CUSTOM(env, COMMON_PARAMETER_ERROR, "Parameter number error");
