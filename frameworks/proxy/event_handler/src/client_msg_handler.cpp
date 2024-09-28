2a10a473bd4b1a8a06575ff927d53c0a74a2e8b0
郭旭 <guoxu34@huawei.com>
2024-09-28 02:14:36 +0000

    update frameworks/proxy/event_handler/src/client_msg_handler.cpp.
    Signed-off-by: 郭旭 <guoxu34@huawei.com>

--- frameworks/proxy/event_handler/src/client_msg_handler.cpp
+++ frameworks/proxy/event_handler/src/client_msg_handler.cpp
@@ -79,6 +79,8 @@ void ClientMsgHandler::Init()
 #endif // OHOS_BUILD_ENABLE_POINTER || OHOS_BUILD_ENABLE_TOUCH
         { MmiMessageId::NOTIFY_BUNDLE_NAME, [this] (const UDSClient& client, NetPacket& pkt) {
             return this->NotifyBundleName(client, pkt); }},
+        { MmiMessageId::WINDOW_STATE_ERROR_NOTIFY, [this] (const UDSClient& client, NetPacket& pkt) {
+            return this->NotifyWindowStateError(client, pkt); }},
     };
     for (auto &it : funs) {
         if (!RegistrationEvent(it)) {
@@ -188,7 +190,9 @@ int32_t ClientMsgHandler::OnPointerEvent(const UDSClient& client, NetPacket& pkt
         aggregator_.Record({MMI_LOG_DISPATCH, INPUT_KEY_FLOW, __FUNCTION__, __LINE__}, logInfo.c_str(),
             std::to_string(pointerEvent->GetId()));
     }
-    if (PointerEvent::POINTER_ACTION_CANCEL == pointerEvent->GetPointerAction()) {
+    if (PointerEvent::POINTER_ACTION_CANCEL == pointerEvent->GetPointerAction() ||
+        PointerEvent::POINTER_ACTION_HOVER_CANCEL == pointerEvent->GetPointerAction() ||
+        PointerEvent::POINTER_ACTION_FINGERPRINT_CANCEL == pointerEvent->GetPointerAction()) {
         MMI_HILOG_DISPATCHI("Operation canceled");
     }
     pointerEvent->SetProcessedCallback(dispatchCallback_);
@@ -232,7 +236,7 @@ int32_t ClientMsgHandler::OnSubscribeKeyEventCallback(const UDSClient &client, N
                 subscribeId, fd, keyEvent->GetId(), keyEvent->GetAction(), keyEvent->GetKeyAction(),
                 keyEvent->GetEventType(), keyEvent->GetFlag());
         } else {
-            MMI_HILOGI("Subscribe:%{public}d,Fd:%{public}d,KeyEvent:%{public}d,"
+            MMI_HILOGI("Subscribe:%{public}d,Fd:%{public}d,KeyEvent:%{public}d, "
                 "Action:%{public}d, KeyAction:%{public}d, EventType:%{public}d,Flag:%{public}u",
                 subscribeId, fd, keyEvent->GetId(), keyEvent->GetAction(), keyEvent->GetKeyAction(),
                 keyEvent->GetEventType(), keyEvent->GetFlag());
@@ -391,5 +395,22 @@ int32_t ClientMsgHandler::OnAnr(const UDSClient& client, NetPacket& pkt)
     InputMgrImpl.OnAnr(pid, eventId);
     return RET_OK;
 }
+
+int32_t ClientMsgHandler::NotifyWindowStateError(const UDSClient& client, NetPacket& pkt)
+{
+    CALL_DEBUG_ENTER;
+    int32_t pid = 0;
+    int32_t windowId = 0;
+    pkt >> pid;
+    pkt >> windowId;
+    if (pkt.ChkRWError()) {
+        MMI_HILOG_ANRDETECTE("Packet read data failed");
+        return RET_ERR;
+    }
+    MMI_HILOG_ANRDETECTI("Client pid:%{public}d windowId:%{public}d", pid, windowId);
+    InputMgrImpl.OnWindowStateError(pid, windowId);
+    return RET_OK;
+}
+
 } // namespace MMI
 } // namespace OHOS
