c2d6cdc3b0ac0d51dca0414dc7fa78c9778c4f9f
Dragon51 <longchao14@h-partners.com>
2024-09-30 14:13:38 +0800

    uinput黄蓝同步
    Signed-off-by: Dragon51 <longchao14@h-partners.com>

--- tools/inject_event/test/inject_event_test.cpp
+++ tools/inject_event/test/inject_event_test.cpp
@@ -248,168 +248,25 @@ HWTEST_F(InjectEventTest, InjectEvent_InjectKey_003, TestSize.Level1)
 }
 
 /**
- * @tc.name:InjectEvent_InjectKey_004
- * @tc.desc: test inject another press and hold the key interface, this is pressing '0' and hold 3 seconds
+ * @tc.name:InjectEvent_InjectTouch_001
+ * @tc.desc: test inject touch screen smooth movement interface
  * @tc.type: FUNC
- * @tc.require:
+ * @tc.require:SR000GGQBJ
  */
-HWTEST_F(InjectEventTest, InjectEvent_InjectKey_004, TestSize.Level1)
+HWTEST_F(InjectEventTest, InjectEvent_InjectTouch_001, TestSize.Level1)
 {
     std::unique_ptr<InputManagerCommand> inputManagerCommand = std::make_unique<InputManagerCommand>();
     char command1[] = {"uinput"};
-    char command2[] = {"-K"};
-    char command3[] = {"-r"};
-    char command4[] = {"2103"};
-    char *argv[] = {command1, command2, command3, command4};
-    int32_t result = inputManagerCommand->ParseCommand(sizeof(argv) / sizeof(argv[0]), argv);
+    char command2[] = {"-T"};
+    char command3[] = {"-m"};
+    char command4[] = {"100"};
+    char command5[] = {"200"};
+    char command6[] = {"100"};
+    char command7[] = {"600"};
+    char command8[] = {"1000"};
+    char *argv[] = {command1, command2, command3, command4, command5, command6, command7, command8};
+    int32_t result = inputManagerCommand->ParseCommand(8, argv);
     EXPECT_EQ(OHOS::ERR_OK, result);
-}
-
-/**
- * @tc.name:InjectEvent_InjectKey_005
- * @tc.desc: test inject another press and hold the key interface, this is pressing 'a' and hold 4 seconds
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(InjectEventTest, InjectEvent_InjectKey_005, TestSize.Level1)
-{
-    std::unique_ptr<InputManagerCommand> inputManagerCommand = std::make_unique<InputManagerCommand>();
-    char command1[] = {"uinput"};
-    char command2[] = {"-K"};
-    char command3[] = {"-r"};
-    char command4[] = {"2017"};
-    char command5[] = {"4000"};
-    char *argv[] = {command1, command2, command3, command4, command5};
-    int32_t result = inputManagerCommand->ParseCommand(sizeof(argv) / sizeof(argv[0]), argv);
-    EXPECT_EQ(OHOS::ERR_OK, result);
-}
-
-/**
- * @tc.name:InjectEvent_InjectKey_006
- * @tc.desc: test inject another press and hold the key interface, this is pressing 'Shift' and hold 3 seconds
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(InjectEventTest, InjectEvent_InjectKey_006, TestSize.Level1)
-{
-    std::unique_ptr<InputManagerCommand> inputManagerCommand = std::make_unique<InputManagerCommand>();
-    char command1[] = {"uinput"};
-    char command2[] = {"-K"};
-    char command3[] = {"-r"};
-    char command4[] = {"2047"};
-    char *argv[] = {command1, command2, command3, command4};
-    int32_t result = inputManagerCommand->ParseCommand(sizeof(argv) / sizeof(argv[0]), argv);
-    EXPECT_EQ(OHOS::ERR_OK, result);
-}
-
-/**
- * @tc.name:InjectEvent_InjectKey_007
- * @tc.desc: test inject keyevent with the keyintention whether has an expection, this is press 'Ctrl' + '-'
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(InjectEventTest, InjectEvent_InjectKey_007, TestSize.Level1)
-{
-    std::unique_ptr<InputManagerCommand> inputManagerCommand = std::make_unique<InputManagerCommand>();
-    char command1[] = {"uinput"};
-    char command2[] = {"-K"};
-    char command3[] = {"-d"};
-    char command4[] = {"2072"};
-    char command5[] = {"-d"};
-    char command6[] = {"2057"};
-    char *argv[] = {command1, command2, command3, command4, command5, command6};
-    int32_t result = inputManagerCommand->ParseCommand(sizeof(argv) / sizeof(argv[0]), argv);
-    EXPECT_EQ(OHOS::ERR_OK, result);
-}
-
-/**
- * @tc.name:InjectEvent_InjectKey_008
- * @tc.desc: test inject keyevent with the keyintention whether has an expection, this is press 'Home'
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(InjectEventTest, InjectEvent_InjectKey_008, TestSize.Level1)
-{
-    std::unique_ptr<InputManagerCommand> inputManagerCommand = std::make_unique<InputManagerCommand>();
-    char command1[] = {"uinput"};
-    char command2[] = {"-K"};
-    char command3[] = {"-d"};
-    char command4[] = {"2081"};
-    char *argv[] = {command1, command2, command3, command4};
-    int32_t result = inputManagerCommand->ParseCommand(sizeof(argv) / sizeof(argv[0]), argv);
-    EXPECT_EQ(OHOS::ERR_OK, result);
-}
-
-/**
- * @tc.name:InjectEvent_InjectKey_009
- * @tc.desc: test inject keyevent of text.
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(InjectEventTest, InjectEvent_InjectKey_009, TestSize.Level1)
-{
-    auto inputManagerCommand = std::make_unique<InputManagerCommand>();
-    char command1[] = {"uinput"};
-    char command2[] = {"-K"};
-    char command3[] = {"-t"};
-    char command4[] = {"abc ABC 123 ,-+*/.=~[{}]"};
-    char *argv[] = {command1, command2, command3, command4};
-    int32_t result = inputManagerCommand->ParseCommand(sizeof(argv) / sizeof(argv[0]), argv);
-    EXPECT_EQ(OHOS::ERR_OK, result);
-}
-
-/**
- * @tc.name:InjectEvent_InjectKey_010
- * @tc.desc: test inject keyevent of illegal character text.
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(InjectEventTest, InjectEvent_InjectKey_010, TestSize.Level1)
-{
-    auto inputManagerCommand = std::make_unique<InputManagerCommand>();
-    char command1[] = {"uinput"};
-    char command2[] = {"-K"};
-    char command3[] = {"-t"};
-    char command4[] = {"abc涓€浜屼笁"};
-    char *argv[] = {command1, command2, command3, command4};
-    int32_t result = inputManagerCommand->ParseCommand(sizeof(argv) / sizeof(argv[0]), argv);
-    EXPECT_NE(OHOS::ERR_OK, result);
-}
-
-/**
- * @tc.name:InjectEvent_InjectKey_011
- * @tc.desc: test inject keyevent of text.
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(InjectEventTest, InjectEvent_InjectKey_011, TestSize.Level1)
-{
-    auto inputManagerCommand = std::make_unique<InputManagerCommand>();
-    char command1[] = {"uinput"};
-    char command2[] = {"-K"};
-    char command3[] = {"--text"};
-    char command4[] = {"abc ABC 123 ,-+*/.=~[{}]"};
-    char *argv[] = {command1, command2, command3, command4};
-    int32_t result = inputManagerCommand->ParseCommand(sizeof(argv) / sizeof(argv[0]), argv);
-    EXPECT_EQ(OHOS::ERR_OK, result);
-}
-
-/**
- * @tc.name:InjectEvent_InjectKey_012
- * @tc.desc: test inject keyevent of illegal character text.
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(InjectEventTest, InjectEvent_InjectKey_012, TestSize.Level1)
-{
-    auto inputManagerCommand = std::make_unique<InputManagerCommand>();
-    char command1[] = {"uinput"};
-    char command2[] = {"-K"};
-    char command3[] = {"--text"};
-    char command4[] = {"abc涓€浜屼笁"};
-    char *argv[] = {command1, command2, command3, command4};
-    int32_t result = inputManagerCommand->ParseCommand(sizeof(argv) / sizeof(argv[0]), argv);
-    EXPECT_NE(OHOS::ERR_OK, result);
 }
 
 /**
@@ -555,43 +412,27 @@ HWTEST_F(InjectEventTest, InjectEvent_InjectTouchPad_001, TestSize.Level1)
     char command5[] = {"205"};
     char *argv[] = {command1, command2, command3, command4, command5};
     int32_t result = inputManagerCommand->ParseCommand(sizeof(argv) / sizeof(argv[0]), argv);
-    EXPECT_EQ(OHOS::ERR_OK, result);
+    EXPECT_NE(OHOS::ERR_OK, result);
 }
 
 /**
- * @tc.name:InjectEvent_InjectTouchPad_002
- * @tc.desc: test inject touchpad third finger pinch, scale is 3.05
+ * @tc.name:InjectEvent_InjectStylus_001
+ * @tc.desc: test inject stylus smooth movement interface
  * @tc.type: FUNC
  * @tc.require:
  */
-HWTEST_F(InjectEventTest, InjectEvent_InjectTouchPad_002, TestSize.Level1)
-{
-    auto inputManagerCommand = std::make_unique<InputManagerCommand>();
-    char command1[] = {"uinput"};
-    char command2[] = {"-P"};
-    char command3[] = {"-p"};
-    char command4[] = {"3"};
-    char command5[] = {"305"};
-    char *argv[] = {command1, command2, command3, command4, command5};
-    int32_t result = inputManagerCommand->ParseCommand(sizeof(argv) / sizeof(argv[0]), argv);
-    EXPECT_EQ(OHOS::ERR_OK, result);
-}
-
-/**
- * @tc.name:InjectEvent_InjectStylus_002
- * @tc.desc: test inject stylus click interface
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(InjectEventTest, InjectEvent_InjectStylus_002, TestSize.Level1)
+HWTEST_F(InjectEventTest, InjectEvent_InjectStylus_001, TestSize.Level1)
 {
     auto inputManagerCommand = std::make_unique<InputManagerCommand>();
     char command1[] = {"uinput"};
     char command2[] = {"-S"};
-    char command3[] = {"-c"};
-    char command4[] = {"200"};
-    char command5[] = {"300"};
-    char *argv[] = {command1, command2, command3, command4, command5};
+    char command3[] = {"-m"};
+    char command4[] = {"100"};
+    char command5[] = {"200"};
+    char command6[] = {"100"};
+    char command7[] = {"600"};
+    char command8[] = {"1000"};
+    char *argv[] = {command1, command2, command3, command4, command5, command6, command7, command8};
     int32_t result = inputManagerCommand->ParseCommand(sizeof(argv) / sizeof(argv[0]), argv);
     EXPECT_EQ(OHOS::ERR_OK, result);
 }
