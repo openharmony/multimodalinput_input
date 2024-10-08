0dc02a4f820614dbf619c6354ab0cf3f72d08566
张海峰 <zhanghaifeng45@huawei.com>
2024-10-08 17:20:27 +0800

    skip TRANSMIT_ALL
    Signed-off-by: 张海峰 <zhanghaifeng45@huawei.com>
    Change-Id: Id4f3dc136af3a99f7ecd8dbb8bc38a67f7e92dce

--- service/event_dispatch/src/event_dispatch_handler.cpp
+++ service/event_dispatch/src/event_dispatch_handler.cpp
@@ -173,7 +173,7 @@ void EventDispatchHandler::HandleMultiWindowPointerEvent(std::shared_ptr<Pointer
         if (pointerEvent->GetPointerAction() == PointerEvent::POINTER_ACTION_PULL_UP &&
             windowInfo->windowInputType == WindowInputType::TRANSMIT_ALL && windowIds.size() > 1) {
             MMI_HILOGD("When the drag is finished, the multi-window distribution is canceled. window:%{public}d,"
-                "windowInputType:%{public}d", windowId, static_cast<int32_t>(windowInfo->->windowInputType));
+                "windowInputType:%{public}d", windowId, static_cast<int32_t>(windowInfo->windowInputType));
             pointerEvent->SetPointerAction(PointerEvent::POINTER_ACTION_CANCEL);
         }
         auto fd = WIN_MGR->GetClientFd(pointerEvent, windowInfo->id);
