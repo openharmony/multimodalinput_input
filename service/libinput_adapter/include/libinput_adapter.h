8b9067f8086e6d22af91914fa560c49c18d2397b
a84329412 <a84329412@china.huawei.com>
2024-11-21 01:21:20 -0500

    Signed-off-by: a84329412 <arthur.song@h-partners.com>
    fix codecheck defects

--- service/libinput_adapter/include/libinput_adapter.h
+++ service/libinput_adapter/include/libinput_adapter.h
@@ -37,8 +37,8 @@ typedef std::function<void(double screenX, double screenY, int touchId, bool tip
 typedef std::function<void(double screenX, double screenY, int touchId, bool tipDown, std::string buttonName)> KeyDown;
 typedef std::function<void(double screenX, double screenY, int touchId, bool tipDown, std::string buttonName)> KeyUp;
 typedef std::function<int32_t(double screenX, double screenY, int touchId, bool tipDown)> HandleTouchPoint;
-typedef std::function<int32_t(std::string& buttonName, std::string& toggleButtonName, int& buttonMode, std::string& RestList)>
-                              GetMessage;
+typedef std::function<int32_t(std::string& buttonName, std::string& toggleButtonName, int& buttonMode,
+                      std::string& RestList)> GetMessage;
 typedef std::function<int32_t(std::string keyName)> GetKeyCodeByKeyName;
 
 enum VKeyboardMessageType {
