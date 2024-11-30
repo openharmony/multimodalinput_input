ff3d6fe88119c02e779c6c2bdb19faefaa18f4cb
baoshubei <baoshubei@huawei.com>
2024-11-30 17:27:38 +0800

    Fix uinput not supported unicode
    Signed-off-by: baoshubei <baoshubei@huawei.com>

--- tools/inject_event/src/input_manager_command.cpp
+++ tools/inject_event/src/input_manager_command.cpp
@@ -34,6 +34,7 @@
 
 #include "error_multimodal.h"
 #include "event_log_helper.h"
+#include "hos_key_event.h"
 #include "input_manager.h"
 #include "mmi_log.h"
 #include "multimodal_event_handler.h"
@@ -96,6 +97,78 @@ struct JoystickInfo {
     int32_t absValue { -1 };
     int32_t taktTime { 0 };
     PointerEvent::AxisType absType;
+};
+
+struct KeyUnicode {
+    uint32_t original { 0 };
+    uint32_t transitioned { 0 };
+};
+
+constexpr uint32_t DEFAULT_UNICODE = 0x0000;
+
+const std::map<int32_t, KeyUnicode> KEY_UNICODE_TRANSFORMATION = {
+    { HOS_KEY_A,                { 0x0061, 0x0041 } },
+    { HOS_KEY_B,                { 0x0062, 0x0042 } },
+    { HOS_KEY_C,                { 0x0063, 0x0043 } },
+    { HOS_KEY_D,                { 0x0064, 0x0044 } },
+    { HOS_KEY_E,                { 0x0065, 0x0045 } },
+    { HOS_KEY_F,                { 0x0066, 0x0046 } },
+    { HOS_KEY_G,                { 0x0067, 0x0047 } },
+    { HOS_KEY_H,                { 0x0068, 0x0048 } },
+    { HOS_KEY_I,                { 0x0069, 0x0049 } },
+    { HOS_KEY_J,                { 0x006A, 0x004A } },
+    { HOS_KEY_K,                { 0x006B, 0x004B } },
+    { HOS_KEY_L,                { 0x006C, 0x004C } },
+    { HOS_KEY_M,                { 0x006D, 0x004D } },
+    { HOS_KEY_N,                { 0x006E, 0x004E } },
+    { HOS_KEY_O,                { 0x006F, 0x004F } },
+    { HOS_KEY_P,                { 0x0070, 0x0050 } },
+    { HOS_KEY_Q,                { 0x0071, 0x0051 } },
+    { HOS_KEY_R,                { 0x0072, 0x0052 } },
+    { HOS_KEY_S,                { 0x0073, 0x0053 } },
+    { HOS_KEY_T,                { 0x0074, 0x0054 } },
+    { HOS_KEY_U,                { 0x0075, 0x0055 } },
+    { HOS_KEY_V,                { 0x0076, 0x0056 } },
+    { HOS_KEY_W,                { 0x0077, 0x0057 } },
+    { HOS_KEY_X,                { 0x0078, 0x0058 } },
+    { HOS_KEY_Y,                { 0x0079, 0x0059 } },
+    { HOS_KEY_Z,                { 0x007A, 0x005A } },
+    { HOS_KEY_0,                { 0x0030, 0x0029 } },
+    { HOS_KEY_1,                { 0x0031, 0x0021 } },
+    { HOS_KEY_2,                { 0x0032, 0x0040 } },
+    { HOS_KEY_3,                { 0x0033, 0x0023 } },
+    { HOS_KEY_4,                { 0x0034, 0x0024 } },
+    { HOS_KEY_5,                { 0x0035, 0x0025 } },
+    { HOS_KEY_6,                { 0x0036, 0x005E } },
+    { HOS_KEY_7,                { 0x0037, 0x0026 } },
+    { HOS_KEY_8,                { 0x0038, 0x002A } },
+    { HOS_KEY_9,                { 0x0039, 0x0028 } },
+    { HOS_KEY_GRAVE,            { 0x0060, 0x007E } },
+    { HOS_KEY_MINUS,            { 0x002D, 0x005F } },
+    { HOS_KEY_EQUALS,           { 0x002B, 0x003D } },
+    { HOS_KEY_LEFT_BRACKET,     { 0x005B, 0x007B } },
+    { HOS_KEY_RIGHT_BRACKET,    { 0x005D, 0x007D } },
+    { HOS_KEY_BACKSLASH,        { 0x005C, 0x007C } },
+    { HOS_KEY_SEMICOLON,        { 0x003B, 0x003A } },
+    { HOS_KEY_APOSTROPHE,       { 0x0027, 0x0022 } },
+    { HOS_KEY_SLASH,            { 0x002F, 0x003F } },
+    { HOS_KEY_COMMA,            { 0x002C, 0x003C } },
+    { HOS_KEY_PERIOD,           { 0x002E, 0x003E } },
+    { HOS_KEY_NUMPAD_0,         { 0x0030, 0x0000 } },
+    { HOS_KEY_NUMPAD_1,         { 0x0031, 0x0000 } },
+    { HOS_KEY_NUMPAD_2,         { 0x0032, 0x0000 } },
+    { HOS_KEY_NUMPAD_3,         { 0x0033, 0x0000 } },
+    { HOS_KEY_NUMPAD_4,         { 0x0034, 0x0000 } },
+    { HOS_KEY_NUMPAD_5,         { 0x0035, 0x0000 } },
+    { HOS_KEY_NUMPAD_6,         { 0x0036, 0x0000 } },
+    { HOS_KEY_NUMPAD_7,         { 0x0037, 0x0000 } },
+    { HOS_KEY_NUMPAD_8,         { 0x0038, 0x0000 } },
+    { HOS_KEY_NUMPAD_9,         { 0x0039, 0x0000 } },
+    { HOS_KEY_NUMPAD_DIVIDE,    { 0x002F, 0x0000 } },
+    { HOS_KEY_NUMPAD_MULTIPLY,  { 0x0038, 0x0000 } },
+    { HOS_KEY_NUMPAD_SUBTRACT,  { 0x002D, 0x0000 } },
+    { HOS_KEY_NUMPAD_ADD,       { 0x002B, 0x0000 } },
+    { HOS_KEY_NUMPAD_DOT,       { 0x002E, 0x0000 } }
 };
 } // namespace
 
@@ -731,6 +804,7 @@ int32_t InputManagerCommand::ParseCommand(int32_t argc, char *argv[])
                                     KeyEvent->SetActionTime(time);
                                     KeyEvent->SetKeyAction(KeyEvent::KEY_ACTION_DOWN);
                                     item[i].SetKeyCode(downKey[i]);
+                                    item[i].SetUnicode(KeyCodeToUnicode(downKey[i]));
                                     item[i].SetDownTime(time);
                                     item[i].SetPressed(true);
                                     KeyEvent->AddKeyItem(item[i]);
@@ -747,6 +821,7 @@ int32_t InputManagerCommand::ParseCommand(int32_t argc, char *argv[])
                             KeyEvent::KeyItem item1;
                             item1.SetPressed(true);
                             item1.SetKeyCode(keyCode);
+                            item1.SetUnicode(KeyCodeToUnicode(keyCode));
                             item1.SetDownTime(time);
                             KeyEvent->AddKeyItem(item1);
                             InputManager::GetInstance()->SimulateInputEvent(KeyEvent);
@@ -769,6 +844,7 @@ int32_t InputManagerCommand::ParseCommand(int32_t argc, char *argv[])
                                 KeyEvent::KeyItem item1;
                                 item1.SetPressed(false);
                                 item1.SetKeyCode(keyCode);
+                                item1.SetUnicode(KeyCodeToUnicode(keyCode));
                                 item1.SetDownTime(time);
                                 KeyEvent->AddKeyItem(item1);
                                 InputManager::GetInstance()->SimulateInputEvent(KeyEvent);
@@ -823,6 +899,7 @@ int32_t InputManagerCommand::ParseCommand(int32_t argc, char *argv[])
                             keyEvent->SetKeyAction(KeyEvent::KEY_ACTION_DOWN);
                             KeyEvent::KeyItem item;
                             item.SetKeyCode(keyCode);
+                            item.SetUnicode(KeyCodeToUnicode(keyCode));
                             item.SetPressed(true);
                             auto keyEventTemp = KeyEvent::Clone(keyEvent);
                             if (keyEventTemp == nullptr) {
@@ -885,6 +962,7 @@ int32_t InputManagerCommand::ParseCommand(int32_t argc, char *argv[])
                             keyEvent->SetKeyAction(KeyEvent::KEY_ACTION_DOWN);
                             KeyEvent::KeyItem item;
                             item.SetKeyCode(keyCode);
+                            item.SetUnicode(KeyCodeToUnicode(keyCode));
                             int64_t time = GetSysClockTime();
                             item.SetPressed(true);
                             auto keyEventTemp = KeyEvent::Clone(keyEvent);
@@ -2312,6 +2390,15 @@ void InputManagerCommand::SendTouchDownForPinch(int32_t topX, int32_t topY, int3
     pointerEvent->AddPointerItem(itemFirst);
     pointerEvent->AddPointerItem(itemSecond);
     InputManager::GetInstance()->SimulateInputEvent(pointerEvent);
+}
+
+uint32_t InputManagerCommand::KeyCodeToUnicode(int32_t keyCode)
+{
+    auto iter = KEY_UNICODE_TRANSFORMATION.find(keyCode);
+    if (iter == KEY_UNICODE_TRANSFORMATION.end()) {
+        return DEFAULT_UNICODE;
+    }
+    return iter->second.transitioned;
 }
 
 void InputManagerCommand::PrintMouseUsage()
