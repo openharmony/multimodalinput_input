55ebfba3b25a3665ca12f5d94874965e935c824e
Rosita <wangjing596@huawei.com>
2024-12-16 03:46:36 +0000

    Implicit release of PixelMap
    Signed-off-by: Rosita <wangjing596@huawei.com>
    Change-Id: I59cd1e23b5b5264edd36e754fd6f6372f366ade9

--- service/connect_manager/src/multimodal_input_connect_stub.cpp
+++ service/connect_manager/src/multimodal_input_connect_stub.cpp
@@ -2592,9 +2592,9 @@ int32_t MultimodalInputConnectStub::StubSetPixelMapData(MessageParcel& data, Mes
         MMI_HILOGE("Invalid infoId:%{public}d", infoId);
         return RET_ERR;
     }
-    std::unique_ptr<OHOS::Media::PixelMap> pixelMap(Media::PixelMap::Unmarshalling(data));
+    OHOS::Media::PixelMap* pixelMap = Media::PixelMap::Unmarshalling(data);
     CHKPR(pixelMap, RET_ERR);
-    int32_t ret = SetPixelMapData(infoId, static_cast<void*>(pixelMap.get()));
+    int32_t ret = SetPixelMapData(infoId, static_cast<void*>(pixelMap));
     if (ret != RET_OK) {
         MMI_HILOGE("Failed to call SetPixelMapData, ret:%{public}d", ret);
     }
