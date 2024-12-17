1dde1245e5b5481ed4d920e8927af206222758b0
fcQing <fengchangqing2@huawei.com>
2024-12-17 15:47:58 +0800

    foldPc适配横屏
    Signed-off-by: fcQing <fengchangqing2@huawei.com>

--- service/mouse_event_normalize/include/mouse_transform_processor.h
+++ service/mouse_event_normalize/include/mouse_transform_processor.h
@@ -42,6 +42,7 @@ extern "C" {
         DEVICE_HARD_PC_PRO = 3,
         DEVICE_TABLET = 4,
         DEVICE_FOLD_PC = 5,
+        DEVICE_FOLD_PC_HORIZONTAL = 6,
     };
     int32_t HandleMotionAccelerateMouse(const Offset* offset, bool mode, double* abs_x, double* abs_y,
         int32_t speed, int32_t deviceType);
