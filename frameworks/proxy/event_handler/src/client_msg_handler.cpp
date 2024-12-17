ac7c363be28b5af16a8cebfa75bf81692793f236
mayunteng_1 <mayunteng@huawei.com>
2024-12-17 15:42:31 +0800

    Signed-off-by: mayunteng_1 <mayunteng@huawei.com> Change-Id: Iee969f774ca353c8813d854cf16473c4ac4516b0

--- frameworks/proxy/event_handler/src/client_msg_handler.cpp
+++ frameworks/proxy/event_handler/src/client_msg_handler.cpp
@@ -84,7 +84,7 @@ void ClientMsgHandler::InitProcessedCallback()
 {
     CALL_DEBUG_ENTER;
     int32_t tokenType = MultimodalInputConnMgr->GetTokenType();
-    if (tokenType == TokenType::TOKEN_HAP) {
+    if (tokenType == TokenType::TOKEN_HAP || tokenType == TokenType::TOKEN_SYSTEM_HAP) {
         MMI_HILOGD("Current session is hap");
         dispatchCallback_ = std::bind(&ClientMsgHandler::OnDispatchEventProcessed, std::placeholders::_1,
             std::placeholders::_2);
