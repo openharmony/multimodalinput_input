8aab3b1b3ec8da694b7d85d719c674807c47fd88
athends <huwanyong1@huawei.com>
2024-12-30 20:07:32 +0800

    多模适配触控偏移
    Signed-off-by: athends <huwanyong1@huawei.com>

--- service/message_handle/src/server_msg_handler.cpp
+++ service/message_handle/src/server_msg_handler.cpp
@@ -539,7 +539,8 @@ int32_t ServerMsgHandler::OnDisplayInfo(SessionPtr sess, NetPacket &pkt)
     for (uint32_t i = 0; i < num; i++) {
         DisplayInfo info;
         pkt >> info.id >> info.x >> info.y >> info.width >> info.height >> info.dpi >> info.name
-            >> info.uniq >> info.direction >> info.displayDirection >> info.displayMode >> info.transform >> info.ppi;
+            >> info.uniq >> info.direction >> info.displayDirection >> info.displayMode >> info.transform >> info.ppi
+            >> info.offsetX >> info.offsetY;
         displayGroupInfo.displaysInfo.push_back(info);
         if (pkt.ChkRWError()) {
             MMI_HILOGE("Packet read display info failed");
