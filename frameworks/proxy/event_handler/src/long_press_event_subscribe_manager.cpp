dabb06db1f130e3808d74895a768ab6093c426d5
baoshubei <baoshubei@huawei.com>
2024-12-31 11:31:05 +0800

    Add pointer id for long-press event
    Signed-off-by: baoshubei <baoshubei@huawei.com>

--- frameworks/proxy/event_handler/src/long_press_event_subscribe_manager.cpp
+++ frameworks/proxy/event_handler/src/long_press_event_subscribe_manager.cpp
@@ -122,11 +122,12 @@ int32_t LongPressEventSubscribeManager::OnSubscribeLongPressEventCallback(const 
     CHKPR(callback, ERROR_NULL_POINTER);
     callback(longPressEvent);
     MMI_HILOGD("LongPressEvent fingerCount:%{public}d, duration:%{public}d, pid:%{public}d, displayId:%{public}d, "
-        "displayX:%{public}d, displayY:%{public}d, result:%{public}d, windowId:%{public}d, bundleName:%{public}s, "
-        "subscribeId:%{public}d",
+        "displayX:%{public}d, displayY:%{public}d, result:%{public}d, windowId:%{public}d, pointerId:%{public}d, "
+        "bundleName:%{public}s, subscribeId:%{public}d",
         longPressEvent.fingerCount, longPressEvent.duration, longPressEvent.pid,
         longPressEvent.displayId, longPressEvent.displayX, longPressEvent.displayY,
-        longPressEvent.result, longPressEvent.windowId, longPressEvent.bundleName.c_str(), subscribeId);
+        longPressEvent.result, longPressEvent.windowId, longPressEvent.pointerId, longPressEvent.bundleName.c_str(),
+        subscribeId);
     return RET_OK;
 }
 
