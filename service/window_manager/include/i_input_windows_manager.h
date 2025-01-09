14b15731518f0cda72c7fdefda8d34a40750d6e1
baoshubei <baoshubei@huawei.com>
2025-01-09 11:02:37 +0800

    Eliminate the difference
    Signed-off-by: baoshubei <baoshubei@huawei.com>

--- service/window_manager/include/i_input_windows_manager.h
+++ service/window_manager/include/i_input_windows_manager.h
@@ -118,6 +118,7 @@ public:
     virtual void DispatchPointer(int32_t pointerAction, int32_t windowId = -1) = 0;
     virtual void SendPointerEvent(int32_t pointerAction) = 0;
     virtual bool IsMouseSimulate() const = 0;
+    virtual bool HasMouseHideFlag() const = 0;
 #endif // OHOS_BUILD_ENABLE_POINTER || OHOS_BUILD_ENABLE_TOUCH
 #ifdef OHOS_BUILD_ENABLE_POINTER
 #ifdef OHOS_BUILD_ENABLE_POINTER_DRAWING
