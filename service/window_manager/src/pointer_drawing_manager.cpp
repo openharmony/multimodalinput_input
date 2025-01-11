a5c99ab6e96fe76bf3279bba3e65ff80a9e6b22e
zhanghaifeng <zhanghaifeng45@huawei.com>
2025-01-11 09:29:30 +0800

    fix screenid
    Signed-off-by: zhanghaifeng <zhanghaifeng45@huawei.com>
    Change-Id: Iac2c9d83663afda6905d98dc0f7d00351fe9ec4a

--- service/window_manager/src/pointer_drawing_manager.cpp
+++ service/window_manager/src/pointer_drawing_manager.cpp
@@ -131,7 +131,7 @@ namespace OHOS {
 namespace MMI {
 static bool IsSingleDisplayFoldDevice()
 {
-    return (!FOLD_SCREEN_FLAG.empty() && FOLD_SCREEN_FLAG[0] == '1');
+    return (!FOLD_SCREEN_FLAG.empty() && (FOLD_SCREEN_FLAG[0] == '1' || FOLD_SCREEN_FLAG[0] == '4'));
 }
 
 void RsRemoteDiedCallback()
