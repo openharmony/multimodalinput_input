8a9dcf87a82e1fdcafd263c841e5ec5773a26e95
dock_yu <yuzhongxian@huawei.com>
2025-02-08 06:22:32 +0000

    update service/mouse_event_normalize/src/mouse_transform_processor.cpp.
    Signed-off-by: dock_yu <yuzhongxian@huawei.com>

--- service/mouse_event_normalize/src/mouse_transform_processor.cpp
+++ service/mouse_event_normalize/src/mouse_transform_processor.cpp
@@ -152,8 +152,8 @@ int32_t MouseTransformProcessor::HandleMotionInner(struct libinput_event_pointer
     int32_t ret = RET_ERR;
     DeviceType deviceType = CheckDeviceType(displayInfo->width, displayInfo->height);
     if (type == LIBINPUT_EVENT_POINTER_MOTION_TOUCHPAD) {
-        struct libinput_device *dev = libinput_event_get_device(event);
-        const std::string devName = libinput_device_get_name(dev);
+        struct libinput_device *device = libinput_event_get_device(event);
+        const std::string devName = libinput_device_get_name(device);
         if (PRODUCT_TYPE == DEVICE_TYPE_FOLD_PC && devName == "input_mt_wrapper") {
             deviceType = DeviceType::DEVICE_FOLD_PC_VIRT;
             pointerEvent_->AddFlag(InputEvent::EVENT_FLAG_TOUCHPAD_POINTER);
