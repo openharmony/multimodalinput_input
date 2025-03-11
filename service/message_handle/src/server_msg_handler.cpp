a16269df4e978abd4a5d1bf8c92c293e6280d6b2
Baijian Chen <b84299469@china.huawei.com>
2025-03-10 21:24:50 -0400

    Update base on comments
    Signed-off-by: Baijian Chen <baijian.chen@h-partners.com>

--- service/message_handle/src/server_msg_handler.cpp
+++ service/message_handle/src/server_msg_handler.cpp
@@ -723,8 +723,10 @@ int32_t ServerMsgHandler::ReadDisplayInfo(NetPacket &pkt, DisplayGroupInfo &disp
             >> info.screenRealHeight >> info.screenRealPPI >> info.screenRealDPI >> info.screenCombination
             >> info.validWidth >> info.validHeight >> info.fixedDirection
             >> info.physicalWidth >> info.physicalHeight
-            >> info.oneHandX >> info.oneHandY
-            >> info.pointerActiveWidth >> info.pointerActiveHeight;
+            >> info.oneHandX >> info.oneHandY;
+#ifdef OHOS_BUILD_ENABLE_VKEYBOARD
+        pkt >> info.pointerActiveWidth >> info.pointerActiveHeight;
+#endif // OHOS_BUILD_ENABLE_VKEYBOARD
         displayGroupInfo.displaysInfo.push_back(info);
         if (pkt.ChkRWError()) {
             MMI_HILOGE("Packet read display info failed");
