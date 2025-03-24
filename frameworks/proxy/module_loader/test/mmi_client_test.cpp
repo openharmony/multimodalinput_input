0a6b380a0749e6923c99e04d7c4e40d824457287
Dragon51 <longchao17@h-partners.com>
2025-03-24 09:22:01 +0000

    UT
    Signed-off-by: Dragon51 <longchao17@h-partners.com>

--- frameworks/proxy/module_loader/test/mmi_client_test.cpp
+++ frameworks/proxy/module_loader/test/mmi_client_test.cpp
@@ -12,6 +12,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#define private public
+#define protected public
 
 #include <gtest/gtest.h>
 
