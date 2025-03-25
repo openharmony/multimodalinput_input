146789011310b9f48dbdbdd44deba1dbe987291f
Dragon51 <longchao17@h-partners.com>
2025-03-25 01:28:53 +0000

    更改    "private=public",     "protected=public"
    Signed-off-by: Dragon51 <longchao17@h-partners.com>

--- frameworks/proxy/module_loader/test/mmi_client_test.cpp
+++ frameworks/proxy/module_loader/test/mmi_client_test.cpp
@@ -12,8 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#define private public
-#define protected public
 
 #include <gtest/gtest.h>
 
