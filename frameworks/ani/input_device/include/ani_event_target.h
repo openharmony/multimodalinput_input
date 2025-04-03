20381f35402df248bf62d05060a2df0c60cd36f6
fanzhe <fanzhe001@chinasoftinc.com>
2025-04-03 15:04:46 +0800

    多模输入ANI化
    Signed-off-by: fanzhe <fanzhe001@chinasoftinc.com>
    Change-Id: I73a869ed3c10222cf4069874e2d1dcec5c98a99e

---
+++ frameworks/ani/input_device/include/ani_event_target.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ANI_EVENT_TARGET_H
+#define ANI_EVENT_TARGET_H
+
+#include <ani.h>
+
+#include "define_multimodal.h"
+#include "error_multimodal.h"
+#include "input_manager.h"
+#include "ani_util.h"
+
+namespace OHOS {
+namespace MMI {
+class AniEventTarget : public IInputDeviceListener, public std::enable_shared_from_this<AniEventTarget> {
+public:
+    AniEventTarget();
+    virtual ~AniEventTarget() = default;
+    DISALLOW_COPY_AND_MOVE(AniEventTarget);
+    void AddListener(ani_env *env, const std::string &type, ani_object handle);
+    void ResetEnv();
+    void OnDeviceAdded(int32_t deviceId, const std::string &type) override;
+    void OnDeviceRemoved(int32_t deviceId, const std::string &type) override;
+
+private:
+    static void EmitAddedDeviceEvent(std::shared_ptr<AniUtil::ReportData> reportData);
+    static void EmitRemoveDeviceEvent(std::shared_ptr<AniUtil::ReportData> reportData);
+    static bool EmitCallbackWork(ani_env *env, std::shared_ptr<AniUtil::ReportData> &reportData,
+        const std::string &type);
+
+private:
+    inline static std::map<std::string, std::vector<std::unique_ptr<AniUtil::CallbackInfo>>> devListener_ {};
+    bool isListeningProcess_ { false };
+};
+} // namespace MMI
+} // namespace OHOS
+#endif // ANI_EVENT_TARGET_H
