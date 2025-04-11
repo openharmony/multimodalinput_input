21189ea79c0fc4ffc99cf27aaab075e32e8019a3
pwx1285814 <panzhi9@huawei.com>
2025-04-11 10:46:57 +0800

    补充UT用例
    Signed-off-by: pwx1285814 <panzhi9@huawei.com>
    Change-Id: I0c35477d9c50df179463d9e93cee114fc82a5f62

--- service/window_manager/test/mock_input_windows_manager.h
+++ service/window_manager/test/mock_input_windows_manager.h
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -34,13 +34,13 @@ public:
     virtual ~InputWindowsManagerInterface(){};
     virtual std::shared_ptr<EventNormalizeHandler> GetEventNormalizeHandler() = 0;
     virtual std::shared_ptr<EventDispatchHandler> GetEventDispatchHandler() = 0;
-    virtual UDSServer* GetUDSServer() = 0;
+    virtual UDSServer *GetUDSServer() = 0;
     virtual bool IsAncoWindow(const WindowInfo &window) = 0;
     virtual std::optional<WindowInfo> GetWindowInfoById(int32_t windowId) = 0;
-    virtual bool IsInHotArea(
-        int32_t x, int32_t y, const std::vector<Rect> &rects, const WindowInfo &window) = 0;
+    virtual bool IsInHotArea(int32_t x, int32_t y, const std::vector<Rect> &rects, const WindowInfo &window) = 0;
     virtual bool IsSceneBoardEnabled() = 0;
     virtual std::optional<WindowInfo> GetWindowAndDisplayInfo(int32_t windowId, int32_t displayId) = 0;
+    virtual std::string ReadJsonFile(const std::string &filePath) = 0;
 };
 
 class MockInputWindowsManager : public InputWindowsManagerInterface {
@@ -50,13 +50,14 @@ public:
 
     MOCK_METHOD(std::shared_ptr<EventNormalizeHandler>, GetEventNormalizeHandler, ());
     MOCK_METHOD(std::shared_ptr<EventDispatchHandler>, GetEventDispatchHandler, ());
-    MOCK_METHOD(UDSServer*, GetUDSServer, ());
+    MOCK_METHOD(UDSServer *, GetUDSServer, ());
     MOCK_METHOD(bool, IsAncoWindow, (const WindowInfo &));
     MOCK_METHOD(std::optional<WindowInfo>, GetWindowInfoById, (int32_t));
     MOCK_METHOD(bool, IsInHotArea, (int32_t, int32_t, const std::vector<Rect> &, const WindowInfo &));
     MOCK_METHOD(bool, IsSceneBoardEnabled, ());
     MOCK_METHOD(std::optional<WindowInfo>, GetWindowAndDisplayInfo, (int32_t, int32_t));
+    MOCK_METHOD(std::string, ReadJsonFile, (const std::string &));
 };
-}  // namespace MMI
-}  // namespace OHOS
+} // namespace MMI
+} // namespace OHOS
 #endif
