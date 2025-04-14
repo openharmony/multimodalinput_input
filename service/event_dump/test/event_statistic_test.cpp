0887e488187d0273817710960c7e6022b5b2f793
郭旭 <guoxu34@huawei.com>
2025-04-14 12:47:37 +0000

    update service/event_dump/test/event_statistic_test.cpp.
    Signed-off-by: 郭旭 <guoxu34@huawei.com>

--- service/event_dump/test/event_statistic_test.cpp
+++ service/event_dump/test/event_statistic_test.cpp
@@ -91,8 +91,53 @@ HWTEST_F(EventStatisticTest, EventStatisticTest_PushPointerEvent, TestSize.Level
     pointerEvent->bitwise_ = 0x000040;
     ASSERT_NO_FATAL_FAILURE(eventStatistic.PushPointerEvent(pointerEvent));
 
+    pointerEvent->SetAction(PointerEvent::POINTER_ACTION_UP);
+    pointerEvent->AddFlag(InputEvent::EVENT_FLAG_PRIVACY_MODE);
+    ASSERT_NO_FATAL_FAILURE(eventStatistic.PushPointerEvent(pointerEvent));
+
     pointerEvent->SetAction(PointerEvent::POINTER_ACTION_DOWN);
     ASSERT_NO_FATAL_FAILURE(eventStatistic.PushPointerEvent(pointerEvent));
+}
+
+/**
+ * @tc.name: EventDumpTest_PushKeyEvent
+ * @tc.desc: Event dump PushKeyEvent
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(EventStatisticTest, EventStatisticTest_PushKeyEvent, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    EventStatistic eventStatistic;
+    std::shared_ptr<KeyEvent> keyEvent = KeyEvent::Create();
+    keyEvent->SetAction(KeyEvent::KEY_ACTION_DOWN);
+    keyEvent->bitwise_ = 0x000040;
+    ASSERT_NO_FATAL_FAILURE(eventStatistic.PushKeyEvent(keyEvent));
+
+    keyEvent->AddFlag(InputEvent::EVENT_FLAG_PRIVACY_MODE);
+    ASSERT_NO_FATAL_FAILURE(eventStatistic.PushKeyEvent(keyEvent));
+
+    keyEvent->SetAction(KeyEvent::KEY_ACTION_UP);
+    ASSERT_NO_FATAL_FAILURE(eventStatistic.PushKeyEvent(keyEvent));
+}
+
+/**
+ * @tc.name: EventDumpTest_PushSwitchEvent
+ * @tc.desc: Event dump PushSwitchEvent
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(EventStatisticTest, EventStatisticTest_PushSwitchEvent, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    EventStatistic eventStatistic;
+    std::shared_ptr<SwitchEvent> switchEvent = std::make_shared<SwitchEvent>(0);
+    switchEvent->SetSwitchType(SwitchEvent::SWITCH_DEFAULT);
+    switchEvent->bitwise_ = 0x000040;
+    ASSERT_NO_FATAL_FAILURE(eventStatistic.PushSwitchEvent(switchEvent));
+
+    switchEvent->SetSwitchType(SwitchEvent::SWITCH_TABLET);
+    ASSERT_NO_FATAL_FAILURE(eventStatistic.PushSwitchEvent(switchEvent));
 }
 
 /**
@@ -138,6 +183,202 @@ HWTEST_F(EventStatisticTest, EventStatisticTest_Dump, TestSize.Level1)
 }
 
 /**
+ * @tc.name: EventDumpTest_ConvertEventTypeToString
+ * @tc.desc: Event dump ConvertEventTypeToString
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(EventStatisticTest, EventStatisticTest_ConvertEventTypeToString, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    EventStatistic eventStatistic;
+    int32_t eventType = InputEvent::EVENT_TYPE_BASE;
+    ASSERT_STREQ(eventStatistic.ConvertEventTypeToString(eventType), "base");
+
+    eventType = InputEvent::EVENT_TYPE_KEY;
+    ASSERT_STREQ(eventStatistic.ConvertEventTypeToString(eventType), "key");
+
+    eventType = InputEvent::EVENT_TYPE_POINTER;
+    ASSERT_STREQ(eventStatistic.ConvertEventTypeToString(eventType), "pointer");
+
+    eventType = InputEvent::EVENT_TYPE_AXIS;
+    ASSERT_STREQ(eventStatistic.ConvertEventTypeToString(eventType), "axis");
+
+    eventType = InputEvent::EVENT_TYPE_FINGERPRINT;
+    ASSERT_STREQ(eventStatistic.ConvertEventTypeToString(eventType), "fingerprint");
+
+    eventType = -1;
+    ASSERT_STREQ(eventStatistic.ConvertEventTypeToString(eventType), "unknown");
+}
+
+/**
+ * @tc.name: EventDumpTest_ConvertSourceTypeToString
+ * @tc.desc: Event dump ConvertSourceTypeToString
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(EventStatisticTest, EventStatisticTest_ConvertSourceTypeToString, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    EventStatistic eventStatistic;
+    int32_t sourceType = InputEvent::SOURCE_TYPE_MOUSE;
+    ASSERT_STREQ(eventStatistic.ConvertSourceTypeToString(sourceType), "mouse");
+
+    sourceType = InputEvent::SOURCE_TYPE_TOUCHSCREEN;
+    ASSERT_STREQ(eventStatistic.ConvertSourceTypeToString(sourceType), "touch-screen");
+
+    sourceType = InputEvent::SOURCE_TYPE_TOUCHPAD;
+    ASSERT_STREQ(eventStatistic.ConvertSourceTypeToString(sourceType), "touch-pad");
+
+    sourceType = InputEvent::SOURCE_TYPE_JOYSTICK;
+    ASSERT_STREQ(eventStatistic.ConvertSourceTypeToString(sourceType), "joystick");
+
+    sourceType = InputEvent::SOURCE_TYPE_FINGERPRINT;
+    ASSERT_STREQ(eventStatistic.ConvertSourceTypeToString(sourceType), "fingerprint");
+
+    sourceType = InputEvent::SOURCE_TYPE_CROWN;
+    ASSERT_STREQ(eventStatistic.ConvertSourceTypeToString(sourceType), "crown");
+
+    sourceType = InputEvent::EVENT_FLAG_NONE;
+    ASSERT_STREQ(eventStatistic.ConvertSourceTypeToString(sourceType), "unknown");
+}
+
+/**
+ * @tc.name: EventDumpTest_ConvertPointerActionToString_001
+ * @tc.desc: Event dump ConvertPointerActionToString_001
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(EventStatisticTest, EventStatisticTest_ConvertPointerActionToString_001, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    EventStatistic eventStatistic;
+    std::shared_ptr<PointerEvent> pointerEvent = PointerEvent::Create();
+    pointerEvent->bitwise_ = 0x000040;
+    pointerEvent->SetPointerAction(PointerEvent::POINTER_ACTION_AXIS_BEGIN);
+    pointerEvent->SetAxisValue(PointerEvent::AXIS_TYPE_SCROLL_VERTICAL, 0);
+    ASSERT_STREQ(eventStatistic.ConvertPointerActionToString(pointerEvent), "axis-begin");
+    pointerEvent->ClearAxisValue();
+
+    pointerEvent->SetAxisValue(PointerEvent::AXIS_TYPE_SCROLL_HORIZONTAL, 0);
+    ASSERT_STREQ(eventStatistic.ConvertPointerActionToString(pointerEvent), "axis-begin");
+    pointerEvent->ClearAxisValue();
+
+    pointerEvent->SetAxisValue(PointerEvent::AXIS_TYPE_PINCH, 0);
+    ASSERT_STREQ(eventStatistic.ConvertPointerActionToString(pointerEvent), "pinch-begin");
+    pointerEvent->ClearAxisValue();
+}
+
+/**
+ * @tc.name: EventDumpTest_ConvertPointerActionToString_002
+ * @tc.desc: Event dump ConvertPointerActionToString_002
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(EventStatisticTest, EventStatisticTest_ConvertPointerActionToString_002, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    EventStatistic eventStatistic;
+    std::shared_ptr<PointerEvent> pointerEvent = PointerEvent::Create();
+    pointerEvent->bitwise_ = 0x000040;
+    pointerEvent->SetPointerAction(PointerEvent::POINTER_ACTION_AXIS_UPDATE);
+    pointerEvent->SetAxisValue(PointerEvent::AXIS_TYPE_SCROLL_VERTICAL, 0);
+    ASSERT_STREQ(eventStatistic.ConvertPointerActionToString(pointerEvent), "axis-update");
+    pointerEvent->ClearAxisValue();
+
+    pointerEvent->SetAxisValue(PointerEvent::AXIS_TYPE_SCROLL_HORIZONTAL, 0);
+    ASSERT_STREQ(eventStatistic.ConvertPointerActionToString(pointerEvent), "axis-update");
+    pointerEvent->ClearAxisValue();
+
+    pointerEvent->SetAxisValue(PointerEvent::AXIS_TYPE_PINCH, 0);
+    ASSERT_STREQ(eventStatistic.ConvertPointerActionToString(pointerEvent), "pinch-update");
+    pointerEvent->ClearAxisValue();
+}
+
+/**
+ * @tc.name: EventDumpTest_ConvertPointerActionToString_003
+ * @tc.desc: Event dump ConvertPointerActionToString_003
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(EventStatisticTest, EventStatisticTest_ConvertPointerActionToString_003, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    EventStatistic eventStatistic;
+    std::shared_ptr<PointerEvent> pointerEvent = PointerEvent::Create();
+    pointerEvent->bitwise_ = 0x000040;
+    pointerEvent->SetPointerAction(PointerEvent::POINTER_ACTION_AXIS_END);
+    pointerEvent->SetAxisValue(PointerEvent::AXIS_TYPE_SCROLL_VERTICAL, 0);
+    ASSERT_STREQ(eventStatistic.ConvertPointerActionToString(pointerEvent), "axis-end");
+    pointerEvent->ClearAxisValue();
+
+    pointerEvent->SetAxisValue(PointerEvent::AXIS_TYPE_SCROLL_HORIZONTAL, 0);
+    ASSERT_STREQ(eventStatistic.ConvertPointerActionToString(pointerEvent), "axis-end");
+    pointerEvent->ClearAxisValue();
+
+    pointerEvent->SetAxisValue(PointerEvent::AXIS_TYPE_PINCH, 0);
+    ASSERT_STREQ(eventStatistic.ConvertPointerActionToString(pointerEvent), "pinch-end");
+    pointerEvent->ClearAxisValue();
+}
+
+/**
+ * @tc.name: EventDumpTest_ConvertPointerActionToString_004
+ * @tc.desc: Event dump ConvertPointerActionToString_004
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(EventStatisticTest, EventStatisticTest_ConvertPointerActionToString_004, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    EventStatistic eventStatistic;
+    std::shared_ptr<PointerEvent> pointerEvent = PointerEvent::Create();
+    pointerEvent->bitwise_ = 0x000040;
+    pointerEvent->SetPointerAction(PointerEvent::TOUCH_ACTION_SWIPE_UP);
+    ASSERT_STREQ(eventStatistic.ConvertPointerActionToString(pointerEvent), "touch-swipe-up");
+}
+
+/**
+ * @tc.name: EventDumpTest_ConvertKeyActionToString
+ * @tc.desc: Event dump ConvertKeyActionToString
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(EventStatisticTest, EventStatisticTest_ConvertKeyActionToString, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    EventStatistic eventStatistic;
+    ASSERT_STREQ(eventStatistic.ConvertKeyActionToString(KeyEvent::KEY_ACTION_CANCEL), "key_action_cancel");
+
+    ASSERT_STREQ(eventStatistic.ConvertKeyActionToString(KeyEvent::INTENTION_UNKNOWN), "unknown");
+}
+
+/**
+ * @tc.name: EventDumpTest_ConvertSwitchTypeToString
+ * @tc.desc: Event dump ConvertSwitchTypeToString
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(EventStatisticTest, EventStatisticTest_ConvertSwitchTypeToString, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    EventStatistic eventStatistic;
+    int32_t switchType = SwitchEvent::SWITCH_DEFAULT;
+    ASSERT_STREQ(eventStatistic.ConvertSwitchTypeToString(switchType), "switch_default");
+
+    switchType = SwitchEvent::SWITCH_LID;
+    ASSERT_STREQ(eventStatistic.ConvertSwitchTypeToString(switchType), "switch_lid");
+
+    switchType = SwitchEvent::SWITCH_TABLET;
+    ASSERT_STREQ(eventStatistic.ConvertSwitchTypeToString(switchType), "switch_tablet");
+
+    switchType = SwitchEvent::SWITCH_PRIVACY;
+    ASSERT_STREQ(eventStatistic.ConvertSwitchTypeToString(switchType), "switch_privacy");
+
+    switchType = -1;
+    ASSERT_STREQ(eventStatistic.ConvertSwitchTypeToString(switchType), "unknown");
+}
+
+/**
  * @tc.name: EventDumpTest_PopEvent
  * @tc.desc: Event dump PopEvent
  * @tc.type: FUNC
@@ -147,9 +388,10 @@ HWTEST_F(EventStatisticTest, EventStatisticTest_PopEvent, TestSize.Level1)
 {
     CALL_TEST_DEBUG;
     EventStatistic eventStatistic;
+    auto inputEvent = std::make_shared<InputEvent>(3);
     eventStatistic.writeFileEnabled_ = true;
+    ASSERT_NO_FATAL_FAILURE(eventStatistic.PushEvent(inputEvent));
     std::string str = "";
-    ASSERT_NO_FATAL_FAILURE(eventStatistic.PushEventStr(str));
     str = eventStatistic.PopEvent();
     ASSERT_TRUE(!str.empty());
 }
