3f00fc0e48da26414df42daa6d9c247b62e23deb
Justin_Hu <hugongcheng@huawei.com>
2025-05-08 04:00:08 +0000

    fix code
    Signed-off-by: Justin_Hu <hugongcheng@huawei.com>

--- service/window_manager/src/input_windows_manager.cpp
+++ service/window_manager/src/input_windows_manager.cpp
@@ -1716,6 +1716,7 @@ void InputWindowsManager::UpdateDisplayInfo(DisplayGroupInfo &displayGroupInfo)
 #endif // OHOS_BUILD_ENABLE_POINTER_DRAWING
 #endif // OHOS_BUILD_ENABLE_POINTER
 }
+}
 
 #if defined(OHOS_BUILD_ENABLE_POINTER) && defined(OHOS_BUILD_ENABLE_POINTER_DRAWING)
 void InputWindowsManager::AdjustDisplayRotation(int32_t groupId)
