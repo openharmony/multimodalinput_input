5c340a50c310487ad871b3bdaf9e3483df85eff0
郭旭 <guoxu34@huawei.com>
2025-05-27 12:50:23 +0000

    update tools/inject_event/include/event_replayer.h.
    Signed-off-by: 郭旭 <guoxu34@huawei.com>

--- tools/inject_event/include/event_replayer.h
+++ tools/inject_event/include/event_replayer.h
@@ -47,8 +47,8 @@ private:
     std::unordered_map<uint32_t, std::vector<input_event>> deviceEventBuffers_;
     std::map<uint16_t, std::string> deviceMapping_;
     std::map<std::string, std::string> hashToDevicePath_;
-    unsigned long lastSec_{0};
-    unsigned long lastUsec_{0};
+    long lastSec_{0};
+    long lastUsec_{0};
     bool firstEvent_{true};
 };
 } // namespace MMI
