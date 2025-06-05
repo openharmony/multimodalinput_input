0a63cfdd66231c25fc538d8e3ec06c28312cb196
second_89ed <l2311057382@gmail.com>
2025-06-05 21:13:40 +0000

    update service/window_manager/include/input_windows_manager.h.
    Signed-off-by: second_89ed <l2311057382@gmail.com>

--- service/window_manager/include/input_windows_manager.h
+++ service/window_manager/include/input_windows_manager.h
@@ -227,6 +227,7 @@ public:
 #endif // OHOS_BUILD_ENABLE_TOUCH
     void SwitchTouchTracking(bool touchTracking);
 #ifdef OHOS_BUILD_ENABLE_VKEYBOARD
+    bool IsPointInsideGuideWindow(double x, double y);
     bool IsMouseInCastWindow();
     bool IsCaptureMode();
 #endif // OHOS_BUILD_ENABLE_VKEYBOARD
