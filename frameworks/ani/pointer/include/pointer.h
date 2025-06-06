02be64ff90452da5db90dcf8b91e99af2ab45ad6
limabiao <limabiao1@h-partners.com>
2025-06-06 19:37:44 +0800

    multimodalinput ANI改造
    Signed-off-by: limabiao <limabiao1@h-partners.com>
    Change-Id: Ifaea9404414887876bd46846b6448053c29596b9

---
+++ frameworks/ani/pointer/include/pointer.h
@@ -0,0 +1,32 @@
+ /*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ANI_MULTI_MODAL_INPUT_POINTER_H
+#define ANI_MULTI_MODAL_INPUT_POINTER_H
+
+#include <ani.h>
+#include <array>
+#include <list>
+#include <map>
+#include <set>
+#include <string>
+
+#define SUCCESS_CODE 0
+#define ERROR_CODE (-1)
+#define UNREGISTERED_CODE (-2)
+#define PRE_KEY_MAX_COUNT 4
+
+
+#endif // ANI_MULTI_MODAL_INPUT_POINTER_H
