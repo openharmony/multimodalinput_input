213c444dcdf58e206269a403589fba485ea18e47
王力 <wangli427@huawei.com>
2025-06-20 03:56:25 +0000

    update service/mouse_event_normalize/src/mouse_transform_processor.cpp.
    Signed-off-by: 王力 <wangli427@huawei.com>

--- service/mouse_event_normalize/src/mouse_transform_processor.cpp
+++ service/mouse_event_normalize/src/mouse_transform_processor.cpp
@@ -62,6 +62,7 @@ const char* DEVICE_TYPE_M_PC { "M_PC" };
 const char* DEVICE_TYPE_M_TABLET1 { "MRDI" };
 const char* DEVICE_TYPE_M_TABLET2 { "MRO" };
 const char* DEVICE_TYPE_M_TABLET3 { "MRDIL" };
+const char* DEVICE_TYPE_Q_TABLET { "Q_TABLET" };
 const std::string PRODUCT_TYPE = OHOS::system::GetParameter("const.build.product", "HYM");
 const std::string MOUSE_FILE_NAME { "mouse_settings.xml" };
 const std::string TOUCHPAD_FILE_NAME { "touchpad_settings.xml" };
@@ -804,6 +805,9 @@ double MouseTransformProcessor::HandleAxisAccelateTouchPad(double axisValue)
     if (PRODUCT_TYPE == DEVICE_TYPE_M_TABLET1 || PRODUCT_TYPE == DEVICE_TYPE_M_TABLET2 || 
         PRODUCT_TYPE == DEVICE_TYPE_M_TABLET3) {
         deviceType = DeviceType::DEVICE_M_TABLET;
+    }
+    if (PRODUCT_TYPE == DEVICE_TYPE_Q_TABLET) {
+        deviceType = DeviceType::DEVICE_Q_TABLET;
     }
 #ifdef OHOS_BUILD_ENABLE_VKEYBOARD
     if (isVirtualDeviceEvent_) {
