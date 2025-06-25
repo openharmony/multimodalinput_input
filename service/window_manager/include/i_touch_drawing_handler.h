f4a53124cfa0691dd82f7398442d5c6d749b8fa2
blueyouth <bailiang18@huawei.com>
2025-06-25 19:23:24 +0800

    Load touch drawing dynamically (part 1)
    Signed-off-by: blueyouth <bailiang18@huawei.com>
    Change-Id: I3cefc201ab58e8e9dc04c183e8b839ab7674b16e

---
+++ service/window_manager/include/i_touch_drawing_handler.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2023-2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef I_TOUCH_DRAWING_HANDLER_H
+#define I_TOUCH_DRAWING_HANDLER_H
+
+#include "pointer_event.h"
+#include "window_info.h"
+
+namespace OHOS {
+namespace MMI {
+class ITouchDrawingHandler {
+public:
+    ITouchDrawingHandler() = default;
+    virtual ~ITouchDrawingHandler() = default;
+
+    virtual void UpdateDisplayInfo(const DisplayInfo &displayInfo) = 0;
+    virtual void TouchDrawHandler(std::shared_ptr<PointerEvent> pointerEvent) = 0;
+    virtual void RotationScreen() = 0;
+    virtual void UpdateLabels(bool isOn) = 0;
+    virtual void UpdateBubbleData(bool isOn) = 0;
+    virtual void SetMultiWindowScreenId(uint64_t screenId, uint64_t displayNodeScreenId) = 0;
+    virtual void Dump(int32_t fd, const std::vector<std::string> &args) = 0;
+    virtual bool IsWindowRotation() const = 0;
+};
+} // namespace MMI
+} // namespace OHOS
+#endif // I_TOUCH_DRAWING_HANDLER_H
