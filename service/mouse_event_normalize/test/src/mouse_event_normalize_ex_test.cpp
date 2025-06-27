5af5c737c5030280d72cade197a3f6da2e79904a
Rosita <wangjing596@huawei.com>
2025-06-27 07:22:56 +0000

    fix ut
    Signed-off-by: Rosita <wangjing596@huawei.com>
    Change-Id: Ibcdd02485a0c650f1fde7d5c1532ec9220282955

--- service/mouse_event_normalize/test/src/mouse_event_normalize_ex_test.cpp
+++ service/mouse_event_normalize/test/src/mouse_event_normalize_ex_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -28,12 +28,12 @@ using namespace testing::ext;
 }
 class MouseEventNormalizeEXTest : public testing::Test {
 public:
-    static void SetUpTestCase(void) {};
-    static void TearDownTestCase(void) {};
+    static void SetUpTestCase(void) {}
+    static void TearDownTestCase(void) {}
 };
  
 /**
- * @tc.name: MouseTransformProcessorTest_HandleTouchpadLeftButton_001
+ * @tc.name: MouseEventNormalizeEXTest_GetPointerLocation_001
  * @tc.desc: Test GetPointerLocation
  * @tc.type: FUNC
  * @tc.require:
