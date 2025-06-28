63230950b25a8a3d69f9cb5862e8f7b0bc32c7f0
pwx1285814 <panzhi9@huawei.com>
2025-06-28 09:10:28 +0800

    补充UT用例
    Signed-off-by: pwx1285814 <panzhi9@huawei.com>
    Change-Id: Ia5ebbaae22250a49cd41b1c67384f47e13276904

--- service/crown_transform_processor/test/src/crown_transform_processor_test.cpp
+++ service/crown_transform_processor/test/src/crown_transform_processor_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -21,267 +21,311 @@
 #include "i_input_windows_manager.h"
 #include "input_device_manager.h"
 #include "input_event_handler.h"
-#include "libinput.h"
+#include "libinput_mock.h"
 #include "libinput_wrapper.h"
+#include "libinput.h"
 #include "mmi_log.h"
+#include "timer_manager.h"
 #include "window_info.h"
 
 #undef MMI_LOG_TAG
 #define MMI_LOG_TAG "CrownTransformProcessorTest"
 
+using namespace testing::ext;
+using namespace testing;
+
 namespace OHOS {
 namespace MMI {
-namespace {
-using namespace testing::ext;
+bool TimerManager::IsExist(int32_t timerId)
+{
+    return true;
 }
+
 class CrownTransformProcessorTest : public testing::Test {
 public:
-    static void SetUpTestCase(void);
-    static void TearDownTestCase(void);
-    static void SetupCrown();
-    static void CloseCrown();
-    static void InitHandler();
-    libinput_event *GetEvent();
-
-private:
-    static GeneralCrown vCrown_;
-    static LibinputWrapper libinput_;
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp();
+    void TearDown();
 };
 
-GeneralCrown CrownTransformProcessorTest::vCrown_;
-LibinputWrapper CrownTransformProcessorTest::libinput_;
+void CrownTransformProcessorTest::SetUpTestCase() {}
 
-void CrownTransformProcessorTest::SetUpTestCase(void)
-{
-    ASSERT_TRUE(libinput_.Init());
-    SetupCrown();
-    InitHandler();
-}
+void CrownTransformProcessorTest::TearDownTestCase() {}
 
-void CrownTransformProcessorTest::TearDownTestCase(void)
-{
-    CloseCrown();
-}
+void CrownTransformProcessorTest::SetUp() {}
 
-void CrownTransformProcessorTest::SetupCrown()
-{
-    ASSERT_TRUE(vCrown_.SetUp());
-    MMI_HILOGD("Device node name:%{public}s", vCrown_.GetDevPath().c_str());
-    ASSERT_TRUE(libinput_.AddPath(vCrown_.GetDevPath()));
+void CrownTransformProcessorTest::TearDown() {}
 
-    libinput_event *event = libinput_.Dispatch();
-    ASSERT_TRUE(event != nullptr);
-    ASSERT_EQ(libinput_event_get_type(event), LIBINPUT_EVENT_DEVICE_ADDED);
-    struct libinput_device *device = libinput_event_get_device(event);
-    ASSERT_TRUE(device != nullptr);
-    INPUT_DEV_MGR->OnInputDeviceAdded(device);
-}
-
-void CrownTransformProcessorTest::CloseCrown()
-{
-    libinput_.RemovePath(vCrown_.GetDevPath());
-    vCrown_.Close();
-}
-
-void CrownTransformProcessorTest::InitHandler()
-{
-    UDSServer udsServer;
-    std::shared_ptr<OHOS::MMI::InputEventHandler> inputHandler = InputHandler;
-    ASSERT_NO_FATAL_FAILURE(inputHandler->Init(udsServer));
-}
-
-libinput_event *CrownTransformProcessorTest::GetEvent()
-{
-    libinput_event *event = nullptr;
-    libinput_event *evt = libinput_.Dispatch();
-    while (evt != nullptr) {
-        auto type = libinput_event_get_type(evt);
-        if (type == LIBINPUT_EVENT_POINTER_AXIS) {
-            event = evt;
-        }
-        evt = libinput_.Dispatch();
-    }
-    return event;
-}
-
-/**
+/* *
  * @tc.name: CrownTransformProcessorTest_GetPointerEvent_001
- * @tc.desc: Test GetPointerEvent
- * @tc.type: FUNC
+ * @tc.desc: Test the funcation GetPointerEvent
+ * @tc.type: Function
  * @tc.require:
  */
 HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_GetPointerEvent_001, TestSize.Level1)
 {
     CALL_TEST_DEBUG;
-    auto ret = CROWN_EVENT_HDR->GetPointerEvent();
+    std::shared_ptr<CrownTransformProcessor> processor = std::make_shared<CrownTransformProcessor>();
+    auto ret = processor->GetPointerEvent();
     ASSERT_NE(ret, nullptr);
 }
 
-/**
+/* *
+ * @tc.name: CrownTransformProcessorTest_IsCrownEvent_001
+ * @tc.desc: Test the funcation IsCrownEvent
+ * @tc.type: Function
+ * @tc.require:
+ */
+HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_IsCrownEvent_001, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    std::shared_ptr<CrownTransformProcessor> processor = std::make_shared<CrownTransformProcessor>();
+    libinput_event *event = nullptr;
+    bool ret = processor->IsCrownEvent(event);
+    EXPECT_FALSE(ret);
+}
+
+/* *
  * @tc.name: CrownTransformProcessorTest_IsCrownEvent_002
- * @tc.desc: Test IsCrownEvent
- * @tc.type: FUNC
+ * @tc.desc: Test the funcation IsCrownEvent
+ * @tc.type: Function
  * @tc.require:
  */
 HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_IsCrownEvent_002, TestSize.Level1)
 {
     CALL_TEST_DEBUG;
-    vCrown_.SendEvent(EV_REL, REL_WHEEL, 5);
-    vCrown_.SendEvent(EV_SYN, SYN_REPORT, 0);
-    libinput_event *event = GetEvent();
-    ASSERT_TRUE(event != nullptr);
-    struct libinput_device *dev = libinput_event_get_device(event);
-    ASSERT_TRUE(dev != nullptr);
-    std::string name = libinput_device_get_name(dev);
-    MMI_HILOGD("Pointer device:%{public}s", name.c_str());
-    ASSERT_TRUE(CROWN_EVENT_HDR->IsCrownEvent(event));
+    std::shared_ptr<CrownTransformProcessor> processor = std::make_shared<CrownTransformProcessor>();
+    NiceMock<LibinputInterfaceMock> libinputMock;
+    libinput_device *device = nullptr;
+    libinput_event_pointer touchpadButtonEvent;
+    EXPECT_CALL(libinputMock, GetDevice).WillOnce(Return(device));
+    libinput_event event;
+    bool ret = processor->IsCrownEvent(&event);
+    EXPECT_FALSE(ret);
 }
 
+/* *
+ * @tc.name: CrownTransformProcessorTest_IsCrownEvent_003
+ * @tc.desc: Test the funcation IsCrownEvent
+ * @tc.type: Function
+ * @tc.require:
+ */
+HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_IsCrownEvent_003, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    std::shared_ptr<CrownTransformProcessor> processor = std::make_shared<CrownTransformProcessor>();
+    NiceMock<LibinputInterfaceMock> libinputMock;
+    libinput_device device;
+    libinput_event_pointer touchpadButtonEvent;
+    EXPECT_CALL(libinputMock, GetDevice).WillOnce(Return(&device));
+    EXPECT_CALL(libinputMock, DeviceGetName).WillOnce(Return(const_cast<char *>("rotary_crown")));
+    EXPECT_CALL(libinputMock, GetEventType).WillOnce(Return(LIBINPUT_EVENT_POINTER_AXIS));
+    libinput_event event;
+    bool ret = processor->IsCrownEvent(&event);
+    EXPECT_FALSE(ret);
+}
 
-/**
+/* *
+ * @tc.name: CrownTransformProcessorTest_IsCrownEvent_004
+ * @tc.desc: Test the funcation IsCrownEvent
+ * @tc.type: Function
+ * @tc.require:
+ */
+HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_IsCrownEvent_004, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    std::shared_ptr<CrownTransformProcessor> processor = std::make_shared<CrownTransformProcessor>();
+    NiceMock<LibinputInterfaceMock> libinputMock;
+    libinput_device device;
+    libinput_event_pointer touchpadButtonEvent;
+    EXPECT_CALL(libinputMock, GetDevice).WillOnce(Return(&device));
+    EXPECT_CALL(libinputMock, DeviceGetName).WillOnce(Return(const_cast<char *>("Virtual Crown")));
+    EXPECT_CALL(libinputMock, GetEventType).WillOnce(Return(LIBINPUT_EVENT_NONE));
+    libinput_event event;
+    bool ret = processor->IsCrownEvent(&event);
+    EXPECT_FALSE(ret);
+}
+
+/* *
+ * @tc.name: CrownTransformProcessorTest_IsCrownEvent_005
+ * @tc.desc: Test the funcation IsCrownEvent
+ * @tc.type: Function
+ * @tc.require:
+ */
+HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_IsCrownEvent_005, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    std::shared_ptr<CrownTransformProcessor> processor = std::make_shared<CrownTransformProcessor>();
+    NiceMock<LibinputInterfaceMock> libinputMock;
+    libinput_device device;
+    libinput_event_pointer touchpadButtonEvent;
+    EXPECT_CALL(libinputMock, GetDevice).WillOnce(Return(&device));
+    EXPECT_CALL(libinputMock, DeviceGetName).WillOnce(Return(const_cast<char *>("Other")));
+    libinput_event event;
+    bool ret = processor->IsCrownEvent(&event);
+    EXPECT_FALSE(ret);
+}
+
+/* *
+ * @tc.name: CrownTransformProcessorTest_NormalizeRotateEvent_001
+ * @tc.desc: Test the funcation NormalizeRotateEvent
+ * @tc.type: Function
+ * @tc.require:
+ */
+HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_NormalizeRotateEvent_001, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    std::shared_ptr<CrownTransformProcessor> processor = std::make_shared<CrownTransformProcessor>();
+    libinput_event *event = nullptr;
+    int32_t ret = processor->NormalizeRotateEvent(event);
+    EXPECT_EQ(ret, ERROR_NULL_POINTER);
+}
+
+/* *
+ * @tc.name: CrownTransformProcessorTest_NormalizeRotateEvent_002
+ * @tc.desc: Test the funcation NormalizeRotateEvent
+ * @tc.type: Function
+ * @tc.require:
+ */
+HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_NormalizeRotateEvent_002, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    std::shared_ptr<CrownTransformProcessor> processor = std::make_shared<CrownTransformProcessor>();
+    NiceMock<LibinputInterfaceMock> libinputMock;
+    libinput_device *device = nullptr;
+    EXPECT_CALL(libinputMock, GetDevice).WillOnce(Return(device));
+    libinput_event event;
+    int32_t ret = processor->NormalizeRotateEvent(&event);
+    EXPECT_EQ(ret, ERROR_NULL_POINTER);
+}
+
+/* *
  * @tc.name: CrownTransformProcessorTest_NormalizeRotateEvent_003
- * @tc.desc: Test NormalizeRotateEvent
- * @tc.type: FUNC
+ * @tc.desc: Test the funcation NormalizeRotateEvent
+ * @tc.type: Function
  * @tc.require:
  */
 HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_NormalizeRotateEvent_003, TestSize.Level1)
 {
     CALL_TEST_DEBUG;
-    vCrown_.SendEvent(EV_REL, REL_WHEEL, 5);
-    vCrown_.SendEvent(EV_SYN, SYN_REPORT, 0);
-    libinput_event *event = GetEvent();
-    ASSERT_TRUE(event != nullptr);
-    struct libinput_device *dev = libinput_event_get_device(event);
-    ASSERT_TRUE(dev != nullptr);
-    std::string name = libinput_device_get_name(dev);
-    MMI_HILOGD("Pointer device:%{public}s", name.c_str());
-    int32_t result = CROWN_EVENT_HDR->NormalizeRotateEvent(event);
-    EXPECT_NE(result, RET_OK);
+    std::shared_ptr<CrownTransformProcessor> processor = std::make_shared<CrownTransformProcessor>();
+    NiceMock<LibinputInterfaceMock> libinputMock;
+    libinput_event_pointer touchpadButtonEvent;
+    libinput_device device;
+    EXPECT_CALL(libinputMock, GetDevice).WillOnce(Return(&device));
+    libinput_event event;
+    int32_t ret = processor->NormalizeRotateEvent(&event);
+    EXPECT_EQ(ret, RET_ERR);
 }
 
-/**
- * @tc.name: CrownTransformProcessorTest_HandleCrownRotateBegin_004
- * @tc.desc: Test HandleCrownRotateBegin
- * @tc.type: FUNC
+/* *
+ * @tc.name: CrownTransformProcessorTest_HandleCrownRotateBegin_001
+ * @tc.desc: Test the funcation HandleCrownRotateBegin
+ * @tc.type: Function
  * @tc.require:
  */
-HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_HandleCrownRotateBegin_004, TestSize.Level1)
+HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_HandleCrownRotateBegin_001, TestSize.Level1)
 {
     CALL_TEST_DEBUG;
-    vCrown_.SendEvent(EV_REL, REL_WHEEL, 30);
-    vCrown_.SendEvent(EV_SYN, SYN_REPORT, 0);
-    libinput_event *event = GetEvent();
-    ASSERT_TRUE(event != nullptr);
-    struct libinput_device *dev = libinput_event_get_device(event);
-    ASSERT_TRUE(dev != nullptr);
-    std::string name = libinput_device_get_name(dev);
-    MMI_HILOGD("Pointer device:%{public}s", name.c_str());
-    struct libinput_event_pointer *rawPointerEvent = libinput_event_get_pointer_event(event);
-    ASSERT_TRUE(rawPointerEvent != nullptr);
-    int32_t result = CROWN_EVENT_HDR->HandleCrownRotateBegin(rawPointerEvent);
-    EXPECT_EQ(result, RET_OK);
+    std::shared_ptr<CrownTransformProcessor> processor = std::make_shared<CrownTransformProcessor>();
+    libinput_event_pointer *rawPointerEvent = nullptr;
+    int32_t ret = processor->HandleCrownRotateBegin(rawPointerEvent);
+    EXPECT_EQ(ret, ERROR_NULL_POINTER);
 }
 
-/**
- * @tc.name: CrownTransformProcessorTest_HandleCrownRotateUpdate_005
- * @tc.desc: Test HandleCrownRotateUpdate
- * @tc.type: FUNC
+/* *
+ * @tc.name: CrownTransformProcessorTest_HandleCrownRotateUpdate_001
+ * @tc.desc: Test the funcation HandleCrownRotateUpdate
+ * @tc.type: Function
  * @tc.require:
  */
-HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_HandleCrownRotateUpdate_005, TestSize.Level1)
+HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_HandleCrownRotateUpdate_001, TestSize.Level1)
 {
     CALL_TEST_DEBUG;
-    vCrown_.SendEvent(EV_REL, REL_WHEEL, -30);
-    vCrown_.SendEvent(EV_SYN, SYN_REPORT, 0);
-    libinput_event *event = GetEvent();
-    ASSERT_TRUE(event != nullptr);
-    struct libinput_device *dev = libinput_event_get_device(event);
-    ASSERT_TRUE(dev != nullptr);
-    std::string name = libinput_device_get_name(dev);
-    MMI_HILOGD("Pointer device:%{public}s", name.c_str());
-    struct libinput_event_pointer *rawPointerEvent = libinput_event_get_pointer_event(event);
-    ASSERT_TRUE(rawPointerEvent != nullptr);
-    int32_t result = CROWN_EVENT_HDR->HandleCrownRotateUpdate(rawPointerEvent);
-    EXPECT_EQ(result, RET_OK);
+    std::shared_ptr<CrownTransformProcessor> processor = std::make_shared<CrownTransformProcessor>();
+    libinput_event_pointer *rawPointerEvent = nullptr;
+    int32_t ret = processor->HandleCrownRotateUpdate(rawPointerEvent);
+    EXPECT_EQ(ret, ERROR_NULL_POINTER);
 }
 
-/**
- * @tc.name: CrownTransformProcessorTest_HandleCrownRotateEnd_006
- * @tc.desc: Test HandleCrownRotateEnd
- * @tc.type: FUNC
+/* *
+ * @tc.name: CrownTransformProcessorTest_HandleCrownRotateEnd_001
+ * @tc.desc: Test the funcation HandleCrownRotateEnd
+ * @tc.type: Function
  * @tc.require:
  */
-HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_HandleCrownRotateEnd_006, TestSize.Level1)
+HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_HandleCrownRotateEnd_001, TestSize.Level1)
 {
     CALL_TEST_DEBUG;
-    ASSERT_NO_FATAL_FAILURE(CROWN_EVENT_HDR->HandleCrownRotateEnd());
+    std::shared_ptr<CrownTransformProcessor> processor = std::make_shared<CrownTransformProcessor>();
+    int32_t ret = processor->HandleCrownRotateEnd();
+    EXPECT_EQ(ret, RET_OK);
 }
 
-/**
- * @tc.name: CrownTransformProcessorTest_HandleCrownRotateBeginAndUpdate_007
- * @tc.desc: Test HandleCrownRotateBeginAndUpdate
- * @tc.type: FUNC
+/* *
+ * @tc.name: CrownTransformProcessorTest_HandleCrownRotateBeginAndUpdate_001
+ * @tc.desc: Test the funcation HandleCrownRotateBeginAndUpdate
+ * @tc.type: Function
  * @tc.require:
  */
-HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_HandleCrownRotateBeginAndUpdate_007, TestSize.Level1)
+HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_HandleCrownRotateBeginAndUpdate_001, TestSize.Level1)
 {
     CALL_TEST_DEBUG;
-    vCrown_.SendEvent(EV_REL, REL_WHEEL, 10);
-    vCrown_.SendEvent(EV_SYN, SYN_REPORT, 0);
-    libinput_event *event = GetEvent();
-    ASSERT_TRUE(event != nullptr);
-    struct libinput_device *dev = libinput_event_get_device(event);
-    ASSERT_TRUE(dev != nullptr);
-    std::string name = libinput_device_get_name(dev);
-    MMI_HILOGD("Pointer device:%{public}s", name.c_str());
-    struct libinput_event_pointer *rawPointerEvent = libinput_event_get_pointer_event(event);
-    ASSERT_TRUE(rawPointerEvent != nullptr);
-    int32_t result = CROWN_EVENT_HDR->HandleCrownRotateBeginAndUpdate(rawPointerEvent,
-        PointerEvent::POINTER_ACTION_AXIS_BEGIN);
-    EXPECT_EQ(result, RET_OK);
+    std::shared_ptr<CrownTransformProcessor> processor = std::make_shared<CrownTransformProcessor>();
+    libinput_event_pointer *rawPointerEvent = nullptr;
+    int32_t action = 0;
+    int32_t ret = processor->HandleCrownRotateBeginAndUpdate(rawPointerEvent, action);
+    EXPECT_EQ(ret, ERROR_NULL_POINTER);
 }
 
-/**
- * @tc.name: CrownTransformProcessorTest_HandleCrownRotatePostInner_008
- * @tc.desc: Test HandleCrownRotatePostInner
- * @tc.type: FUNC
+/* *
+ * @tc.name: CrownTransformProcessorTest_HandleCrownRotateBeginAndUpdate_002
+ * @tc.desc: Test the funcation HandleCrownRotateBeginAndUpdate
+ * @tc.type: Function
  * @tc.require:
  */
-HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_HandleCrownRotatePostInner_008, TestSize.Level1)
+HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_HandleCrownRotateBeginAndUpdate_002, TestSize.Level1)
 {
     CALL_TEST_DEBUG;
-    double angularVelocity = 67.0;
-    double degree = 11.0;
-    int32_t action = PointerEvent::POINTER_ACTION_AXIS_UPDATE;
-    ASSERT_NO_FATAL_FAILURE(CROWN_EVENT_HDR->HandleCrownRotatePostInner(angularVelocity, degree, action));
+    std::shared_ptr<CrownTransformProcessor> processor = std::make_shared<CrownTransformProcessor>();
+    libinput_event_pointer *rawPointerEvent = nullptr;
+    int32_t action = PointerEvent::POINTER_ACTION_UNKNOWN;
+    int32_t ret = processor->HandleCrownRotateBeginAndUpdate(rawPointerEvent, action);
+    EXPECT_EQ(ret, ERROR_NULL_POINTER);
 }
 
-/**
- * @tc.name: CrownTransformProcessorTest_DumpInner_009
- * @tc.desc: Test DumpInner
- * @tc.type: FUNC
+/* *
+ * @tc.name: CrownTransformProcessorTest_HandleCrownRotatePostInner_001
+ * @tc.desc: Test the funcation HandleCrownRotatePostInner
+ * @tc.type: Function
  * @tc.require:
  */
-HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_DumpInner_009, TestSize.Level1)
+HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_HandleCrownRotatePostInner_001, TestSize.Level1)
 {
     CALL_TEST_DEBUG;
-    ASSERT_NO_FATAL_FAILURE(CROWN_EVENT_HDR->DumpInner());
+    std::shared_ptr<CrownTransformProcessor> processor = std::make_shared<CrownTransformProcessor>();
+    processor->pointerEvent_ = nullptr;
+    double velocity = 0;
+    double degree = 0;
+    int32_t action = 0;
+    EXPECT_NO_FATAL_FAILURE(processor->HandleCrownRotatePostInner(velocity, degree, action));
 }
 
-/**
- * @tc.name: CrownTransformProcessorTest_Dump_010
- * @tc.desc: Test Dump
- * @tc.type: FUNC
+/* *
+ * @tc.name: CrownTransformProcessorTest_Dump_001
+ * @tc.desc: Test the funcation Dump
+ * @tc.type: Function
  * @tc.require:
  */
-HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_Dump_010, TestSize.Level1)
+HWTEST_F(CrownTransformProcessorTest, CrownTransformProcessorTest_Dump_001, TestSize.Level1)
 {
     CALL_TEST_DEBUG;
+    std::shared_ptr<CrownTransformProcessor> processor = std::make_shared<CrownTransformProcessor>();
+    processor->pointerEvent_ = nullptr;
+    int32_t fd = -1;
     std::vector<std::string> args;
-    std::vector<std::string> idNames;
-    int32_t fd = 0;
-    CROWN_EVENT_HDR->Dump(fd, args);
-    ASSERT_EQ(args, idNames);
+    EXPECT_NO_FATAL_FAILURE(processor->Dump(fd, args));
 }
-}
-}
+} // namespace MMI
+} // namespace OHOS
---
+++ service/fingersense_wrapper/test/src/fingersense_wrapper_test.cpp
@@ -0,0 +1,284 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gmock/gmock.h>
+
+#include <dlfcn.h>
+#include "define_multimodal.h"
+#include "fingersense_wrapper.h"
+#include "pointer_event.h"
+
+#undef MMI_LOG_TAG
+#define MMI_LOG_TAG "FingersenseWrapperTest"
+
+namespace OHOS {
+namespace MMI {
+namespace {
+using namespace testing;
+using namespace testing::ext;
+
+const float EPSILON = 10.0;
+} // namespace
+class FingersenseWrapperTest : public testing::Test {
+public:
+    static void SetUpTestCase(void){};
+    static void TearDownTestCase(void){};
+    void SetUp()
+    {
+        wrapper_ = std::make_shared<FingersenseWrapper>();
+    };
+    void TearDown(){};
+
+    std::shared_ptr<FingersenseWrapper> wrapper_;
+};
+
+#ifdef OHOS_BUILD_ENABLE_FINGERSENSE
+/* *
+ * @tc.name  : CrownTransformProcessorTest_SaveTouchInfo_001
+ * @tc.desc: Test the funcation SaveTouchInfo
+ * @tc.type: Function
+ * @tc.require:
+ */
+HWTEST_F(FingersenseWrapperTest, CrownTransformProcessorTest_SaveTouchInfo_001, TestSize.Level1)
+{
+    float pointX = 10.0f;
+    float pointY = 20.0f;
+    int32_t toolType = 1;
+    wrapper_->SaveTouchInfo(pointX, pointY, toolType);
+
+    EXPECT_EQ(wrapper_->touchInfos_.size(), 1);
+    EXPECT_EQ(wrapper_->touchInfos_[0].x, pointX);
+    EXPECT_EQ(wrapper_->touchInfos_[0].y, pointY);
+    EXPECT_EQ(wrapper_->touchInfos_[0].touch_type, toolType);
+}
+
+/* *
+ * @tc.name  : CrownTransformProcessorTest_SaveTouchInfo_002
+ * @tc.desc: Test the funcation SaveTouchInfo
+ * @tc.type: Function
+ * @tc.require:
+ */
+HWTEST_F(FingersenseWrapperTest, CrownTransformProcessorTest_SaveTouchInfo_002, TestSize.Level1)
+{
+    float pointX = 10.0f;
+    float pointY = 20.0f;
+    int32_t toolType = 1;
+    size_t vectorSize = 10;
+    size_t maxVectorSize = 10;
+
+    for (size_t i = 0; i < vectorSize; i++) {
+        TouchInfo touchInfo;
+        touchInfo.x = pointX;
+        touchInfo.y = pointY;
+        touchInfo.touch_type = toolType;
+        wrapper_->touchInfos_.push_back(touchInfo);
+    }
+
+    wrapper_->SaveTouchInfo(pointX, pointY, toolType);
+    EXPECT_EQ(wrapper_->touchInfos_.size(), maxVectorSize);
+}
+
+/* *
+ * @tc.name  : CrownTransformProcessorTest_SaveTouchInfo_003
+ * @tc.desc: Test the funcation SaveTouchInfo
+ * @tc.type: Function
+ * @tc.require:
+ */
+HWTEST_F(FingersenseWrapperTest, CrownTransformProcessorTest_SaveTouchInfo_003, TestSize.Level1)
+{
+    float pointX = 10.0f;
+    float pointY = 20.0f;
+    int32_t toolType = 1;
+    size_t vectorSize = 20;
+    size_t maxVectorSize = 10;
+
+    for (size_t i = 0; i < vectorSize; i++) {
+        TouchInfo touchInfo;
+        touchInfo.x = pointX;
+        touchInfo.y = pointY;
+        touchInfo.touch_type = toolType;
+        wrapper_->touchInfos_.push_back(touchInfo);
+    }
+
+    wrapper_->SaveTouchInfo(pointX, pointY, toolType);
+    EXPECT_EQ(wrapper_->touchInfos_.size(), maxVectorSize);
+}
+
+/* *
+ * @tc.name  : CrownTransformProcessorTest_IsEqual_001
+ * @tc.desc: Test the funcation IsEqual
+ * @tc.type: Function
+ * @tc.require:
+ */
+HWTEST_F(FingersenseWrapperTest, CrownTransformProcessorTest_IsEqual_001, TestSize.Level1)
+{
+    float a = 10.0f;
+    float b = 10.0f;
+    float epsilon = 0.001f;
+
+    bool result = wrapper_->IsEqual(a, b, epsilon);
+    EXPECT_TRUE(result);
+}
+
+/* *
+ * @tc.name  : CrownTransformProcessorTest_IsEqual_002
+ * @tc.desc: Test the funcation IsEqual
+ * @tc.type: Function
+ * @tc.require:
+ */
+HWTEST_F(FingersenseWrapperTest, CrownTransformProcessorTest_IsEqual_002, TestSize.Level1)
+{
+    float a = 10.0f;
+    float b = 10.1f;
+    float epsilon = 0.05f;
+
+    bool result = wrapper_->IsEqual(a, b, epsilon);
+    EXPECT_FALSE(result);
+}
+
+/* *
+ * @tc.name  : CrownTransformProcessorTest_CheckKnuckleEvent_001
+ * @tc.desc: Test the funcation CheckKnuckleEvent
+ * @tc.type: Function
+ * @tc.require:
+ */
+HWTEST_F(FingersenseWrapperTest, CrownTransformProcessorTest_CheckKnuckleEvent_001, TestSize.Level1)
+{
+    float pointX = 10.0f;
+    float pointY = 20.0f;
+    bool isKnuckleType = false;
+
+    int32_t result = wrapper_->CheckKnuckleEvent(pointX, pointY, isKnuckleType);
+    EXPECT_EQ(result, RET_ERR);
+    EXPECT_FALSE(isKnuckleType);
+}
+
+/* *
+ * @tc.name  : CrownTransformProcessorTest_CheckKnuckleEvent_002
+ * @tc.desc: Test the funcation CheckKnuckleEvent
+ * @tc.type: Function
+ * @tc.require:
+ */
+HWTEST_F(FingersenseWrapperTest, CrownTransformProcessorTest_CheckKnuckleEvent_002, TestSize.Level1)
+{
+    float pointX = 10.0f;
+    float pointY = 20.0f;
+    int32_t toolType = MMI::PointerEvent::TOOL_TYPE_KNUCKLE;
+    TouchInfo touchInfo;
+    touchInfo.x = pointX;
+    touchInfo.y = pointY;
+    touchInfo.touch_type = toolType;
+    wrapper_->touchInfos_.push_back(touchInfo);
+    bool isKnuckleType = false;
+
+    int32_t result = wrapper_->CheckKnuckleEvent(pointX, pointY, isKnuckleType);
+    EXPECT_EQ(result, RET_OK);
+    EXPECT_TRUE(isKnuckleType);
+}
+
+/* *
+ * @tc.name  : CrownTransformProcessorTest_CheckKnuckleEvent_003
+ * @tc.desc: Test the funcation CheckKnuckleEvent
+ * @tc.type: Function
+ * @tc.require:
+ */
+HWTEST_F(FingersenseWrapperTest, CrownTransformProcessorTest_CheckKnuckleEvent_003, TestSize.Level1)
+{
+    float pointX = 10.0f;
+    float pointY = 20.0f;
+    int32_t toolType = MMI::PointerEvent::TOOL_TYPE_KNUCKLE;
+    TouchInfo touchInfo;
+    touchInfo.x = pointX;
+    touchInfo.y = pointY + EPSILON;
+    touchInfo.touch_type = toolType;
+    wrapper_->touchInfos_.push_back(touchInfo);
+    bool isKnuckleType = false;
+
+    int32_t result = wrapper_->CheckKnuckleEvent(pointX, pointY, isKnuckleType);
+    EXPECT_EQ(result, RET_ERR);
+    EXPECT_FALSE(isKnuckleType);
+}
+
+/* *
+ * @tc.name  : CrownTransformProcessorTest_CheckKnuckleEvent_004
+ * @tc.desc: Test the funcation CheckKnuckleEvent
+ * @tc.type: Function
+ * @tc.require:
+ */
+HWTEST_F(FingersenseWrapperTest, CrownTransformProcessorTest_CheckKnuckleEvent_004, TestSize.Level1)
+{
+    float pointX = 10.0f;
+    float pointY = 20.0f;
+    int32_t toolType = MMI::PointerEvent::TOOL_TYPE_KNUCKLE;
+    TouchInfo touchInfo;
+    touchInfo.x = pointX;
+    touchInfo.y = pointY + EPSILON * 2;
+    touchInfo.touch_type = toolType;
+    wrapper_->touchInfos_.push_back(touchInfo);
+    bool isKnuckleType = false;
+
+    int32_t result = wrapper_->CheckKnuckleEvent(pointX, pointY, isKnuckleType);
+    EXPECT_EQ(result, RET_ERR);
+    EXPECT_FALSE(isKnuckleType);
+}
+
+/* *
+ * @tc.name  : CrownTransformProcessorTest_CheckKnuckleEvent_005
+ * @tc.desc: Test the funcation CheckKnuckleEvent
+ * @tc.type: Function
+ * @tc.require:
+ */
+HWTEST_F(FingersenseWrapperTest, CrownTransformProcessorTest_CheckKnuckleEvent_005, TestSize.Level1)
+{
+    float pointX = 10.0f;
+    float pointY = 20.0f;
+    int32_t toolType = MMI::PointerEvent::TOOL_TYPE_KNUCKLE;
+    TouchInfo touchInfo;
+    touchInfo.x = pointX + EPSILON;
+    touchInfo.y = pointY;
+    touchInfo.touch_type = toolType;
+    wrapper_->touchInfos_.push_back(touchInfo);
+    bool isKnuckleType = false;
+
+    int32_t result = wrapper_->CheckKnuckleEvent(pointX, pointY, isKnuckleType);
+    EXPECT_EQ(result, RET_ERR);
+    EXPECT_FALSE(isKnuckleType);
+}
+
+/* *
+ * @tc.name  : CrownTransformProcessorTest_CheckKnuckleEvent_006
+ * @tc.desc: Test the funcation CheckKnuckleEvent
+ * @tc.type: Function
+ * @tc.require:
+ */
+HWTEST_F(FingersenseWrapperTest, CrownTransformProcessorTest_CheckKnuckleEvent_006, TestSize.Level1)
+{
+    float pointX = 10.0f;
+    float pointY = 20.0f;
+    int32_t toolType = MMI::PointerEvent::TOOL_TYPE_KNUCKLE;
+    TouchInfo touchInfo;
+    touchInfo.x = pointX + EPSILON * 2;
+    touchInfo.y = pointY;
+    touchInfo.touch_type = toolType;
+    wrapper_->touchInfos_.push_back(touchInfo);
+    bool isKnuckleType = false;
+
+    int32_t result = wrapper_->CheckKnuckleEvent(pointX, pointY, isKnuckleType);
+    EXPECT_EQ(result, RET_ERR);
+    EXPECT_FALSE(isKnuckleType);
+}
+#endif // OHOS_BUILD_ENABLE_FINGERSENSE
+} // namespace MMI
+} // namespace OHOS
