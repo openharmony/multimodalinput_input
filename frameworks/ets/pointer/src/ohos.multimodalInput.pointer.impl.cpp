64e7f7f418cb8a8dbfc01d63ed352764753780af
liyaoyao <liyaoyao7@huawei.com>
2025-07-02 09:03:55 +0000

    update frameworks/ets/pointer/src/ohos.multimodalInput.pointer.impl.cpp.
    Signed-off-by: liyaoyao <liyaoyao7@huawei.com>

--- frameworks/ets/pointer/src/ohos.multimodalInput.pointer.impl.cpp
+++ frameworks/ets/pointer/src/ohos.multimodalInput.pointer.impl.cpp
@@ -103,6 +103,11 @@ void SetPointerVisibleSync(bool visible)
 PointerStyle GetPointerStyleSync(int32_t windowId)
 {
     OHOS::MMI::PointerStyle pointerStyle;
+    if (windowId < 0 && windowId != OHOS::MMI::GLOBAL_WINDOW_ID) {
+        MMI_HILOGE("Invalid windowId");
+        taihe::set_business_error(COMMON_PARAMETER_ERROR, "windowId is invalid");
+        return PointerStyle::key_t::DEFAULT;
+    }
     int32_t ret = OHOS::MMI::InputManager::GetInstance()->GetPointerStyle(windowId, pointerStyle);
     if (ret == COMMON_PARAMETER_ERROR) {
         taihe::set_business_error(ret, "failed to get default GetPointerStyle!");
