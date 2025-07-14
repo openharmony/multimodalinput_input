3207533f6f71e2718155ebf87c5f480a1fc7048c
wuliangdong <wuliangdong1@huawei.com>
2025-07-14 15:09:20 +0800

    Add Tdd for OnConnected
    Signed-off-by: wuliangdong <wuliangdong1@huawei.com>
    Change-Id: Ie0241af8478463dfee033534b2f6c5cf7f6bf414

--- frameworks/proxy/event_handler/test/input_manager_impl_test.cpp
+++ frameworks/proxy/event_handler/test/input_manager_impl_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -488,6 +488,8 @@ HWTEST_F(InputManagerImplTest, InputManagerImplTest_OnConnected_04, TestSize.Lev
     EXPECT_FALSE(InputMgrImpl.userScreenInfo_.displayGroups[DEFAULT_GROUP_ID].windowsInfo.empty());
     ASSERT_NO_FATAL_FAILURE(InputMgrImpl.OnConnected());
 }
+
+
 
 /**
  * @tc.name: InputManagerImplTest_SetPixelMapData_01
