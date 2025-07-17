46a34ddff12c3f03dbf6a999f6d65ccad4dbab90
mr-liang2504 <liangzhicheng10@huawei.com>
2025-07-17 09:01:47 +0800

    Signed-off-by: mr-liang2504 <liangzhicheng10@huawei.com>
    Change-Id: I4d75d522a6ce3c253c15a294ae93c69c3d1b8301

--- service/infrared_emitter/include/i_infrared_emitter_adapter.h
+++ service/infrared_emitter/include/i_infrared_emitter_adapter.h
@@ -39,7 +39,6 @@ public:
     virtual int32_t Transmit(int32_t carrierFreq, const std::vector<int32_t>& pattern, bool& ret) = 0;
     virtual int32_t GetCarrierFreqs(bool& ret,
         std::vector<OHOS::HDI::Consumerir::V1_0::ConsumerIrFreqRange>& range) = 0;
-
 };
 } // namespace MMI
 } // namespace OHOS
