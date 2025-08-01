011aa260354240c7d455256e535e9468b63ac143
zhengyaozong <zhengyaozong@h-partners.com>
2025-08-01 08:57:02 +0000

    update service/joystick/test/joystick_event_processor_test.cpp.
    Signed-off-by: zhengyaozong <zhengyaozong@h-partners.com>

--- service/joystick/test/joystick_event_processor_test.cpp
+++ service/joystick/test/joystick_event_processor_test.cpp
@@ -438,7 +438,7 @@ HWTEST_F(JoystickEventProcessorTest, JoystickEventProcessorTest_Normalize_002, T
 {
     CALL_TEST_DEBUG;
     auto JoystickEvent = new JoystickEventProcessor(2);
-    const struct libinput_event_joystick_axis_abs_info axis = { 
+    const struct libinput_event_joystick_axis_abs_info axis = {
         .code = 1,
         .value = 5,
         .maximum = 0,
