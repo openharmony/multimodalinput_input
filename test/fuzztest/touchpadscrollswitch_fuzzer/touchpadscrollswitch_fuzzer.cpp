f937609f4c2c42dd6c971d4a28ca8e1fd128509f
l30075025 <lifeitong@h-partners.com>
2025-08-04 15:34:25 +0800

    fix
    Signed-off-by: l30075025 <lifeitong@h-partners.com>

---
+++ test/fuzztest/touchpadscrollswitch_fuzzer/touchpadscrollswitch_fuzzer.cpp
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "touchpadscrollswitch_fuzzer.h"
+
+#include "securec.h"
+
+#include "input_manager.h"
+#include "mmi_log.h"
+
+namespace OHOS {
+namespace MMI {
+
+template <class T>
+size_t GetObject(T &object, const uint8_t *data, size_t size)
+{
+    size_t objectSize = sizeof(object);
+    if (objectSize > size) {
+        return 0;
+    }
+    errno_t ret = memcpy_s(&object, objectSize, data, objectSize);
+    if (ret != EOK) {
+        return 0;
+    }
+    return objectSize;
+}
+
+void TouchpadScrollSwitchFuzzTest(const uint8_t *data, size_t size)
+{
+    int32_t random = 0;
+    GetObject<int32_t>(random, data, size);
+    bool enable = (random % 2 == 0);
+    InputManager::GetInstance()->SetTouchpadScrollSwitch(enable);
+    bool state = false;
+    InputManager::GetInstance()->GetTouchpadScrollSwitch(state);
+}
+
+} // namespace MMI
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    OHOS::MMI::TouchpadScrollSwitchFuzzTest(data, size);
+    return 0;
+}
