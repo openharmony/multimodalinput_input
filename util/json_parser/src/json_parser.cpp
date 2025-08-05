7d5ee8fda9309aa15aad1f9bb4851220b9eaa3b0
wuliangdong <wuliangdong1@huawei.com>
2025-08-05 09:23:11 +0000

    update util/json_parser/src/json_parser.cpp.
    Signed-off-by: wuliangdong <wuliangdong1@huawei.com>

--- util/json_parser/src/json_parser.cpp
+++ util/json_parser/src/json_parser.cpp
@@ -55,5 +55,65 @@ cJSON* JsonParser::Get() const
     return json_;
 }
 
+int32_t JsonParser::ParseInt32(cJSON *json, const std::string &key, int32_t &value)
+{
+    cJSON *jsonNode = cJSON_GetObjectItemCaseSensitive(json, key.c_str());
+    CHKPR(jsonNode, RET_ERR);
+    if (!cJSON_IsNumber(jsonNode)) {
+        MMI_HILOGE("value is not number");
+        return RET_ERR;
+    }
+    value = jsonNode->valueint;
+    return RET_OK;
+}
+ 
+int32_t JsonParser::ParseString(cJSON *json, const std::string &key, std::string &value)
+{
+    cJSON *jsonNode = cJSON_GetObjectItemCaseSensitive(json, key.c_str());
+    CHKPR(jsonNode, RET_ERR);
+    if (!cJSON_IsString(jsonNode)) {
+        MMI_HILOGE("value is not str");
+        return RET_ERR;
+    }
+    value = jsonNode->valuestring;
+    return RET_OK;
+}
+ 
+int32_t JsonParser::ParseBool(cJSON *json, const std::string &key, bool &value)
+{
+    cJSON *jsonNode = cJSON_GetObjectItemCaseSensitive(json, key.c_str());
+    CHKPR(jsonNode, RET_ERR);
+    if (!cJSON_IsBool(jsonNode)) {
+        MMI_HILOGE("value is not bool");
+        return RET_ERR;
+    }
+    value = cJSON_IsTrue(jsonNode);
+    return RET_OK;
+}
+ 
+int32_t JsonParser::ParseStringArray(cJSON *json, const std::string &key, std::vector<std::string> &value, int32_t maxSize)
+{
+    cJSON *jsonNode = cJSON_GetObjectItemCaseSensitive(json, key.c_str());
+    CHKPR(jsonNode, RET_ERR);
+    if (!cJSON_IsArray(jsonNode)) {
+        MMI_HILOGE("jsonNode is not array");
+        return RET_ERR;
+    }
+    int32_t arraySize = cJSON_GetArraySize(jsonNode);
+    if (arraySize > maxSize) {
+        MMI_HILOGW("arraySize is too much, truncate it");
+    }
+    value.clear();
+    for (int32_t i = 0; i < std::min(maxSize, arraySize); i++) {
+        cJSON* arrayItem = cJSON_GetArrayItem(jsonNode, i);
+        if (!cJSON_IsString(arrayItem)) {
+            MMI_HILOGE("The arrayItem is not string");
+            return RET_ERR;
+        }
+        value.push_back(arrayItem->valuestring);
+    }
+    return RET_OK;
+}
+
 } // namespace MMI
 } // namespace OHOS
