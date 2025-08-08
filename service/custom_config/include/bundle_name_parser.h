583d889014dc6a2da5553d6e11d72524b3c7a425
wuliangdong <wuliangdong1@huawei.com>
2025-08-08 14:36:59 +0000

    update service/custom_config/include/bundle_name_parser.h.
    Signed-off-by: wuliangdong <wuliangdong1@huawei.com>

--- service/custom_config/include/bundle_name_parser.h
+++ service/custom_config/include/bundle_name_parser.h
@@ -12,3 +12,48 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+ 
+#ifndef BUNDLE_NAME_PARSER_H
+#define BUNDLE_NAME_PARSER_H
+ 
+#include <shared_mutex>
+#include <string>
+#include <map>
+ 
+#include "json_parser.h"
+ 
+namespace OHOS {
+namespace MMI {
+ 
+class BundleNameParser {
+public:
+    BundleNameParser(const BundleNameParser&) = delete;
+    BundleNameParser& operator=(const BundleNameParser&) = delete;
+    static BundleNameParser& GetInstance();
+    int32_t Init();
+    std::string GetBundleName(const std::string &key);
+ 
+private:
+    struct BundleNameItem {
+        std::string placeHolder;
+        std::string bundleName;
+    };
+ 
+private:
+    BundleNameParser() = default;
+    ~BundleNameParser() = default;
+ 
+    int32_t ParseBundleNameMap(const JsonParser &jsonParser);
+    int32_t ParseBundleNameItem(const cJSON *json, BundleNameItem &bundleNameItem);
+    void PrintBundleNames();
+ 
+private:
+    std::map<std::string, std::string> bundleNames_;
+    std::shared_mutex lock_;
+    std::atomic_bool isInitialized_ { false };
+};
+} // namespace MMI
+} // namespace OHOS
+#define BUNDLE_NAME_PARSER OHOS::MMI::BundleNameParser::GetInstance()
+#endif // BUNDLE_NAME_PARSER_H
