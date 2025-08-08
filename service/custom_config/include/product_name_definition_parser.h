ee27cdf2b3c6022b62223bfe3af337f745f63a0b
wuliangdong <wuliangdong1@huawei.com>
2025-08-08 14:38:11 +0000

    update service/custom_config/include/product_name_definition_parser.h.
    Signed-off-by: wuliangdong <wuliangdong1@huawei.com>

--- service/custom_config/include/product_name_definition_parser.h
+++ service/custom_config/include/product_name_definition_parser.h
@@ -12,3 +12,49 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+ #ifndef PRODUCT_NAME_DEFINITION_PARSER_H
+#define PRODUCT_NAME_DEFINITION_PARSER_H
+ 
+#include <shared_mutex>
+#include <string>
+#include <map>
+#include <vector>
+ 
+#include "json_parser.h"
+#include "cJSON.h"
+ 
+namespace OHOS {
+namespace MMI {
+ 
+class ProductNameDefinitionParser {
+public:
+    ProductNameDefinitionParser(const ProductNameDefinitionParser&) = delete;
+    ProductNameDefinitionParser& operator=(const ProductNameDefinitionParser&) = delete;
+    static ProductNameDefinitionParser& GetInstance();
+    int32_t Init();
+    std::string GetProductName(const std::string &key);
+    int32_t ParseProductNameMap(const JsonParser &jsonParser);
+ 
+private:
+    struct ProductNameDefinitionItem {
+        std::string productAlias;
+        std::string productName;
+    };
+ 
+private:
+    ProductNameDefinitionParser() = default;
+    ~ProductNameDefinitionParser() = default;
+    void PrintProductNames();
+    int32_t ParserProductNameItem(const cJSON *json, ProductNameDefinitionItem &productNameItem);
+ 
+private:
+    std::map<std::string, std::string> productNames_;
+    std::shared_mutex lock_;
+    std::atomic_bool isInitialized_ { false };
+};
+ 
+} // namespace MMI
+} // namespace OHOS
+#define PRODUCT_NAME_DEFINITION_PARSER OHOS::MMI::ProductNameDefinitionParser::GetInstance()
+#endif // PRODUCT_NAME_DEFINITION_PARSER_H
