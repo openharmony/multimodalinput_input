a32038d339d0289a07c469a90dae69fa6e9f2254
zhengyaozong <zhengyaozong@h-partners.com>
2025-08-11 17:35:14 +0800

    add fuzztest
    Signed-off-by: zhengyaozong <zhengyaozong@h-partners.com>

--- test/fuzztest/transformsamplewindowxy_fuzzer/transformsamplewindowxy_fuzzer.cpp
+++ test/fuzztest/transformsamplewindowxy_fuzzer/transformsamplewindowxy_fuzzer.cpp
@@ -12,7 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 #include <fuzzer/FuzzedDataProvider.h>
 #include "event_resample.h"
 #include "transformsamplewindowxy_fuzzer.h"
