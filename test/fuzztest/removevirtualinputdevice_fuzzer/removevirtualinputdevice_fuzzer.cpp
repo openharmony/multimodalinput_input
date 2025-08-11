6280ff3d27f0fbe4d1fc7fd960542b0e3a8274d9
l30075025 <lifeitong@h-partners.com>
2025-08-11 11:19:39 +0800

    fix
    Signed-off-by: l30075025 <lifeitong@h-partners.com>

---
+++ test/fuzztest/removevirtualinputdevice_fuzzer/removevirtualinputdevice_fuzzer.cpp
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "removevirtualinputdevice_fuzzer.h"
+
+#include "multimodal_input_connect_stub.h"
+#include "mmi_service.h"
+#include "mmi_log.h"
+
+#undef LOG_TAG
+#define LOG_TAG "RemoveVirtualInputDeviceFuzzTest"
+
+namespace OHOS {
+namespace MMI {
+namespace OHOS {
+
+const std::u16string FORMMGR_INTERFACE_TOKEN { u"ohos.multimodalinput.IConnectManager" };
+
+bool RemoveVirtualInputDeviceFuzzTest(const uint8_t* data, size_t size)
+{
+    if (data == nullptr || size < sizeof(int32_t)) {
+        return false;
+    }
+
+    MessageParcel datas;
+    if (!datas.WriteInterfaceToken(FORMMGR_INTERFACE_TOKEN)) {
+        return false;
+    }
+
+    int32_t deviceId = *(reinterpret_cast<const int32_t*>(data));
+    if (!datas.WriteInt32(deviceId)) {
+        return false;
+    }
+
+    if (!datas.RewindRead(0)) {
+        return false;
+    }
+
+    MessageParcel reply;
+    MessageOption option;
+
+    MMIService::GetInstance()->state_ = ServiceRunningState::STATE_RUNNING;
+    MMIService::GetInstance()->OnRemoteRequest(
+        static_cast<uint32_t>(IMultimodalInputConnectIpcCode::COMMAND_REMOVE_VIRTUAL_INPUT_DEVICE),
+        datas, reply, option);
+    return true;
+}
+
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    if (data == nullptr) {
+        return 0;
+    }
+
+    OHOS::RemoveVirtualInputDeviceFuzzTest(data, size);
+    return 0;
+}
+} // namespace MMI
+} // namespace OHOS
