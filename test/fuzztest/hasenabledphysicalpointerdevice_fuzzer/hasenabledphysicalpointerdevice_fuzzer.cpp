d995c67e702182c96878cded40cfd07189f8676b
xqq879988 <xieqiqi2@huawei.com>
2025-08-11 15:51:08 +0800

    add fuzz
    Signed-off-by: xqq879988 <xieqiqi2@huawei.com>

---
+++ test/fuzztest/hasenabledphysicalpointerdevice_fuzzer/hasenabledphysicalpointerdevice_fuzzer.cpp
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "mmi_log.h"
+#include "input_device_manager.h"
+#include "hasenabledphysicalpointerdevice_fuzzer.h"
+
+#include "securec.h"
+
+#undef MMI_LOG_TAG
+#define MMI_LOG_TAG "HasEnabledPhysicalPointerDeviceFuzzTest"
+
+namespace OHOS {
+namespace MMI {
+namespace OHOS {
+template<class T>
+size_t GetObject(T &object, const uint8_t *data, size_t size)
+{
+    size_t objectSize = sizeof(object);
+    if (objectSize > size) {
+        return 0;
+    }
+    errno_t ret = memcpy_s(&object, objectSize, data, objectSize);
+    if (ret != EOK) {
+        return 0;
+    }
+    return objectSize;
+}
+
+bool HasEnabledPhysicalPointerDeviceFuzzTest(const uint8_t *data, size_t size)
+{
+    size_t startPos = 0;
+    int32_t rowsBefore;
+    startPos += GetObject<int32_t>(rowsBefore, data + startPos, size - startPos);
+    INPUT_DEV_MGR->HasEnabledPhysicalPointerDevice();
+    return true;
+}
+} // namespace OHOS
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    /* Run your code on data */
+    if (data == nullptr) {
+        return 0;
+    }
+
+    OHOS::HasEnabledPhysicalPointerDeviceFuzzTest(data, size);
+    return 0;
+}
+} // namespace MMI
+} // namespace OHOS
