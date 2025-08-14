aa51987b72b96f486bee3009132c403e76ff3c93
xqq879988 <xieqiqi2@huawei.com>
2025-08-14 14:41:19 +0800

    add fuzz
    Signed-off-by: xqq879988 <xieqiqi2@huawei.com>

---
+++ test/fuzztest/keyitem_fuzzer/keyitem_fuzzer.cpp
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "key_event.h"
+#include "keyitem_fuzzer.h"
+#include "fuzzer/FuzzedDataProvider.h"
+#include "mmi_log.h"
+
+#include "securec.h"
+
+#undef MMI_LOG_TAG
+#define MMI_LOG_TAG "KeyItemFuzzTest"
+
+namespace OHOS {
+namespace MMI {
+bool KeyItemFuzzTest(const uint8_t *data, size_t size)
+{
+    FuzzedDataProvider provider(data, size);
+    KeyEvent::KeyItem keyItem;
+    int32_t keyCode = provider.ConsumeIntegral<int32_t>();
+    keyItem.SetKeyCode(keyCode);
+
+    int64_t downTime = provider.ConsumeIntegral<int64_t>();
+    keyItem.SetDownTime(downTime);
+
+    int32_t deviceId = provider.ConsumeIntegral<int32_t>();
+    keyItem.SetDeviceId(deviceId);
+
+    bool pressed = provider.ConsumeBool();
+    keyItem.SetPressed(pressed);
+
+    uint32_t unicode = provider.ConsumeIntegral<uint32_t>();
+    keyItem.SetUnicode(unicode);
+
+    Parcel parcel;
+    keyItem.WriteToParcel(parcel);
+    keyItem.ReadFromParcel(parcel);
+    MMI_HILOGD("KeyItemFuzzTest");
+    return true;
+}
+} // MMI
+} // OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    /* Run your code on data */
+    if (data == nullptr) {
+        return 0;
+    }
+
+    OHOS::MMI::KeyItemFuzzTest(data, size);
+    return 0;
+}
