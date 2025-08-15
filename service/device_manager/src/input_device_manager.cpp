209bca1ac9ad860919e409b00e57df4f2594c51e
Justin_Hu <hugongcheng@huawei.com>
2025-08-15 07:22:04 +0000

    code review
    Signed-off-by: Justin_Hu <hugongcheng@huawei.com>

--- service/device_manager/src/input_device_manager.cpp
+++ service/device_manager/src/input_device_manager.cpp
@@ -260,6 +260,7 @@ int32_t InputDeviceManager::GetDeviceSupportKey(int32_t deviceId, int32_t &keybo
         keyboardType = KEYBOARD_TYPE_UNKNOWN;
         MMI_HILOGD("Undefined keyboard type");
     }
+    MMI_HILOGD("Get keyboard type results by supporting keys:%{public}d", keyboardType);
     return RET_OK;
 }
 
