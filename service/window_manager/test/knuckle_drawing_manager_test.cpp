31a807a269b66d2526a9f8771e5552cb8008ae9a
ninghejuan <ninghejuan@huawei.com>
2025-08-25 09:32:09 +0800

    fix failed test
    Signed-off-by: ninghejuan <ninghejuan@huawei.com>

--- service/window_manager/test/knuckle_drawing_manager_test.cpp
+++ service/window_manager/test/knuckle_drawing_manager_test.cpp
@@ -692,33 +692,6 @@ HWTEST_F(KnuckleDrawingManagerTest, KnuckleDrawingManagerTest_KnuckleDrawHandler
     EXPECT_NO_FATAL_FAILURE(kceDrawMgr.KnuckleDrawHandler(touchEvent));
 }
 
-/**
- * @tc.name: KnuckleDrawingManagerTest_KnuckleDrawHandler_008
- * @tc.desc: Test Overrides KnuckleDrawHandler function branches
- * @tc.type: Function
- * @tc.require:
- */
-HWTEST_F(KnuckleDrawingManagerTest, KnuckleDrawingManagerTest_KnuckleDrawHandler_008, TestSize.Level1)
-{
-    CALL_TEST_DEBUG;
-    KnuckleDrawingManager kceDrawMgr;
-    std::shared_ptr<PointerEvent> touchEvent = PointerEvent::Create();
-    ASSERT_NE(touchEvent, nullptr);
-    touchEvent->SetPointerId(1);
-    touchEvent->SetActionTime(10);
-    touchEvent->SetPointerAction(PointerEvent::POINTER_ACTION_UP);
-    PointerEvent::PointerItem item;
-    item.SetPointerId(1);
-    item.SetDisplayX(150);
-    item.SetDisplayY(150);
-    item.SetToolType(PointerEvent::TOOL_TYPE_KNUCKLE);
-    touchEvent->AddPointerItem(item);
-    item.SetPointerId(2);
-    touchEvent->AddPointerItem(item);
-    kceDrawMgr.isRotate_ = true;
-    constexpr int32_t rsId = 1;
-    EXPECT_NO_FATAL_FAILURE(kceDrawMgr.KnuckleDrawHandler(touchEvent, rsId));
-}
 
 /**
  * @tc.name: KnuckleDrawingManagerTest_IsSingleKnuckleDoubleClick
