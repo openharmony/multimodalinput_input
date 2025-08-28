0261c448d26d97446791d280951d558e955f6e55
xqq879988 <xieqiqi2@huawei.com>
2025-08-28 10:07:40 +0800

    add fuzz coverage
    Signed-off-by: xqq879988 <xieqiqi2@huawei.com>

---
+++ test/fuzztest/pointerevent4_fuzzer/pointerevent4_fuzzer.cpp
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "pointer_event.h"
+#include "fuzzer/FuzzedDataProvider.h"
+#include "mmi_log.h"
+
+#include "securec.h"
+
+#undef MMI_LOG_TAG
+#define MMI_LOG_TAG "PointerEvent4FuzzTest"
+
+namespace OHOS {
+namespace MMI {
+void PointerEvent4FuzzTest(const uint8_t *data, size_t size)
+{
+    FuzzedDataProvider provider(data, size);
+    int32_t eventType = provider.ConsumeIntegral<int32_t>();
+    PointerEvent pointEvent(eventType);
+
+    int32_t pointerId = provider.ConsumeIntegral<int32_t>();
+    PointerEvent::PointerItem item;
+    item.SetPointerId(pointerId);
+
+    pointerId = provider.ConsumeIntegral<int32_t>();
+    pointEvent.GetOriginPointerItem(pointerId, item);
+    pointEvent.RemovePointerItem(pointerId);
+
+    int32_t pullId = provider.ConsumeIntegral<int32_t>();
+    pointEvent.SetPullId(pullId);
+
+    MMI_HILOGD("PointerEvent4FuzzTest");
+}
+} // MMI
+} // OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    /* Run your code on data */
+    if (data == nullptr) {
+        return 0;
+    }
+
+    OHOS::MMI::PointerEvent4FuzzTest(data, size);
+    return 0;
+}
