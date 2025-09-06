bb18d794965aa5fc0d572547d3475d2da5855e0d
erliangqi <zhangjiaxiang7@huawei.com>
2025-09-06 18:47:59 +0800

    input devicemanager Fuzz case rectification
    Signed-off-by: erliangqi <zhangjiaxiang7@huawei.com>

--- test/fuzztest/inputonenable_fuzzer/inputonenable_fuzzer.h
+++ test/fuzztest/inputonenable_fuzzer/inputonenable_fuzzer.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -16,6 +16,6 @@
 #ifndef INPUTDEVICEMANAGER_FUZZER
 #define INPUTDEVICEMANAGER_FUZZER
 
-#define FUZZ_PROJECT_NAME "inputdevicemanager_fuzzer"
+#define FUZZ_PROJECT_NAME "inputonenable_fuzzer"
 
 #endif // INPUTDEVICEMANAGER_FUZZER
