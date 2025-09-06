662da9c283e9b3b0c07c56a4438b01d7791a5c80
erliangqi <zhangjiaxiang7@huawei.com>
2025-09-06 18:47:59 +0800

    input devicemanager Fuzz case rectification
    Signed-off-by: erliangqi <zhangjiaxiang7@huawei.com>

---
+++ test/fuzztest/inputpointerdevice_fuzzer/inputpointerdevice_fuzzer.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <fuzzer/FuzzedDataProvider.h>
+
+#include "input_device_manager.h"
+#include "inputpointerdevice_fuzzer.h"
+#include "mmi_log.h"
+#include "securec.h"
+
+#undef MMI_LOG_TAG
+#define MMI_LOG_TAG "InputDeviceManagerFuzzTest"
+
+namespace OHOS {
+namespace MMI {
+namespace OHOS {
+
+bool InputPointerDeviceFuzzTest(const uint8_t *data, size_t size)
+{
+    FuzzedDataProvider provider(data, size);
+    bool hasPointerDevice = provider.ConsumeBool();
+    bool isVisible = provider.ConsumeBool();
+    bool isHotPlug = provider.ConsumeBool();
+
+    INPUT_DEV_MGR->NotifyPointerDevice(hasPointerDevice, isVisible, isHotPlug);
+
+    return true;
+}
+} // namespace OHOS
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    /* Run your code on data */
+    if (data == nullptr) {
+        return 0;
+    }
+
+    OHOS::InputPointerDeviceFuzzTest(data, size);
+    return 0;
+}
+} // namespace MMI
+} // namespace OHOS
