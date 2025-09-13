1fd8ad615212b8ddbffc0fdd068a1a5bdc6965ac
huxianghao <huxianghao@huawei.com>
2025-09-13 03:54:02 +0000

    [test]:Modify fuzz stubhandle 2
    Signed-off-by: huxianghao <huxianghao@huawei.com>

---
+++ test/fuzztest/stubhandlekeystate_fuzzer/stubhandlekeystate_fuzzer.cpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <fuzzer/FuzzedDataProvider.h>
+#include "stubhandlekeystate_fuzzer.h"
+
+#include "mmi_service.h"
+#include "multimodal_input_connect_stub.h"
+
+#undef MMI_LOG_TAG
+#define MMI_LOG_TAG "StubHandleAllocSocketFdFuzzTest"
+
+class UDSSession;
+using SessionPtr = std::shared_ptr<UDSSession>;
+
+namespace OHOS {
+namespace MMI {
+
+void StubHandleKeyStateFuzzTest(const uint8_t *data, size_t size)
+{
+    FuzzedDataProvider provider(data, size);
+
+    int32_t funcKey = provider.ConsumeIntegral<int32_t>();
+    bool enable = provider.ConsumeBool();
+
+    MMIService::GetInstance()->SetFunctionKeyState(funcKey, enable);
+}
+} // MMI
+} // OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    /* Run your code on data */
+    if (data == nullptr) {
+        return 0;
+    }
+
+    OHOS::MMI::StubHandleKeyStateFuzzTest(data, size);
+    return 0;
+}
