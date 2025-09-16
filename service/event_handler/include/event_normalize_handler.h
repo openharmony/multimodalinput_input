5bcd94eced4cc2788e0dae15635b6c467b1a8f15
wu-yang0329 <wuyang57@huawei.com>
2025-09-16 10:57:27 +0800

    修改超大函数
    Signed-off-by: wu-yang0329 <wuyang57@huawei.com>

--- service/event_handler/include/event_normalize_handler.h
+++ service/event_handler/include/event_normalize_handler.h
@@ -72,6 +72,8 @@ private:
         enum libinput_event_type type, libinput_event* event);
     void SwipeInwardProcess(std::shared_ptr<PointerEvent> pointerEvent,
         enum libinput_event_type type, libinput_event* event, int32_t* angleTolerance, int32_t lastDirection);
+    void SwipeInwardButtonJudge(std::shared_ptr<PointerEvent> pointerEvent);
+    void SwipeInwardSpeedJudge(std::shared_ptr<PointerEvent> pointerEvent);
 #ifdef OHOS_BUILD_ENABLE_KEYBOARD
     void UpdateKeyEventHandlerChain(const std::shared_ptr<KeyEvent> keyEvent);
 #endif // OHOS_BUILD_ENABLE_KEYBOARD
