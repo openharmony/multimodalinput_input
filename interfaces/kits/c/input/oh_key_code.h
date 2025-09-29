565631946f9694980e409e807b0a6e1bc3c17071
zhengyaozong <zhengyaozong@h-partners.com>
2025-09-29 16:59:19 +0800

    新增多模ndk接口
    Signed-off-by: zhengyaozong <zhengyaozong@h-partners.com>

--- interfaces/kits/c/input/oh_key_code.h
+++ interfaces/kits/c/input/oh_key_code.h
@@ -446,12 +446,12 @@ typedef enum {
      * @since 22
      */
     KEYCODE_RO = 2603,
-    /** 
+    /**
      * Katakana key
      * @since 22
      */
     KEYCODE_KATAKANA = 2604,
-    /** 
+    /**
      * Hiragana key
      * @since 22
      */
@@ -461,42 +461,42 @@ typedef enum {
      * @since 22
      */
     KEYCODE_HENKAN = 2606,
-    /** 
+    /**
      * Katakana/Hiragana key
      * @since 22
      */
     KEYCODE_KATAKANA_HIRAGANA = 2607,
-    /** 
+    /**
      * Muhenkan key
      * @since 22
      */
     KEYCODE_MUHENKAN = 2608,
-    /** 
+    /**
      * Linefeed key
      * @since 22
      */
     KEYCODE_LINEFEED = 2609,
-    /** 
+    /**
      * Macro key
      * @since 22
      */
     KEYCODE_MACRO = 2610,
-    /** 
+    /**
      * Plus/Minus key on the numeric keypad
      * @since 22
      */
     KEYCODE_NUMPAD_PLUSMINUS = 2611,
-    /** 
+    /**
      * Scale key
      * @since 22
      */
     KEYCODE_SCALE = 2612,
-    /** 
+    /**
      * Hanguel key
      * @since 22
      */
     KEYCODE_HANGUEL = 2613,
-    /** 
+    /**
      * Hanja key
      * @since 22
      */
@@ -506,507 +506,507 @@ typedef enum {
      * @since 22
      */
     KEYCODE_YEN = 2615,
-    /** 
+    /**
      * Stop key
      * @since 22
      */
     KEYCODE_STOP = 2616,
-    /** 
+    /**
      * Again key
      * @since 22
      */
     KEYCODE_AGAIN = 2617,
-    /** 
+    /**
      * Props key
      * @since 22
      */
     KEYCODE_PROPS = 2618,
-    /** 
+    /**
      * Undo key
      * @since 22
      */
     KEYCODE_UNDO = 2619,
-    /** 
+    /**
      * Copy key
      * @since 22
      */
     KEYCODE_COPY = 2620,
-    /** 
+    /**
      * Open key
      * @since 22
      */
     KEYCODE_OPEN = 2621,
-    /** 
+    /**
      * Paste key
      * @since 22
      */
     KEYCODE_PASTE = 2622,
-    /** 
+    /**
      * Find key
      * @since 22
      */
     KEYCODE_FIND = 2623,
-    /** 
+    /**
      * Cut key
      * @since 22
      */
     KEYCODE_CUT = 2624,
-    /** 
+    /**
      * Help key
      * @since 22
      */
     KEYCODE_HELP = 2625,
-    /** 
+    /**
      *  Calc key, which is used to start the calculator application
      * @since 22
      */
     KEYCODE_CALC = 2626,
-    /** 
+    /**
      * File key
      * @since 22
      */
     KEYCODE_FILE = 2627,
-    /** 
+    /**
      * Bookmarks key
      * @since 22
      */
     KEYCODE_BOOKMARKS = 2628,
-    /** 
+    /**
      * Next key
      * @since 22
      */
     KEYCODE_NEXT = 2629,
-    /** 
+    /**
      * Play/Pause key
      * @since 22
      */
     KEYCODE_PLAYPAUSE = 2630,
-    /** 
+    /**
      * Previous key
      * @since 22
      */
     KEYCODE_PREVIOUS = 2631,
-    /** 
+    /**
      * Stop CD key
      * @since 22
      */
     KEYCODE_STOPCD = 2632,
-    /** 
+    /**
      * Config key
      * @since 22
      */
     KEYCODE_CONFIG = 2634,
-    /** 
+    /**
      * Refresh key
      * @since 22
      */
     KEYCODE_REFRESH = 2635,
-    /** 
+    /**
      * Exit key
      * @since 22
      */
     KEYCODE_EXIT = 2636,
-    /** 
+    /**
      * Edit key
      * @since 22
      */
     KEYCODE_EDIT = 2637,
-    /** 
+    /**
      * Scroll Up key
      * @since 22
      */
     KEYCODE_SCROLLUP = 2638,
-    /** 
+    /**
      * Scroll Down key
      * @since 22
      */
     KEYCODE_SCROLLDOWN = 2639,
-    /** 
+    /**
      * New key
      * @since 22
      */
     KEYCODE_NEW = 2640,
-    /** 
+    /**
      * Redo key
      * @since 22
      */
     KEYCODE_REDO = 2641,
-    /** 
+    /**
      * Close key
      * @since 22
      */
     KEYCODE_CLOSE = 2642,
-    /** 
+    /**
      * Play key
      * @since 22
      */
     KEYCODE_PLAY = 2643,
-    /** 
+    /**
      *Bass Boost key
      * @since 22
      */
     KEYCODE_BASSBOOST = 2644,
-    /** 
+    /**
      * Print key
      * @since 22
      */
     KEYCODE_PRINT = 2645,
-    /** 
+    /**
      * Chat key
      * @since 22
      */
     KEYCODE_CHAT = 2646,
-    /** 
+    /**
      * Finance key
      * @since 22
      */
     KEYCODE_FINANCE = 2647,
-    /** 
+    /**
      * Cancel key
      * @since 22
      */
     KEYCODE_CANCEL = 2648,
-    /** 
+    /**
      * Keyboard Illumination Toggle key
      * @since 22
      */
     KEYCODE_KBDILLUM_TOGGLE = 2649,
-    /** 
+    /**
      * Keyboard Illumination Down key
      * @since 22
      */
     KEYCODE_KBDILLUM_DOWN = 2650,
-    /** 
+    /**
      * Keyboard Illumination Up key
      * @since 22
      */
     KEYCODE_KBDILLUM_UP = 2651,
-    /** 
+    /**
      * Send key
      * @since 22
      */
     KEYCODE_SEND = 2652,
-    /** 
+    /**
      * Reply key
      * @since 22
      */
     KEYCODE_REPLY = 2653,
-    /** 
+    /**
      * Forward Mail key
      * @since 22
      */
     KEYCODE_FORWARDMAIL = 2654,
-    /** 
+    /**
      * Save key
      * @since 22
      */
     KEYCODE_SAVE = 2655,
-    /** 
+    /**
      * Documents key
      * @since 22
      */
     KEYCODE_DOCUMENTS = 2656,
-    /** 
+    /**
      * Next Video key
      * @since 22
      */
     KEYCODE_VIDEO_NEXT = 2657,
-    /** 
+    /**
      * Previous Video key
      * @since 22
      */
     KEYCODE_VIDEO_PREV = 2658,
-    /** 
+    /**
      * Brightness Cycle key
      * @since 22
      */
     KEYCODE_BRIGHTNESS_CYCLE = 2659,
-    /** 
+    /**
      * Brightness Zero key
      * @since 22
      */
     KEYCODE_BRIGHTNESS_ZERO = 2660,
-    /** 
+    /**
      * Display Off key
      * @since 22
      */
     KEYCODE_DISPLAY_OFF = 2661,
-    /** 
+    /**
      * Misc Button key
      * @since 22
      */
     KEYCODE_BTN_MISC = 2662,
-    /** 
+    /**
      * Goto key
      * @since 22
      */
     KEYCODE_GOTO = 2663,
-    /** 
+    /**
      * Info key
      * @since 22
      */
     KEYCODE_INFO = 2664,
-    /** 
+    /**
      * Program key
      * @since 22
      */
     KEYCODE_PROGRAM = 2665,
-    /** 
+    /**
      * PVR key
      * @since 22
      */
     KEYCODE_PVR = 2666,
-    /** 
+    /**
      * Subtitle key
      * @since 22
      */
     KEYCODE_SUBTITLE = 2667,
-    /** 
+    /**
      * Full Screen key
      * @since 22
      */
     KEYCODE_FULL_SCREEN = 2668,
-    /** 
+    /**
      * Keyboard
      * @since 22
      */
     KEYCODE_KEYBOARD = 2669,
-    /** 
+    /**
      * Aspect Ratio key
      * @since 22
      */
     KEYCODE_ASPECT_RATIO = 2670,
-    /** 
+    /**
      *  Port Control key
      * @since 22
      */
     KEYCODE_PC = 2671,
-    /** 
+    /**
      * TV key
      * @since 22
      */
     KEYCODE_TV = 2672,
-    /** 
+    /**
      * TV key 2
      * @since 22
      */
     KEYCODE_TV2 = 2673,
-    /** 
+    /**
      * VCR key
      * @since 22
      */
     KEYCODE_VCR = 2674,
-    /** 
+    /**
      * VCR key 2
      * @since 22
      */
     KEYCODE_VCR2 = 2675,
-    /** 
+    /**
      * SAT key
      * @since 22
      */
     KEYCODE_SAT = 2676,
-    /** 
+    /**
      * CD key
      * @since 22
      */
     KEYCODE_CD = 2677,
-    /** 
+    /**
      * Tape key
      * @since 22
      */
     KEYCODE_TAPE = 2678,
-    /** 
+    /**
      * Tuner key
      * @since 22
      */
     KEYCODE_TUNER = 2679,
-    /** 
+    /**
      * Player key
      * @since 22
      */
     KEYCODE_PLAYER = 2680,
-    /** 
+    /**
      * DVD key
      * @since 22
      */
     KEYCODE_DVD = 2681,
-    /** 
+    /**
      * Audio key
      * @since 22
      */
     KEYCODE_AUDIO = 2682,
-    /** 
+    /**
      * Video key
      * @since 22
      */
     KEYCODE_VIDEO = 2683,
-    /** 
+    /**
      * Memo key
      * @since 22
      */
     KEYCODE_MEMO = 2684,
-    /** 
+    /**
      * Calendar key
      * @since 22
      */
     KEYCODE_CALENDAR = 2685,
-    /** 
+    /**
      * Red indicator
      * @since 22
      */
     KEYCODE_RED = 2686,
-    /** 
+    /**
      * Green indicator
      * @since 22
      */
     KEYCODE_GREEN = 2687,
-    /** 
+    /**
      * Yellow indicator
      * @since 22
      */
     KEYCODE_YELLOW = 2688,
-    /** 
+    /**
      * Blue indicator
      * @since 22
      */
     KEYCODE_BLUE = 2689,
-    /** 
+    /**
      * Channel Up key
      * @since 22
      */
     KEYCODE_CHANNELUP = 2690,
-    /** 
+    /**
      * Channel Down key
      * @since 22
      */
     KEYCODE_CHANNELDOWN = 2691,
-    /** 
+    /**
      * Last key
      * @since 22
      */
     KEYCODE_LAST = 2692,
-    /** 
+    /**
      * Restart key
      * @since 22
      */
     KEYCODE_RESTART = 2693,
-    /** 
+    /**
      * Slow key
      * @since 22
      */
     KEYCODE_SLOW = 2694,
-    /** 
+    /**
      * Shuffle key
      * @since 22
      */
     KEYCODE_SHUFFLE = 2695,
-    /** 
+    /**
      * Videophone key
      * @since 22
      */
     KEYCODE_VIDEOPHONE = 2696,
-    /** 
+    /**
      * Games key
      * @since 22
      */
     KEYCODE_GAMES = 2697,
-    /** 
+    /**
      * Zoom in
      * @since 22
      */
     KEYCODE_ZOOMIN = 2698,
-    /** 
+    /**
      * Zoom out
      * @since 22
      */
     KEYCODE_ZOOMOUT = 2699,
-    /** 
+    /**
      * Zoom reset key
      * @since 22
      */
     KEYCODE_ZOOMRESET = 2700,
-    /** 
+    /**
      * Word Processor key
      * @since 22
      */
     KEYCODE_WORDPROCESSOR = 2701,
-    /** 
+    /**
      * Editor key
      * @since 22
      */
     KEYCODE_EDITOR = 2702,
-    /** 
+    /**
      * Spreadsheet key
      * @since 22
      */
     KEYCODE_SPREADSHEET = 2703,
-    /** 
+    /**
      * Graphics Editor key
      * @since 22
      */
     KEYCODE_GRAPHICSEDITOR = 2704,
-    /** 
+    /**
      * Presentation key
      * @since 22
      */
     KEYCODE_PRESENTATION = 2705,
-    /** 
+    /**
      * Database key
      * @since 22
      */
     KEYCODE_DATABASE = 2706,
-    /** 
+    /**
      * News key
      * @since 22
      */
     KEYCODE_NEWS = 2707,
-    /** 
+    /**
      * Voicemail key
      * @since 22
      */
     KEYCODE_VOICEMAIL = 2708,
-    /** 
+    /**
      * Address book key
      * @since 22
      */
     KEYCODE_ADDRESSBOOK = 2709,
-    /** 
+    /**
      * Messenger key
      * @since 22
      */
     KEYCODE_MESSENGER = 2710,
-    /** 
+    /**
      * Brightness Toggle key
      * @since 22
      */
     KEYCODE_BRIGHTNESS_TOGGLE = 2711,
-    /** 
+    /**
      * Spell Check key
      * @since 22
      */
     KEYCODE_SPELLCHECK = 2712,
-    /** 
+    /**
      * Coffee key, which is used to launch screen lock or screen saver
      * @since 22
      */
     KEYCODE_COFFEE = 2713,
-    /** 
+    /**
      * Media Repeat key
      * @since 22
      */
     KEYCODE_MEDIA_REPEAT = 2714,
-    /** 
+    /**
      * Images key
      * @since 22
      */
     KEYCODE_IMAGES = 2715,
-    /** 
+    /**
      * Button Configuration key
      * @since 22
      */
     KEYCODE_BUTTONCONFIG = 2716,
-    /** 
+    /**
      * Task Manager key
      * @since 22
      */
@@ -1016,262 +1016,262 @@ typedef enum {
      * @since 22
      */
     KEYCODE_JOURNAL = 2718,
-    /** 
+    /**
      * Control Panel key
      * @since 22
      */
     KEYCODE_CONTROLPANEL = 2719,
-    /** 
+    /**
      * App Select key
      * @since 22
      */
     KEYCODE_APPSELECT = 2720,
-    /** 
+    /**
      * Screen Saver key
      * @since 22
      */
     KEYCODE_SCREENSAVER = 2721,
-    /** 
+    /**
      * Smart key
      * @since 22
      */
     KEYCODE_ASSISTANT = 2722,
-    /** 
+    /**
      * Next Keyboard Layout key
      * @since 22
      */
     KEYCODE_KBD_LAYOUT_NEXT = 2723,
-    /** 
+    /**
      * Min Brightness key
      * @since 22
      */
     KEYCODE_BRIGHTNESS_MIN = 2724,
-    /** 
+    /**
      * Max Brightness key
      * @since 22
      */
     KEYCODE_BRIGHTNESS_MAX = 2725,
-    /** 
+    /**
      * Assist_Previous key, used to view historical inputs
      * @since 22
      */
     KEYCODE_KBDINPUTASSIST_PREV = 2726,
-    /** 
+    /**
      * Assist_Next key, used to view predictive inputs
      * @since 22
      */
     KEYCODE_KBDINPUTASSIST_NEXT = 2727,
-    /** 
+    /**
      * Assist_Previous_Group key, used to switch to the previous input method in the input group
      * @since 22
      */
     KEYCODE_KBDINPUTASSIST_PREVGROUP = 2728,
-    /** 
+    /**
      *  Assist_Next_Group key, used to switch to the next input method in the input group
      * @since 22
      */
     KEYCODE_KBDINPUTASSIST_NEXTGROUP = 2729,
-    /** 
+    /**
      * Keyboard Input-assisted Accept key
      * @since 22
      */
     KEYCODE_KBDINPUTASSIST_ACCEPT = 2730,
-    /** 
+    /**
      * Keyboard Input-assisted Cancel key
      * @since 22
      */
     KEYCODE_KBDINPUTASSIST_CANCEL = 2731,
-    /** 
+    /**
      * Front key, which is used to launch the windshield defogger
      * @since 22
      */
     KEYCODE_FRONT = 2800,
-    /** 
+    /**
      * Setup key
      * @since 22
      */
     KEYCODE_SETUP = 2801,
-    /** 
+    /**
      * Wakeup key
      * @since 22
      */
     KEYCODE_WAKEUP = 2802,
-    /** 
+    /**
      * Send File key
      * @since 22
      */
     KEYCODE_SENDFILE = 2803,
-    /** 
+    /**
      * Delete File key
      * @since 22
      */
     KEYCODE_DELETEFILE = 2804,
-    /** 
+    /**
      * XFER key, which is used to start file transfer
      * @since 22
      */
     KEYCODE_XFER = 2805,
-    /** 
+    /**
      * Program key 1
      * @since 22
      */
     KEYCODE_PROG1 = 2806,
-    /** 
+    /**
      * Program key 2
      * @since 22
      */
     KEYCODE_PROG2 = 2807,
-    /** 
+    /**
      * MS-DOS key
      * @since 22
      */
     KEYCODE_MSDOS = 2808,
-    /** 
+    /**
      * Screen Lock key
      * @since 22
      */
     KEYCODE_SCREENLOCK = 2809,
-    /** 
+    /**
      * Directional Rotation Display key
      * @since 22
      */
     KEYCODE_DIRECTION_ROTATE_DISPLAY = 2810,
-    /** 
+    /**
      * Windows Cycle key
      * @since 22
      */
     KEYCODE_CYCLEWINDOWS = 2811,
-    /** 
+    /**
      * Key
      * @since 22
      */
     KEYCODE_COMPUTER = 2812,
-    /** 
+    /**
      * Eject CD key
      * @since 22
      */
     KEYCODE_EJECTCLOSECD = 2813,
-    /** 
+    /**
      * ISO key
      * @since 22
      */
     KEYCODE_ISO = 2814,
-    /** 
+    /**
      * Move key
      * @since 22
      */
     KEYCODE_MOVE = 2815,
-    /** 
+    /**
      * F13 key
      * @since 22
      */
     KEYCODE_F13 = 2816,
-    /** 
+    /**
      * F14 key
      * @since 22
      */
     KEYCODE_F14 = 2817,
-    /** 
+    /**
      * F15 key
      * @since 22
      */
     KEYCODE_F15 = 2818,
-    /** 
+    /**
      * F16 key
      * @since 22
      */
     KEYCODE_F16 = 2819,
-    /** 
+    /**
      * F17 key
      * @since 22
      */
     KEYCODE_F17 = 2820,
-    /** 
+    /**
      * F18 key
      * @since 22
      */
     KEYCODE_F18 = 2821,
-    /** 
+    /**
      * F19 key
      * @since 22
      */
     KEYCODE_F19 = 2822,
-    /** 
+    /**
      * F20 key
      * @since 22
      */
     KEYCODE_F20 = 2823,
-    /** 
+    /**
      * F21 key
      * @since 22
      */
     KEYCODE_F21 = 2824,
-    /** 
+    /**
      * F22 key
      * @since 22
      */
     KEYCODE_F22 = 2825,
-    /** 
+    /**
      * F23 key
      * @since 22
      */
     KEYCODE_F23 = 2826,
-    /** 
+    /**
      * F24 key
      * @since 22
      */
     KEYCODE_F24 = 2827,
-    /** 
+    /**
      * Program key 3
      * @since 22
      */
     KEYCODE_PROG3 = 2828,
-    /** 
+    /**
      * Program key 4
      * @since 22
      */
     KEYCODE_PROG4 = 2829,
-    /** 
+    /**
      * Dashboard key
      * @since 22
      */
     KEYCODE_DASHBOARD = 2830,
-    /** 
+    /**
      * Suspend key
      * @since 22
      */
     KEYCODE_SUSPEND = 2831,
-    /** 
+    /**
      * HP key
      * @since 22
      */
     KEYCODE_HP = 2832,
-    /** 
+    /**
      * Sound key
      * @since 22
      */
     KEYCODE_SOUND = 2833,
-    /** 
+    /**
      * Question key
      * @since 22
      */
     KEYCODE_QUESTION = 2834,
-    /** 
+    /**
      * Connect key
      * @since 22
      */
     KEYCODE_CONNECT = 2836,
-    /** 
+    /**
      * Sport key
      * @since 22
      */
     KEYCODE_SPORT = 2837,
-    /** 
+    /**
      * Shop key
      * @since 22
      */
     KEYCODE_SHOP = 2838,
-    /** 
+    /**
      * Alterase key
      * @since 22
      */
@@ -1281,87 +1281,87 @@ typedef enum {
      * @since 22
      */
     KEYCODE_SWITCHVIDEOMODE = 2841,
-    /** 
+    /**
      * Battery key
      * @since 22
      */
     KEYCODE_BATTERY = 2842,
-    /** 
+    /**
      * Bluetooth key
      * @since 22
      */
     KEYCODE_BLUETOOTH = 2843,
-    /** 
+    /**
      * WLAN key
      * @since 22
      */
     KEYCODE_WLAN = 2844,
-    /** 
+    /**
      * Ultra-wideband key
      * @since 22
      */
     KEYCODE_UWB = 2845,
-    /** 
+    /**
      * WWAN WiMAX key
      * @since 22
      */
     KEYCODE_WWAN_WIMAX = 2846,
-    /** 
+    /**
      * RF Kill key
      * @since 22
      */
     KEYCODE_RFKILL = 2847,
-    /** 
+    /**
      * Channel key
      * @since 22
      */
     KEYCODE_CHANNEL = 3001,
-    /** 
+    /**
      *Button 0
      * @since 22
      */
     KEYCODE_BTN_0 = 3100,
-    /** 
+    /**
      * Button 1
      * @since 22
      */
     KEYCODE_BTN_1 = 3101,
-    /** 
+    /**
      * Button 2
      * @since 22
      */
     KEYCODE_BTN_2 = 3102,
-    /** 
+    /**
      * Button 3
      * @since 22
      */
     KEYCODE_BTN_3 = 3103,
-    /** 
+    /**
      * Button 4
      * @since 22
      */
     KEYCODE_BTN_4 = 3104,
-    /** 
+    /**
      * Button 5
      * @since 22
      */
     KEYCODE_BTN_5 = 3105,
-    /** 
+    /**
      * Button 6
      * @since 22
      */
     KEYCODE_BTN_6 = 3106,
-    /** 
+    /**
      * Button 7
      * @since 22
      */
     KEYCODE_BTN_7 = 3107,
-    /** 
+    /**
      * Button 8
      * @since 22
      */
     KEYCODE_BTN_8 = 3108,
-    /** 
+    /**
      * Button 9
      * @since 22
      */
