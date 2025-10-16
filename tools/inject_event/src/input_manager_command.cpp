22da0b23f211e204675355823b253e393f7cdc26
wanghao1717 <wanghao505@huawei.com>
2025-10-16 16:19:56 +0800

    add test
    Signed-off-by: wanghao1717 <wanghao505@huawei.com>

--- tools/inject_event/src/input_manager_command.cpp
+++ tools/inject_event/src/input_manager_command.cpp
@@ -1804,10 +1804,6 @@ int32_t InputManagerCommand::QueryMouseInfo(int32_t argc, char *argv[])
         std::cerr << "Mouse is invisible" << std::endl;
         return RET_ERR;
     }
-    if (g_pointerStyle.id != THIRD_PARTY_CURSOR_ID) {
-        std::cerr << "Mouse is invisible" << std::endl;
-        return RET_ERR;
-    }
     if (argc - optind <= 0) {
         return RET_OK;
     }
