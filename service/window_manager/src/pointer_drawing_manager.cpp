ed87918c4bc82a5377e06409898b84831701f6a0
chenchong_666 <chenchong57@h-partners.com>
2025-10-16 19:54:32 +0800

    fix:add mouseDisplayState
    Signed-off-by: chenchong_666 <chenchong57@h-partners.com>

--- service/window_manager/src/pointer_drawing_manager.cpp
+++ service/window_manager/src/pointer_drawing_manager.cpp
@@ -1512,6 +1512,7 @@ void PointerDrawingManager::SetMouseDisplayState(bool state)
         MMI_HILOGI("The state:%{public}s", state ? "true" : "false");
         UpdatePointerVisible();
     }
+    POINTER_DEV_MGR.mouseDisplayState = mouseDisplayState_;
 }
 
 bool PointerDrawingManager::GetMouseDisplayState() const
