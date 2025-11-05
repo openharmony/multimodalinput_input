898ff122411d00775c4d8a0883c3db15d1559713
xueyuanzhao <zhaoxueyuan@huawei.com>
2025-11-05 11:25:18 +0800

    Signed-off-by:xueyuanzhao<zhaoxueyuan@huawei.com>
    Signed-off-by: xueyuanzhao <zhaoxueyuan@huawei.com>
    
    
    revert merge request: 检视意见修改&插拔触控屏适配
    
    7ebdac49fb2e44cf7ccb3c125258c491142e7c1f
    
    This revert merge request !8553

--- service/window_manager/test/input_windows_manager_test.cpp
+++ service/window_manager/test/input_windows_manager_test.cpp
@@ -649,6 +649,31 @@ HWTEST_F(InputWindowsManagerTest, InputWindowsManagerTest_UpdateDisplayIdAndName
 }
 
 /**
+ * @tc.name: InputWindowsManagerTest_GetAllUsersDisplays_001
+ * @tc.desc: Test GetAllUsersDisplays
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(InputWindowsManagerTest, InputWindowsManagerTest_GetAllUsersDisplays_001, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    OHOS::MMI::WindowInfo win1 = {.id = NUM_1, .pid = NUM_100};
+    OHOS::MMI::WindowInfo win2 = {.id = NUM_2, .pid = NUM_200};
+    std::vector<OHOS::MMI::WindowInfo> windowsInfo1 = { win1, win2 };
+    std::vector<OHOS::MMI::WindowInfo> windowsInfo2 = { win2 };
+    OLD::DisplayGroupInfo group1 = InputWindowsManagerTest::CreateDisplayGroupInfo(NUM_100, NUM_100, NUM_100 + NUM_1);
+    group1.windowsInfo = windowsInfo1;
+    OLD::DisplayGroupInfo group2 = InputWindowsManagerTest::CreateDisplayGroupInfo(NUM_200, NUM_200);
+    group2.windowsInfo = windowsInfo2;
+
+    WIN_MGR->UpdateDisplayInfo(group1);
+    WIN_MGR->UpdateDisplayInfo(group2);
+
+    std::vector<OLD::DisplayInfo> displays = WIN_MGR->GetAllUsersDisplays();
+    EXPECT_EQ(displays.size(), NUM_1 + NUM_2);
+}
+
+/**
  * @tc.name: InputWindowsManagerTest_GetDisplayBindInfo_001
  * @tc.desc: Test getting display binding information
  * @tc.type: FUNC
@@ -709,8 +734,7 @@ HWTEST_F(InputWindowsManagerTest, InputWindowsManagerTest_UpdateWindowsInfoPerDi
     CALL_TEST_DEBUG;
     OLD::DisplayGroupInfo displayGroupInfo;
     displayGroupInfo.focusWindowId = 2;
-    std::vector<int32_t> deleteGroups;
-    WIN_MGR->UpdateWindowsInfoPerDisplay(displayGroupInfo, deleteGroups);
+    WIN_MGR->UpdateWindowsInfoPerDisplay(displayGroupInfo);
     WindowInfo window1 {1};
     WindowInfo window2 {2};
     displayGroupInfo.windowsInfo.push_back(window1);
@@ -7437,12 +7461,11 @@ HWTEST_F(InputWindowsManagerTest, InputWindowsManagerTest_UpdateWindowsInfoPerDi
     displayGroupInfo.focusWindowId = 300;
     displayGroupInfo.windowsInfo.push_back(winInfo);
     inputWindowsManager.windowsPerDisplay_.insert(std::make_pair(displayId, winGroupInfo));
-    std::vector<int32_t> deleteGroups;
-    EXPECT_NO_FATAL_FAILURE(inputWindowsManager.UpdateWindowsInfoPerDisplay(displayGroupInfo, deleteGroups));
+    EXPECT_NO_FATAL_FAILURE(inputWindowsManager.UpdateWindowsInfoPerDisplay(displayGroupInfo));
 
     winInfo.windowType = static_cast<int32_t>(Rosen::WindowType::WINDOW_TYPE_TRANSPARENT_VIEW);
     displayGroupInfo.windowsInfo.push_back(winInfo);
-    EXPECT_NO_FATAL_FAILURE(inputWindowsManager.UpdateWindowsInfoPerDisplay(displayGroupInfo, deleteGroups));
+    EXPECT_NO_FATAL_FAILURE(inputWindowsManager.UpdateWindowsInfoPerDisplay(displayGroupInfo));
 }
 
 /**
