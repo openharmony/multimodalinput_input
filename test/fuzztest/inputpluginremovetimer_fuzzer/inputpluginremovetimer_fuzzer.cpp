8b862f9aa653041ffb507d6c382e0b5032487dfe
hanyi1150 <hanyi1150@126.com>
2025-11-13 02:36:51 +0000

    [test]:Add fuzz for InputPlugin::RemoveTimer
    Signed-off-by: hanyi1150 <hanyi1150@126.com>

---
+++ test/fuzztest/inputpluginremovetimer_fuzzer/inputpluginremovetimer_fuzzer.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "fuzzer/FuzzedDataProvider.h"
+#include "inputpluginremovetimer_fuzzer.h"
+#include "multimodal_input_plugin_manager.h"
+#include "securec.h"
+
+#undef MMI_LOG_TAG
+#define MMI_LOG_TAG "InputPluginRemoveTimerFuzzTest"
+
+namespace OHOS {
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    /* Run your code on data */
+    if (data == nullptr) {
+        return 0;
+    }
+
+    FuzzedDataProvider provider(data, size);
+    int32_t id = provider.ConsumeIntegral<int32_t>();
+    InputPlugin plugin;
+    plugin.RemoveTimer(id);
+
+    return 0;
+}
+} // namespace MMI
