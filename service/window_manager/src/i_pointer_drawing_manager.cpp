b24ec0dfac51f762f30e5b8b2a6dd549f0ac3b2d
RZH-150 <renzhonghao3@h-partners.com>
2025-12-15 15:55:42 +0800

    调整卸载光标so的逻辑，调整屏幕模式监听的逻辑
    Signed-off-by: RZH-150 <renzhonghao3@h-partners.com>

--- service/window_manager/src/i_pointer_drawing_manager.cpp
+++ service/window_manager/src/i_pointer_drawing_manager.cpp
@@ -13,195 +13,49 @@
  * limitations under the License.
  */
 
-#ifndef I_POINTER_DRAWING_MANAGER_H
-#define I_POINTER_DRAWING_MANAGER_H
+#include "i_pointer_drawing_manager.h"
 
-#include "pointer_style.h"
-#include "old_display_info.h"
-#include "delegate_interface.h"
+#include <common/rs_common_def.h>
+
+#include "define_multimodal.h"
+
+#undef MMI_LOG_DOMAIN
+#define MMI_LOG_DOMAIN MMI_LOG_CURSOR
 
 namespace OHOS {
 namespace MMI {
-class IPointerDrawingManager {
+#ifdef OHOS_BUILD_EMULATOR
+class EmulatorPointerDrawingManager final : public IPointerDrawingManager {
 public:
-    IPointerDrawingManager() = default;
-    virtual ~IPointerDrawingManager() = default;
-
-    static IPointerDrawingManager* GetInstance();
-    virtual void DrawPointer(uint64_t rsId, int32_t physicalX, int32_t physicalY,
-        const PointerStyle pointerStyle, Direction direction) {}
-    virtual void UpdateDisplayInfo(const OLD::DisplayInfo& displayInfo) {}
-    virtual void UpdateBindDisplayId(uint64_t rsId) {}
-    virtual void OnDisplayInfo(const OLD::DisplayGroupInfo& displayGroupInfo) {}
-    virtual void OnWindowInfo(const WinInfo &info) {}
-    virtual bool Init()
+    void SetMouseDisplayState(bool state) override
     {
-        return true;
-    }
-    virtual void DeletePointerVisible(int32_t pid) {}
-    virtual int32_t SetPointerVisible(int32_t pid, bool visible, int32_t priority, bool isHap)
-    {
-        return 0;
-    }
-    virtual bool GetPointerVisible(int32_t pid)
-    {
-        return true;
-    }
-    virtual int32_t SetPointerColor(int32_t color)
-    {
-        return 0;
-    }
-    virtual int32_t GetPointerColor()
-    {
-        return 0;
-    }
-    virtual int32_t SetPointerStyle(int32_t pid, int32_t windowId, PointerStyle pointerStyle,
-        bool isUiExtension = false)
-    {
-        return 0;
-    }
-    virtual int32_t ClearWindowPointerStyle(int32_t pid, int32_t windowId)
-    {
-        return 0;
-    }
-    virtual int32_t GetPointerStyle(int32_t pid, int32_t windowId, PointerStyle &pointerStyle,
-        bool isUiExtension = false)
-    {
-        return 0;
-    }
-    virtual void DrawPointerStyle(const PointerStyle& pointerStyle) {}
-    virtual bool IsPointerVisible()
-    {
-        return false;
-    }
-    virtual void SetPointerLocation(int32_t x, int32_t y, uint64_t rsId) {}
-    virtual void SetMouseDisplayState(bool state) {}
-    virtual bool GetMouseDisplayState() const
-    {
-        return false;
-    }
-    virtual int32_t SetCustomCursor(CursorPixelMap curPixelMap,
-        int32_t pid, int32_t windowId, int32_t focusX, int32_t focusY)
-    {
-        return 0;
-    }
-    virtual int32_t SetCustomCursor(int32_t pid, int32_t windowId, CustomCursor cursor, CursorOptions options)
-    {
-        return 0;
-    }
-    virtual int32_t SetMouseIcon(int32_t pid, int32_t windowId, CursorPixelMap curPixelMap)
-    {
-        return 0;
-    }
-    virtual int32_t SetMouseHotSpot(int32_t pid, int32_t windowId, int32_t hotSpotX, int32_t hotSpotY)
-    {
-        return 0;
-    }
-    virtual int32_t SetPointerSize(int32_t size)
-    {
-        return 0;
-    }
-    virtual int32_t GetPointerSize()
-    {
-        return 0;
-    }
-    virtual void GetPointerImageSize(int32_t &width, int32_t &height)
-    {
-        return;
+        mouseDisplayState_ = state;
     }
 
-    virtual int32_t GetCursorSurfaceId(uint64_t &surfaceId);
+    bool GetMouseDisplayState() const override
+    {
+        return mouseDisplayState_;
+    }
 
-    virtual PointerStyle GetLastMouseStyle()
-    {
-        return {};
-    }
-    virtual IconStyle GetIconStyle(const MOUSE_ICON mouseStyle)
-    {
-        return {};
-    }
-    virtual const std::map<MOUSE_ICON, IconStyle>& GetMouseIconPath()
-    {
-        static std::map<MOUSE_ICON, IconStyle> emptyMap;
-        return emptyMap;
-    }
-    virtual int32_t SwitchPointerStyle()
-    {
-        return 0;
-    }
-    virtual void DrawMovePointer(uint64_t rsId, int32_t physicalX, int32_t physicalY) {}
-    virtual void Dump(int32_t fd, const std::vector<std::string> &args) {}
-    virtual void InitPointerCallback() {}
-    virtual void InitScreenInfo() {}
-    virtual int32_t EnableHardwareCursorStats(int32_t pid, bool enable)
-    {
-        return 0;
-    }
-    virtual int32_t GetHardwareCursorStats(int32_t pid, uint32_t &frameCount, uint32_t &vsyncCount)
-    {
-        return 0;
-    }
-    virtual OLD::DisplayInfo GetCurrentDisplayInfo()
-    {
-        return {};
-    }
-#ifdef OHOS_BUILD_ENABLE_MAGICCURSOR
-    virtual int32_t GetPointerSnapshot(void *pixelMapPtr)
-    {
-        return 0;
-    }
-#endif // OHOS_BUILD_ENABLE_MAGICCURSOR
-    virtual void ForceClearPointerVisibleStatus() {}
-    virtual void InitPointerObserver() {}
-    virtual void OnSessionLost(int32_t pid) {}
-    virtual int32_t SkipPointerLayer(bool isSkip)
-    {
-        return 0;
-    }
-    virtual void SetDelegateProxy(std::shared_ptr<DelegateInterface> proxy) {}
-    virtual std::shared_ptr<DelegateInterface> GetDelegateProxy()
-    {
-        return nullptr;
-    }
-    virtual void DestroyPointerWindow() {}
-    virtual void DrawScreenCenterPointer(const PointerStyle &pointerStyle) {}
-    virtual void SubscribeScreenModeChange() {}
-    virtual void UnSubscribeScreenModeChange() {}
-    virtual void RegisterDisplayStatusReceiver() {}
-    virtual void InitDefaultMouseIconPath() {}
-    virtual int32_t UpdateMouseLayer(const PointerStyle& pointerStyle,
-        int32_t physicalX, int32_t physicalY)
-    {
-        return 0;
-    }
-    virtual int32_t DrawNewDpiPointer()
-    {
-        return 0;
-    }
-    virtual void AttachAllSurfaceNode() {}
-    virtual void DetachAllSurfaceNode() {}
-    virtual int32_t CheckHwcReady()
-    {
-        return 0;
-    }
-    virtual bool GetHardCursorEnabled()
-    {
-        return false;
-    }
-#ifndef OHOS_BUILD_ENABLE_WATCH
-    virtual void NotifyPointerEventToRS(int32_t pointAction, int32_t pointCnt, int32_t sourceType) {}
-#endif // OHOS_BUILD_ENABLE_WATCH
-    virtual int32_t GetCurrentCursorInfo(bool& visible, PointerStyle& pointerStyle)
-    {
-        return 0;
-    }
-    virtual int32_t GetUserDefinedCursorPixelMap(void *pixelMapPtr)
-    {
-        return 0;
-    }
-    virtual void UpdatePointerItemCursorInfo(PointerEvent::PointerItem& pointerItem) {}
-    virtual void ClearResources() {};
+private:
+    bool mouseDisplayState_ {false};
 };
+#endif
+
+IPointerDrawingManager* IPointerDrawingManager::GetInstance()
+{
+#ifndef OHOS_BUILD_EMULATOR
+    static IPointerDrawingManager iPointDrawMgr_;
+#else
+    static EmulatorPointerDrawingManager iPointDrawMgr_;
+#endif
+    return &iPointDrawMgr_;
+}
+
+int32_t IPointerDrawingManager::GetCursorSurfaceId(uint64_t &surfaceId)
+{
+    surfaceId = Rosen::INVALID_NODEID;
+    return RET_OK;
+}
 } // namespace MMI
 } // namespace OHOS
-#endif // I_POINTER_DRAWING_MANAGER_H
