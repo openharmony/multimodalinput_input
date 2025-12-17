90991b12c41645be7c7db2c8854f4ecec04c3f4b
weixin_66524478 <zhengsong5@huawei.com>
2025-12-17 17:01:15 +0800

    代码格式修改
    Signed-off-by: weixin_66524478 <zhengsong5@huawei.com>

--- service/subscriber/src/switch_subscriber_handler.cpp
+++ service/subscriber/src/switch_subscriber_handler.cpp
@@ -179,31 +179,33 @@ void SwitchSubscriberHandler::HandleSwitchEvent(const std::shared_ptr<SwitchEven
         switchEventType_ = switchEvent->GetSwitchType();
     }
     if (switchEvent->GetSwitchType() == SwitchEvent::SwitchType::SWITCH_TABLET) {
-        ffrt::submit([this, switchEvent] {
-            std::unique_lock<std::mutex> lock(mtx_);
-            bool status = cv_.wait_for(lock, std::chrono::seconds(MAX_WAIT_TIME), [this] {
-                return isCesReady_.load();
-            });
-            if (status) {
-                this->PublishTabletEvent(switchEvent);
-            } else {
-                MMI_HILOGI("wait COMMON_EVENT_SERVICE timeout.");
-            }
-        },
-        ffrt::task_attr().name("publish_tablet_mode_common_event"));
+        ffrt::submit([this, switchEvent]
+            {
+                std::unique_lock<std::mutex> lock(mtx_);
+                bool status = cv_.wait_for(lock, std::chrono::seconds(MAX_WAIT_TIME), [this] {
+                    return isCesReady_.load();
+                });
+                if (status) {
+                    this->PublishTabletEvent(switchEvent);
+                } else {
+                    MMI_HILOGI("wait COMMON_EVENT_SERVICE timeout.");
+                }
+            },
+            ffrt::task_attr().name("publish_tablet_mode_common_event"));
     } else if (switchEvent->GetSwitchType() == SwitchEvent::SwitchType::SWITCH_LID) {
-        ffrt::submit([this, switchEvent] {
-            std::unique_lock<std::mutex> lock(mtx_);
-            bool status = cv_.wait_for(lock, std::chrono::seconds(MAX_WAIT_TIME), [this] {
-                return isCesReady_.load();
-            });
-            if (status) {
-                this->PublishLidEvent(switchEvent);
-            } else {
-                MMI_HILOGI("wait COMMON_EVENT_SERVICE timeout.");
-            }
-        },
-        ffrt::task_attr().name("publish_lid_state_common_event"));
+        ffrt::submit([this, switchEvent]
+            {
+                std::unique_lock<std::mutex> lock(mtx_);
+                bool status = cv_.wait_for(lock, std::chrono::seconds(MAX_WAIT_TIME), [this] {
+                    return isCesReady_.load();
+                });
+                if (status) {
+                    this->PublishLidEvent(switchEvent);
+                } else {
+                    MMI_HILOGI("wait COMMON_EVENT_SERVICE timeout.");
+                }
+            },
+            ffrt::task_attr().name("publish_lid_state_common_event"));
     }
     if (OnSubscribeSwitchEvent(switchEvent)) {
         MMI_HILOGI("Subscribe switchEvent filter success. switchValue:%{public}d", switchEvent->GetSwitchValue());
