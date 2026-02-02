4722b24d9e7d940bf255ad50883637559f82a597
huxianghao <huxianghao@h-partners.com>
2026-02-02 17:36:00 +0800

    [test]:Fix UT
    Signed-off-by: huxianghao <huxianghao@h-partners.com>

--- service/mouse_event_normalize/test/src/mouse_event_normalize_test_with_mock.cpp
+++ service/mouse_event_normalize/test/src/mouse_event_normalize_test_with_mock.cpp
@@ -17,7 +17,11 @@
 
 #include "define_multimodal.h"
 #include "device_observer.h"
+#include "i_input_device_manager.h"
+#include "libinput_interface.h"
 #include "input_device_manager.h"
+#include "input_service_context.h"
+#include "mouse_event_interface.h"
 #include "mouse_event_normalize.h"
 #include "preferences_manager_mock.h"
 
@@ -26,9 +30,7 @@
 
 namespace OHOS {
 namespace MMI {
-namespace {
-constexpr int32_t DEFAULT_SPEED { 10 };
-}
+
 using namespace testing;
 using namespace testing::ext;
 
@@ -38,13 +40,10 @@ public:
     void TearDown();
     static void SetUpTestCase() {}
     static void TearDownTestCase() {}
+    InputServiceContext env_ {};
 };
 
-void MouseEventNormalizeTestWithMock::TearDown()
-{
-    InputDeviceManagerMock::ReleaseInstance();
-    PreferencesManagerMock::ReleaseInstance();
-}
+void MouseEventNormalizeTestWithMock::TearDown() {}
 
 class InputDeviceObserver : public IDeviceObserver {
 public:
@@ -61,78 +60,12 @@ public:
  */
 HWTEST_F(MouseEventNormalizeTestWithMock, MouseEventNormalize_001, TestSize.Level1)
 {
-    EXPECT_CALL(*INPUT_DEV_MGR, Attach).Times(testing::Exactly(1));
-    EXPECT_CALL(*INPUT_DEV_MGR, Detach).Times(testing::Exactly(1));
-    MouseEventNormalize norm;
-    EXPECT_NE(norm.inputDevObserver_, nullptr);
-}
-
-/**
- * @tc.name: SetUpDeviceObserver_001
- * @tc.desc: Test the function MouseEventNormalize::SetUpDeviceObserver
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(MouseEventNormalizeTestWithMock, SetUpDeviceObserver_001, TestSize.Level1)
-{
-    EXPECT_CALL(*INPUT_DEV_MGR, Attach).Times(testing::Exactly(1));
-    EXPECT_CALL(*INPUT_DEV_MGR, Detach).Times(testing::Exactly(1));
-
-    MouseEventNormalize norm;
-    EXPECT_NO_FATAL_FAILURE(norm.SetUpDeviceObserver());
-    EXPECT_NE(norm.inputDevObserver_, nullptr);
-}
-
-/**
- * @tc.name: SetUpDeviceObserver_002
- * @tc.desc: Test the function MouseEventNormalize::SetUpDeviceObserver
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(MouseEventNormalizeTestWithMock, SetUpDeviceObserver_002, TestSize.Level1)
-{
-    EXPECT_CALL(*INPUT_DEV_MGR, Attach).Times(testing::Exactly(2));
-    EXPECT_CALL(*INPUT_DEV_MGR, Detach).Times(testing::Exactly(2));
-
-    MouseEventNormalize norm;
-    norm.TearDownDeviceObserver();
-    EXPECT_NO_FATAL_FAILURE(norm.SetUpDeviceObserver());
-    EXPECT_NE(norm.inputDevObserver_, nullptr);
-}
-
-/**
- * @tc.name: TearDownDeviceObserver_001
- * @tc.desc: Test the function MouseEventNormalize::TearDownDeviceObserver
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(MouseEventNormalizeTestWithMock, TearDownDeviceObserver_001, TestSize.Level1)
-{
-    EXPECT_CALL(*INPUT_DEV_MGR, Attach).Times(testing::Exactly(1));
-    EXPECT_CALL(*INPUT_DEV_MGR, Detach).Times(testing::Exactly(1));
-
-    MouseEventNormalize norm;
-    EXPECT_NO_FATAL_FAILURE(norm.TearDownDeviceObserver());
-    EXPECT_EQ(norm.inputDevObserver_, nullptr);
-}
-
-/**
- * @tc.name: TearDownDeviceObserver_002
- * @tc.desc: Test the function MouseEventNormalize::TearDownDeviceObserver
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(MouseEventNormalizeTestWithMock, TearDownDeviceObserver_002, TestSize.Level1)
-{
-    EXPECT_CALL(*INPUT_DEV_MGR, Attach).Times(testing::Exactly(2));
-    EXPECT_CALL(*INPUT_DEV_MGR, Detach).Times(testing::Exactly(2));
-
-    MouseEventNormalize norm;
-    EXPECT_NO_FATAL_FAILURE(norm.TearDownDeviceObserver());
-
-    norm.SetUpDeviceObserver();
-    EXPECT_NO_FATAL_FAILURE(norm.TearDownDeviceObserver());
-    EXPECT_EQ(norm.inputDevObserver_, nullptr);
+    MouseEventNormalize norm(&env_);
+    EXPECT_NE(norm.env_, nullptr);
+    int32_t deviceId { 1 };
+    norm.OnDeviceAdded(deviceId);
+    std::shared_ptr<MouseTransformProcessor> processor = norm.GetProcessor(deviceId);
+    EXPECT_EQ(processor, nullptr);
 }
 
 /**
@@ -143,25 +76,55 @@ HWTEST_F(MouseEventNormalizeTestWithMock, TearDownDeviceObserver_002, TestSize.L
  */
 HWTEST_F(MouseEventNormalizeTestWithMock, OnDeviceRemoved_001, TestSize.Level1)
 {
-    EXPECT_CALL(*INPUT_DEV_MGR, Attach).Times(testing::Exactly(1));
-    EXPECT_CALL(*INPUT_DEV_MGR, Detach).Times(testing::Exactly(1));
-    EXPECT_CALL(*PREFERENCES_MGR_MOCK, GetIntValue).WillOnce(Return(DEFAULT_SPEED));
-
-    MouseEventNormalize norm;
+    struct libinput_device rawDev {
+        .udevDev { 1 },
+        .busType = 1,
+        .version = 1,
+        .product = 1,
+        .vendor = 1,
+        .name = "test",
+    };
+    MouseEventNormalize norm(&env_);
     int32_t deviceId { 1 };
-    auto [iter, _] = norm.processors_.emplace(deviceId, std::make_shared<MouseTransformProcessor>(deviceId));
-    auto processor = iter->second;
-
-    int32_t deviceId1 { 2 };
-    EXPECT_NO_FATAL_FAILURE(norm.OnDeviceRemoved(deviceId1));
-    auto iter1 = norm.processors_.find(deviceId);
-    if (iter1 != norm.processors_.end()) {
-        EXPECT_EQ(iter1->second, processor);
-    }
-    EXPECT_TRUE(iter1 != norm.processors_.end());
-
-    EXPECT_NO_FATAL_FAILURE(norm.OnDeviceRemoved(deviceId));
+    norm.OnDeviceAdded(deviceId);
+    norm.OnDeviceRemoved(deviceId);
     EXPECT_TRUE(norm.processors_.find(deviceId) == norm.processors_.end());
+}
+
+/**
+ * @tc.name: MouseEventNormalize_002
+ * @tc.desc: Test the function MouseEventNormalize::MouseEventNormalize
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(MouseEventNormalizeTestWithMock, MouseEventNormalize_002, TestSize.Level1)
+{
+    MouseEventNormalize norm(&env_);
+    EXPECT_NE(norm.env_, nullptr);
+    int32_t deviceId { 1 };
+    norm.OnDeviceAdded(deviceId);
+    std::shared_ptr<PointerEvent> pointerEvent = norm.GetPointerEvent(deviceId);
+    EXPECT_EQ(pointerEvent, nullptr);
+}
+
+/**
+ * @tc.name: MouseEventNormalize_003
+ * @tc.desc: Test the function MouseEventNormalize::MouseEventNormalize
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(MouseEventNormalizeTestWithMock, MouseEventNormalize_003, TestSize.Level1)
+{
+    MouseEventNormalize norm(&env_);
+    EXPECT_NE(norm.env_, nullptr);
+    int32_t x { 1 };
+    int32_t y { 1 };
+    int32_t displayId { 1 };
+    double displayX { 1 };
+    double displayY { 1 };
+    int32_t ret = norm.SetPointerLocation(x, y, displayId);
+    ret = norm.GetPointerLocation(displayId, displayX, displayY);
+    EXPECT_EQ(ret, RET_OK);
 }
 } // namespace MMI
 } // namespace OHOS
