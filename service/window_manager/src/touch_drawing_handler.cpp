d8ccfa8e726c5472e8468e12c688bbe17c76a185
zh_qlong <zhaoqianglong@huawei.com>
2026-02-26 13:52:58 +0800

    fix touch point drawing offset
    Signed-off-by: zh_qlong <zhaoqianglong@huawei.com>

--- service/window_manager/src/touch_drawing_handler.cpp
+++ service/window_manager/src/touch_drawing_handler.cpp
@@ -166,6 +166,8 @@ void TouchDrawingHandler::UpdateDisplayInfo(const OLD::DisplayInfo& displayInfo)
             isChangedMode_ = true;
         }
     }
+    isChangedMode_ = isChangedMode_ || 
+        (displayInfo_.validWidth != displayInfo.validWidth || displayInfo_.validHeight != displayInfo.validHeight);
     displayInfo_ = displayInfo;
     bubble_.innerCircleRadius = displayInfo.dpi * INDEPENDENT_INNER_PIXELS / DENSITY_BASELINE / CALCULATE_MIDDLE;
     bubble_.outerCircleRadius = displayInfo.dpi * INDEPENDENT_OUTER_PIXELS / DENSITY_BASELINE / CALCULATE_MIDDLE;
