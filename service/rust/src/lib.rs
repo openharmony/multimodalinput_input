45b52d3483014cd26d44023e2227123720fc8ce9
jinyu3 <huangjinyu3@h-partners.com>
2026-03-23 10:02:26 +0800

    新增触控板显控比
    Signed-off-by: jinyu3 <huangjinyu3@h-partners.com>

--- service/rust/src/lib.rs
+++ service/rust/src/lib.rs
@@ -640,6 +640,11 @@ impl AxisAccelerateCurvesTouchpad {
                         slopes: vec![3.18, 1.70, 1.63, 1.40, 1.25, 1.09, 0.46, 1.43],
                         diff_nums: vec![0.0, 0.75, 0.83, 1.16, 1.47, 1.85, 3.68, 0.0]
                     },
+                    CurveItem {
+                        speeds: vec![0.36, 0.73, 1.46, 2.19, 2.92, 5.83, 10.94, 29.17],
+                        slopes: vec![4.11, 2.83, 2.19, 1.69, 1.41, 1.87, 2.03, 2.03],
+                        diff_nums: vec![0.0, 0.47, 0.93, 1.66, 2.28, 0.93, 0.0, 0.0]
+                    },
                 ],
             });
         });
@@ -1084,6 +1089,7 @@ fn get_axis_gain_touchpad(gain: *mut f64, axis_speed: f64, device_type: i32) -> 
         8 => 7,
         9 => 8,
         10 => 9,
+        11 => 10,
         _ => 1,
     };
     let item = AxisAccelerateCurvesTouchpad::get_instance().get_axis_curve_by_speed_touchpad(valid_device_type as usize);
