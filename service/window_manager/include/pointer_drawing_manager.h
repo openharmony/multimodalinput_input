95d9871407934946e89ae7467ef256a1a3f0b8e7
yueyan <yueyan8@huawei.com>
2026-05-08 21:16:45 +0800

    fix:Fix the issue of the cursor not being displayed
    Signed-off-by: yueyan <yueyan8@huawei.com>
    
    Co-Authored-By:Agent

--- service/window_manager/include/pointer_drawing_manager.h
+++ service/window_manager/include/pointer_drawing_manager.h
@@ -314,8 +314,9 @@ private:
     void GetValidWidthAndHeight(const OLD::DisplayInfo *displayInfo, int32_t &validWidth, int32_t &validHeight);
     bool InitRSUIContext(uint64_t screenId);
     void RsFlushImplicitTransaction();
-    std::shared_ptr<Rosen::RSUIDirector> GetRSUIDirector();
     std::shared_ptr<Rosen::RSUIContext> GetRSUIContext();
+    void DestroyPointerWindowOfHardCursor();
+    void DestroyPointerWindowOfSoftCursor();
 private:
     bool hasDisplay_ { false };
     bool hasPointerDevice_ { false };
@@ -404,6 +405,8 @@ private:
     bool lastCursorBlurEnabled_ { true };
     std::atomic<bool> moveFinished_ { false };
     std::mutex cursorBlurEnableMutex_;
+    bool isRsRemoteDied_ { false };
+    std::atomic<bool> isHardCursorSurfaceNodeInited_ { false };
 };
 } // namespace MMI
 } // namespace OHOS
