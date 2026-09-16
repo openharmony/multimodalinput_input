3d9e1affa55608371ee026598095d4bc61ce07e7
RZH-150 <renzhonghao3@h-partners.com>
2026-09-16 13:44:25 +0800

    Co-Authored-By: Agent
    Signed-off-by: RZH-150 <renzhonghao3@h-partners.com>

--- frameworks/napi/input_device/include/js_util.h
+++ frameworks/napi/input_device/include/js_util.h
@@ -76,7 +76,6 @@ public:
     static bool GetDeviceAxisInfo(sptr<CallbackInfo> cb, napi_value &object);
     static bool GetDeviceSourceType(sptr<CallbackInfo> cb, napi_value &object);
     static bool TypeOf(napi_env env, napi_value value, napi_valuetype type);
-    static void DeleteCallbackInfo(std::unique_ptr<CallbackInfo> callback);
     template <typename T>
     static void DeletePtr(T &ptr)
     {
